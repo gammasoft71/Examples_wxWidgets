7330c7dd135d03c9d62d6a77140e7bcdbc7cabc6
gammasoft71 <gammasoft71@gmail.com>
2018-06-07 13:32:13 +0200

    Application class review

--- src/Application/Application.cpp
+++ src/Application/Application.cpp
@@ -1,15 +1,7 @@
 #include <wx/wx.h>
 
 class Application : public wxApp {
-public:
-  bool OnInit() override {
-    this->form = new wxFrame(nullptr, wxID_ANY, "");
-    this->form->Show(true);
-    return true;
-  }
-  
-private:
-  wxFrame* form;
+  bool OnInit() override {(new wxFrame(nullptr, wxID_ANY, ""))->Show(); return true;}
 };
 
 wxIMPLEMENT_APP(Application);
