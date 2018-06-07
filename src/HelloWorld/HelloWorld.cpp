ca093de56af3877cf7dacbf546eb2b5c585ed3c7
gammasoft71 <gammasoft71@gmail.com>
2018-06-07 13:25:37 +0200

    add HelloWorld

---
+++ src/HelloWorld/HelloWorld.cpp
@@ -0,0 +1,20 @@
+#include <wx/wx.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "My first application", wxDefaultPosition, wxSize(300, 300)) {
+    this->label1->SetFont(wxFont(45, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_BOLD, false, "Arial"));
+    this->label1->wxWindowBase::SetOwnForegroundColour(wxColourDatabase().Find("FOREST GREEN"));
+    this->Show();
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "Hello, World!", wxPoint(10, 100));
+};
+
+class Application : public wxApp {
+  bool OnInit() override {new Form(); return true;}
+};
+
+wxIMPLEMENT_APP(Application);
