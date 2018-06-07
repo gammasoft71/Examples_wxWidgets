bd249a0cd403e0bf3502ccd877468c3232bbe0bb
gammasoft71 <gammasoft71@gmail.com>
2018-06-07 13:57:53 +0200

    add HelloWorld

--- src/HelloWorld/HelloWorld.cpp
+++ src/HelloWorld/HelloWorld.cpp
@@ -5,6 +5,8 @@ public:
   Form() : wxFrame(nullptr, wxID_ANY, "My first application", wxDefaultPosition, wxSize(300, 300)) {
     this->label1->SetFont(wxFont(45, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_BOLD, false, "Arial"));
     this->label1->SetForegroundColour(wxColourDatabase().Find("FOREST GREEN"));
+
+    this->CenterOnScreen();
     this->Show();
   }
   
@@ -14,6 +16,7 @@ private:
 };
 
 class Application : public wxApp {
+public:
   bool OnInit() override {new Form(); return true;}
 };
 
