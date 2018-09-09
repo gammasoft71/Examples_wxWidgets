1b9b9cbea2aeab657f676758d1b110139e3d4e2d
Gammasoft71 <gammasoft71@gmail.com>
2018-09-09 18:54:53 +0200

    Fix button

--- src/Button/Button.cpp
+++ src/Button/Button.cpp
@@ -2,7 +2,7 @@
 
 class Form : public wxFrame {
 public:
-  Form() : wxFrame(nullptr, wxID_ANY, "Form example", wxDefaultPosition, wxSize(300, 300)) {
+  Form() : wxFrame(nullptr, wxID_ANY, "", wxDefaultPosition, wxSize(300, 300)) {
     this->button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
       this->label1->SetLabel(wxString::Format("button1 clicked %d times", ++this->button1Clicked));
     });
@@ -16,8 +16,8 @@ public:
   
 private:
   wxPanel* panel = new wxPanel(this);
-  wxButton* button1 = new wxButton(this->panel, wxID_ANY, "button1", wxPoint(50, 50));;
-  wxButton* button2 = new wxButton(this->panel, wxID_ANY, "button2", wxPoint(50, 100), wxSize(200, 75));;
+  wxButton* button1 = new wxButton(this->panel, wxID_ANY, "button1", wxPoint(50, 50));
+  wxButton* button2 = new wxButton(this->panel, wxID_ANY, "button2", wxPoint(50, 100), wxSize(200, 75));
   wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "button1 clicked 0 times", wxPoint(50, 200), wxSize(200, 20));
   wxStaticText* label2 = new wxStaticText(this->panel, wxID_ANY, "button2 clicked 0 times", wxPoint(50, 230), wxSize(200, 20));
   int button1Clicked = 0;
