b0de7f9ddc4e4438b0c3578c43fa1c1157849218
Gammasoft71 <gammasoft71@gmail.com>
2019-03-14 21:28:03 +0100

    add ComboBox example

---
+++ src/ComboBox/ComboBox.cpp
@@ -0,0 +1,24 @@
+#include <wx/wx.h>
+#include <vector>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "ComboBox example", wxDefaultPosition, wxSize(300, 300)) {
+    this->comboBox1->Insert("item1", 0);
+    this->comboBox1->Insert("item2", 1);
+    this->comboBox1->Insert("item3", 2);
+    this->comboBox1->Select(1);
+    this->Show();
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxComboBox* comboBox1 = new wxComboBox(this->panel, wxID_ANY, "", wxPoint(10, 10));
+};
+
+class Application : public wxApp {
+public:
+  bool OnInit() override {new Form(); return true;}
+};
+
+wxIMPLEMENT_APP(Application);
