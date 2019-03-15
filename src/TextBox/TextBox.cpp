c394b75988e1ac69b438bc4a6cdca76ffad1622e
Gammasoft71 <gammasoft71@gmail.com>
2019-03-15 11:37:34 +0100

    Add TextBox and Timer examples

---
+++ src/TextBox/TextBox.cpp
@@ -0,0 +1,29 @@
+#include <wx/wx.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "TextBox example", wxDefaultPosition, wxSize(300, 300)) {
+    this->textBox1->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
+      if (this->textBox2->GetValue() != this->textBox1->GetValue())
+        this->textBox2->SetValue(this->textBox1->GetValue());
+    });
+
+    this->textBox2->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
+      if (this->textBox1->GetValue() != this->textBox2->GetValue())
+        this->textBox1->SetValue(this->textBox2->GetValue());
+    });
+    this->Show();
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxTextCtrl* textBox1 = new wxTextCtrl(this->panel, wxID_ANY, "textBox", wxPoint(10, 10));
+  wxTextCtrl* textBox2 = new wxTextCtrl(this->panel, wxID_ANY, "textBox", wxPoint(10, 50));
+};
+
+class Application : public wxApp {
+public:
+  bool OnInit() override {new Form(); return true;}
+};
+
+wxIMPLEMENT_APP(Application);
---
+++ src/Timer/Timer.cpp
@@ -0,0 +1,37 @@
+#include <wx/wx.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "Timer example", wxDefaultPosition, wxSize(230, 150)) {
+    this->label->SetFont(wxFont(48, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_NORMAL, false, "Arial"));
+    this->label->SetForegroundColour(wxColour(30, 144, 255));
+
+    this->timer->Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
+      this->label->SetLabelText(wxString::Format("%.1f", static_cast<double>(++this->counter) / 10));
+    });
+    
+    this->button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+      if (this->timer->IsRunning())
+        this->timer->Stop();
+      else
+        this->timer->Start(100);
+      this->button->SetLabel(this->timer->IsRunning() ? "Stop" : "Start");
+    });
+    
+    this->Show();
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxStaticText* label = new wxStaticText(this->panel, wxID_ANY, "0.0", wxPoint(10, 10));
+  wxButton* button = new wxButton(this->panel, wxID_ANY, "Start", wxPoint(10, 90));
+  wxTimer* timer = new wxTimer();
+  int counter = 0;
+};
+
+class Application : public wxApp {
+public:
+  bool OnInit() override {new Form(); return true;}
+};
+
+wxIMPLEMENT_APP(Application);
