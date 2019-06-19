60a1525bbea1da47b7f89db7f486b72b7ff7fa3c
Gammasoft71 <gammasoft71@gmail.com>
2019-06-19 22:33:44 +0200

    rename to CheckedListBox

--- src/CheckedListBox/CheckedListBox.cpp
+++ src/CheckedListBox/CheckedListBox.cpp
@@ -3,10 +3,13 @@
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "CheckedListBox example", wxDefaultPosition, wxSize(300, 300)) {
+    this->SetClientSize(200, 240);
     for (int i = 1; i <= 1000; ++i) {
       this->checkedListBox->Insert(wxString::Format("Item %d", i), i - 1);
       this->checkedListBox->Check(i - 1, i % 2 != 0);
+      this->checkedListBox->EnsureVisible(0);
     }
+    this->checkedListBox->SetSelection(0);
     this->Show();
   }
   
