af68cc0978fdad45c889c6388463a103aa391d62
Gammasoft71 <gammasoft71@gmail.com>
2019-06-20 12:43:51 +0200

    Update DateTimePicker.cpp

--- src/DateTimePicker/DateTimePicker.cpp
+++ src/DateTimePicker/DateTimePicker.cpp
@@ -1,17 +1,23 @@
 #include <wx/wx.h>
 #include <wx/datectrl.h>
+#include <wx/dateevt.h>
 
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "DateTimePicker example", wxDefaultPosition, wxSize(300, 300)) {
-    this->label1->SetLabelText(this->dateTimePicker1->GetValue().FormatDate());
+    this->datePicker1->SetRange(wxDateTime(4, wxDateTime::Apr, 1975), wxDateTime::Now());
+    this->datePicker1->SetValue(wxDateTime(4, wxDateTime::Apr, 1975));
+    this->datePicker1->Bind(wxEVT_DATE_CHANGED, [&](wxDateEvent& event) {
+      this->label1->SetLabelText(this->datePicker1->GetValue().FormatDate());
+    });
+    this->label1->SetLabelText(this->datePicker1->GetValue().FormatDate());
     this->Show();
   }
   
 private:
   wxPanel* panel = new wxPanel(this);
+  wxDatePickerCtrl* datePicker1 = new wxDatePickerCtrl(this->panel, wxID_ANY, wxDefaultDateTime, wxPoint(30, 30));
   wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "", wxPoint(30, 60));
-  wxDatePickerCtrl* dateTimePicker1 = new wxDatePickerCtrl(this->panel, wxID_ANY, wxDateTime(4, wxDateTime::Apr, 1975, 0, 0, 0, 0), wxPoint(30, 30));
 };
 
 class Application : public wxApp {
