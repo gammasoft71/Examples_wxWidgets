105cbffbec4e7a4fed20fb07556efc26c2c99fd5
Gammasoft71 <gammasoft71@gmail.com>
2019-06-23 13:54:50 +0200

    Update PictureBox.cpp

--- src/CommonControls/PictureBox/PictureBox.cpp
+++ src/CommonControls/PictureBox/PictureBox.cpp
@@ -3,11 +3,12 @@
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "PictureBox example", wxDefaultPosition, wxSize(300, 300)) {
+    this->SetClientSize(wxSize(300, 300));
     wxImage::AddHandler(new wxPNGHandler());
     this->pictureBox1 = new wxStaticBitmap(this, wxID_ANY, wxBitmap("Resources/Logo.png", wxBITMAP_TYPE_PNG), wxPoint(20,10), wxSize(260, 280));
     this->pictureBox1->SetWindowStyle(wxBORDER_SUNKEN);
   }
-  
+
 private:
   wxPanel* panel = new wxPanel(this);
   wxStaticBitmap* pictureBox1;
