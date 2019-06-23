cf9cbcdd22f79c71a11825f0eaa243857756ef40
Gammasoft71 <gammasoft71@gmail.com>
2019-06-23 13:58:16 +0200

    Update Form.cpp

--- src/Containers/Form/Form.cpp
+++ src/Containers/Form/Form.cpp
@@ -6,7 +6,7 @@ public:
     this->button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
       this->Close();
     });
-    
+
     this->Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
       if ( event.CanVeto()) {
         if (wxMessageBox("Are you sure you want exit?", "Close Form", wxICON_QUESTION | wxYES_NO) == wxYES) {
@@ -17,14 +17,14 @@ public:
       }
     });
   }
-  
+
 private:
   wxPanel* panel = new wxPanel(this);
   wxButton* button = new wxButton(panel, wxID_ANY, "Close", wxPoint(10, 10));
 };
 
 class Application : public wxApp {
-  bool OnInit() override {new Form(); return true;}
+  bool OnInit() override {return (new Form())->Show();}
 };
 
 wxIMPLEMENT_APP(Application);
