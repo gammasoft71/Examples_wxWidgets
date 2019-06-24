5cfab4517523c33abf3dceaef8ac7acd926c65a5
Gammasoft71 <gammasoft71@gmail.com>
2019-06-24 16:42:11 +0200

    Update HelloWorld.cpp

--- src/HelloWorld/HelloWorld/HelloWorld.cpp
+++ src/HelloWorld/HelloWorld/HelloWorld.cpp
@@ -1,10 +1,16 @@
 #include <wx/wx.h>
 
+#if defined(__APPLE__)
+  constexpr double DeviceUnitScaleCorrection =  4.0 / 3.0;
+#else
+  constexpr double DeviceUnitScaleCorrection =  1.0;
+#endif
+
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "My first application", wxDefaultPosition) {
     this->SetClientSize(wxSize(300, 300));
-    this->label1->SetFont(wxFont(45, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_BOLD, false, "Arial"));
+    this->label1->SetFont(wxFont(32 * DeviceUnitScaleCorrection, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_BOLD, false, "Arial"));
     this->label1->SetForegroundColour(wxTheColourDatabase->Find("FOREST GREEN"));
 
     this->CenterOnScreen();
