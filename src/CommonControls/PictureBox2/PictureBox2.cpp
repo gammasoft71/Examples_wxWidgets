333dea2e33744b6fe51cc86fb9a0f2110cb8408b
gammasoft71 <gammasoft71@gmail.com>
2019-06-24 23:24:41 +0200

    switch PictureBox and PictureBox2

--- src/CommonControls/PictureBox2/PictureBox2.cpp
+++ src/CommonControls/PictureBox2/PictureBox2.cpp
@@ -1,12 +1,11 @@
 #include <wx/wx.h>
-#include "Logo.xpm"
 
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "PictureBox2 example", wxDefaultPosition, wxSize(300, 300)) {
     this->SetClientSize(wxSize(300, 300));
     wxImage::AddHandler(new wxPNGHandler());
-    this->pictureBox1 = new wxStaticBitmap(this, wxID_ANY, wxBitmap(Logo_xpm), wxPoint(20,10), wxSize(260, 280));
+    this->pictureBox1 = new wxStaticBitmap(this, wxID_ANY, wxBitmap("Resources/Logo.png", wxBITMAP_TYPE_PNG), wxPoint(20,10), wxSize(260, 280));
     this->pictureBox1->SetWindowStyle(wxBORDER_SUNKEN);
   }
 
