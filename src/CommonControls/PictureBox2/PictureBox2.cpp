065b9d80fca299ce1b791c171d73b96e701d3ece
Gammasoft71 <gammasoft71@gmail.com>
2019-06-24 23:07:27 +0200

    add PictureBox2 example

---
+++ src/CommonControls/PictureBox2/PictureBox2.cpp
@@ -0,0 +1,22 @@
+#include <wx/wx.h>
+#include "Logo.xpm"
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "PictureBox example", wxDefaultPosition, wxSize(300, 300)) {
+    this->SetClientSize(wxSize(300, 300));
+    wxImage::AddHandler(new wxPNGHandler());
+    this->pictureBox1 = new wxStaticBitmap(this, wxID_ANY, wxBitmap(Logo_xpm), wxPoint(20,10), wxSize(260, 280));
+    this->pictureBox1->SetWindowStyle(wxBORDER_SUNKEN);
+  }
+
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxStaticBitmap* pictureBox1;
+};
+
+class Application : public wxApp {
+  bool OnInit() override {return (new Form())->Show();}
+};
+
+wxIMPLEMENT_APP(Application);
