83472f23b1295ef09bd2951b744891a4052bb4a2
Gammasoft71 <gammasoft71@gmail.com>
2019-06-24 12:53:00 +0200

    Update TrackBar.cpp

--- src/CommonControls/TrackBar/TrackBar.cpp
+++ src/CommonControls/TrackBar/TrackBar.cpp
@@ -4,8 +4,10 @@ class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "TrackBar example", wxDefaultPosition, wxSize(300, 300)) {
     this->progressBar1->SetValue(100);
+    this->label1->SetLabel(wxString::Format("%d", this->trackBar1->GetValue()));
     this->trackBar1->Bind(wxEVT_SLIDER, [&](wxCommandEvent& event) {
       this->progressBar1->SetValue(this->trackBar1->GetValue());
+      this->label1->SetLabel(wxString::Format("%d", this->trackBar1->GetValue()));
     });
   }
   
@@ -13,6 +15,7 @@ private:
   wxPanel* panel = new wxPanel(this);
   wxSlider* trackBar1 = new wxSlider(this->panel, wxID_ANY, 100, 0, 200, wxPoint(20, 50), wxSize(200, 25));
   wxGauge* progressBar1 = new wxGauge(this->panel, wxID_ANY, 200, wxPoint(20, 100), wxSize(200, 25));
+  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, wxEmptyString, wxPoint(20, 150));
 };
 
 class Application : public wxApp {
