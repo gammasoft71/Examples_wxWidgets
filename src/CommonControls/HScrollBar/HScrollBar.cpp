fc8c5e561362f0f240048f1f374b7235c957ee26
Gammasoft71 <gammasoft71@gmail.com>
2019-06-25 10:11:34 +0200

    add HScrollBar example

---
+++ src/CommonControls/HScrollBar/HScrollBar.cpp
@@ -0,0 +1,21 @@
+#include <wx/wx.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "Label example", wxDefaultPosition, wxSize(300, 300)) {
+    this->scrollBar2->SetRange(200);
+    this->scrollBar2->SetThumbPosition(100);
+    this->scrollBar2->SetSize(220, this->scrollBar2->GetSize().GetHeight());
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxScrollBar* scrollBar1 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(30, 40), wxDefaultSize, wxSB_HORIZONTAL);
+  wxScrollBar* scrollBar2 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(30, 80), wxDefaultSize, wxSB_HORIZONTAL);
+};
+
+class Application : public wxApp {
+  bool OnInit() override {return (new Form())->Show();}
+};
+
+wxIMPLEMENT_APP(Application);
