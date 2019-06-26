a8d0c6a8866e30560c1a94f870b912c6c9e2382c
Gammasoft71 <gammasoft71@gmail.com>
2019-06-26 12:04:06 +0200

    Update HScrollBar.cpp

--- src/CommonControls/HScrollBar/HScrollBar.cpp
+++ src/CommonControls/HScrollBar/HScrollBar.cpp
@@ -3,15 +3,14 @@
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "HScrollBar example", wxDefaultPosition, wxSize(300, 300)) {
-    this->scrollBar2->SetRange(200);
-    this->scrollBar2->SetThumbPosition(100);
+    this->scrollBar2->SetScrollbar(100, 1, 200, 1);
     this->scrollBar2->SetSize(220, this->scrollBar2->GetSize().GetHeight());
   }
   
 private:
   wxPanel* panel = new wxPanel(this);
-  wxScrollBar* scrollBar1 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(30, 10), wxDefaultSize, wxSB_HORIZONTAL);
-  wxScrollBar* scrollBar2 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(70, 10), wxDefaultSize, wxSB_HORIZONTAL);
+  wxScrollBar* scrollBar1 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(10, 30), wxDefaultSize, wxSB_HORIZONTAL);
+  wxScrollBar* scrollBar2 = new wxScrollBar(this->panel, wxID_ANY, wxPoint(10, 70), wxDefaultSize, wxSB_HORIZONTAL);
 };
 
 class Application : public wxApp {
