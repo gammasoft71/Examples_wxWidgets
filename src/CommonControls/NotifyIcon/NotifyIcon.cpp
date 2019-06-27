0d1233b1d3b7d84184f147673da75fdb8dd5a48d
Gammasoft71 <gammasoft71@gmail.com>
2019-06-27 17:25:23 +0200

    Update NotifyIcon.cpp

--- src/CommonControls/NotifyIcon/NotifyIcon.cpp
+++ src/CommonControls/NotifyIcon/NotifyIcon.cpp
@@ -3,22 +3,33 @@
 
 #include "Gammasoft.xpm"
 
+class wxNotifyIcon : public wxTaskBarIcon {
+public:
+  wxNotifyIcon(wxFrame* frame) : frame(frame) {}
+  
+  wxMenu* CreatePopupMenu() override {
+    if (this->frame) {
+      if (this->frame->IsShown())
+        this->frame->Hide();
+      else
+        this->frame->Show();
+    }
+    return nullptr;
+  }
+
+  wxFrame* frame = nullptr;
+};
+
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, wxSize(300, 300)) {
     this->notifyIcon1.SetIcon(wxIcon(GammasoftIcon));
-    this->notifyIcon1.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
-      if (this->IsShown())
-        this->Hide();
-      else
-        this->Show();
-    });
   }
   
 private:
   wxPanel* panel = new wxPanel(this);
-  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this form.", wxPoint(10, 10));
-  wxTaskBarIcon notifyIcon1;
+  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "Click on Gammasoft tray icon\nto show or hide this form.", wxPoint(10, 10));
+  wxNotifyIcon notifyIcon1 {this};
 };
 
 class Application : public wxApp {
