e7df7cb42eeea4d58e8664eb46cda142d4bc0c43
Gammasoft71 <gammasoft71@gmail.com>
2019-06-27 23:52:41 +0200

    Update wxNotifyIcon.h

--- src/CommonControls/NotifyIcon/wxNotifyIcon.h
+++ src/CommonControls/NotifyIcon/wxNotifyIcon.h
@@ -5,25 +5,26 @@
 
 class wxNotifyIcon : public wxTaskBarIcon {
 public:
-  wxNotifyIcon(wxTaskBarIconType iconType = wxTBI_DEFAULT_TYPE) : wxTaskBarIcon(iconType) {}
-  
-  wxMenu* CreatePopupMenu() override {
-#if defined(__APPLE__)
-    if (!this->timer.IsRunning())
-      this->timer.StartOnce(wxSystemSettings::GetMetric(wxSYS_DCLICK_MSEC));
-    else {
-      this->timer.Stop();
-      wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_DCLICK, this));
-    }
-    
+  wxNotifyIcon(wxTaskBarIconType iconType = wxTBI_DEFAULT_TYPE) : wxTaskBarIcon(iconType) {
     this->timer.Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
       wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_DOWN, this));
       wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_UP, this));
     });
-#endif
-    
+  }
+  
+protected:
+#if defined(__APPLE__)
+  wxMenu* CreatePopupMenu() override {
+    if (!this->timer.IsRunning()) {
+      this->timer.StartOnce(wxSystemSettings::GetMetric(wxSYS_DCLICK_MSEC));
+    } else {
+      this->timer.Stop();
+      wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_DCLICK, this));
+    }
     return this->wxTaskBarIcon::CreatePopupMenu();
   }
+#endif
 
+private:
   wxTimer timer;
 };
