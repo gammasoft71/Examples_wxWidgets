1fefb66c4d9610f487e7a912255077562880a247
Gammasoft71 <gammasoft71@gmail.com>
2019-06-27 11:09:42 +0200

    Update ComboBox.cpp

--- src/CommonControls/ComboBox/ComboBox.cpp
+++ src/CommonControls/ComboBox/ComboBox.cpp
@@ -4,9 +4,9 @@
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "ComboBox example", wxDefaultPosition, wxSize(300, 300)) {
-    this->comboBox1->Insert("item1", 0);
-    this->comboBox1->Insert("item2", 1);
-    this->comboBox1->Insert("item3", 2);
+    this->comboBox1->Append("item1");
+    this->comboBox1->Append("item2");
+    this->comboBox1->Append("item3");
     this->comboBox1->Select(1);
   }
   
