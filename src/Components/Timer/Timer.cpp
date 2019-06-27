65bbb36cffb65702fbf71826d81ababfb3755fd8
gammasoft71 <gammasoft71@gmail.com>
2019-06-27 15:59:26 +0200

    Update Timer.cpp

--- src/Components/Timer/Timer.cpp
+++ src/Components/Timer/Timer.cpp
@@ -7,16 +7,16 @@ public:
     this->label->SetFont(wxFont(48, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_NORMAL, false, "Arial"));
     this->label->SetForegroundColour(wxColour(30, 144, 255));
 
-    this->timer->Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
+    this->timer.Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
       this->label->SetLabelText(wxString::Format("%.1f", static_cast<double>(++this->counter) / 10));
     });
 
     this->button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      if (this->timer->IsRunning())
-        this->timer->Stop();
+      if (this->timer.IsRunning())
+        this->timer.Stop();
       else
-        this->timer->Start(100);
-      this->button->SetLabel(this->timer->IsRunning() ? "Stop" : "Start");
+        this->timer.Start(100);
+      this->button->SetLabel(this->timer.IsRunning() ? "Stop" : "Start");
     });
   }
 
@@ -24,7 +24,7 @@ private:
   wxPanel* panel = new wxPanel(this);
   wxStaticText* label = new wxStaticText(this->panel, wxID_ANY, "0.0", wxPoint(10, 10));
   wxButton* button = new wxButton(this->panel, wxID_ANY, "Start", wxPoint(10, 90));
-  wxTimer* timer = new wxTimer();
+  wxTimer timer;
   int counter = 0;
 };
 
