ab8abcd361e5aa7ac71bbe4195823ffed809d343
gammasoft71 <gammasoft71@gmail.com>
2019-06-27 17:11:12 +0200

    Update NotifyIcon.cpp

--- src/CommonControls/NotifyIcon/NotifyIcon.cpp
+++ src/CommonControls/NotifyIcon/NotifyIcon.cpp
@@ -6,8 +6,8 @@
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, wxSize(300, 300)) {
-    this->notifyIcon1->SetIcon(wxIcon(GammasoftIcon));
-    this->notifyIcon1->Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
+    this->notifyIcon1.SetIcon(wxIcon(GammasoftIcon));
+    this->notifyIcon1.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
       if (this->IsShown())
         this->Hide();
       else
@@ -18,7 +18,7 @@ public:
 private:
   wxPanel* panel = new wxPanel(this);
   wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this form.", wxPoint(10, 10));
-  wxTaskBarIcon* notifyIcon1 = new wxTaskBarIcon();
+  wxTaskBarIcon notifyIcon1;
 };
 
 class Application : public wxApp {
