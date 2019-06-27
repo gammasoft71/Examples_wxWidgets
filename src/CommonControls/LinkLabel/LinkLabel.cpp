4716025ff04109e622ea0f262d59f12a8909c816
gammasoft71 <gammasoft71@gmail.com>
2019-06-27 15:38:56 +0200

    Update LinkLabel.cpp

--- src/CommonControls/LinkLabel/LinkLabel.cpp
+++ src/CommonControls/LinkLabel/LinkLabel.cpp
@@ -9,7 +9,7 @@ private:
   wxPanel* panel = new wxPanel(this);
   wxHyperlinkCtrl* label1 = new wxHyperlinkCtrl(this->panel, wxID_ANY, "Gammasoft", "https://gammasoft71.wixsite.com/gammasoft", wxPoint(10, 10));
   wxStaticText* label2 = new wxStaticText(this->panel, wxID_ANY, " present ", wxPoint(label1->GetPosition().x + this->label1->GetSize().GetWidth(), 10));
-  wxHyperlinkCtrl* label3 = new wxHyperlinkCtrl(this->panel, wxID_ANY, "CSharp", "https://gammasoft71.wixsite.com/gammasoft/csharp", wxPoint(label2->GetPosition().x + this->label2->GetSize().GetWidth(), 10));
+  wxHyperlinkCtrl* label3 = new wxHyperlinkCtrl(this->panel, wxID_ANY, "wxWidgets", "https://gammasoft71.wixsite.com/gammasoft/wxwidgets", wxPoint(label2->GetPosition().x + this->label2->GetSize().GetWidth(), 10));
   wxStaticText* label4 = new wxStaticText(this->panel, wxID_ANY, " examples", wxPoint(label3->GetPosition().x + this->label3->GetSize().GetWidth(), 10));
 };
 
