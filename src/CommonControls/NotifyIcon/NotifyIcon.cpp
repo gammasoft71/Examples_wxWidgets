a0ca9f711afdb6f79b913fc5cb6165c259814a2f
Gammasoft71 <gammasoft71@gmail.com>
2019-06-27 17:08:34 +0200

    add NotifyIcon example

---
+++ src/CommonControls/NotifyIcon/NotifyIcon.cpp
@@ -0,0 +1,28 @@
+#include <wx/wx.h>
+#include <wx/taskbar.h>
+
+#include "Gammasoft.xpm"
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, wxSize(300, 300)) {
+    this->notifyIcon1->SetIcon(wxIcon(GammasoftIcon));
+    this->notifyIcon1->Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
+      if (this->IsShown())
+        this->Hide();
+      else
+        this->Show();
+    });
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this form.", wxPoint(10, 10));
+  wxTaskBarIcon* notifyIcon1 = new wxTaskBarIcon();
+};
+
+class Application : public wxApp {
+  bool OnInit() override {return (new Form())->Show();}
+};
+
+wxIMPLEMENT_APP(Application);
