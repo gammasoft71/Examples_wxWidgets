91a3cc1e8fc753b3e4e79b88d1d6a99ac1468534
Gammasoft71 <gammasoft71@gmail.com>
2019-07-02 22:24:47 +0200

    fix cmakefiles

--- src/Tests/TestGui/TestGui.cpp
+++ src/Tests/TestGui/TestGui.cpp
@@ -1,16 +1,37 @@
+#include <iostream>
 #include <wx/wx.h>
+#include <wx/vscroll.h>
 
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "Test gui", wxDefaultPosition, wxSize(300, 300)) {
+    this->panel->Fit();
+    this->panel->Refresh();
+    wxSizer* sizer = this->panel->GetSizer();
+    if (sizer) sizer->Layout();
+  }
+  
+  bool ProcessEvent (wxEvent &event) override {
+    if (event.GetEventType() == wxEVT_CREATE) {
+      std::cout << "event {WM_CREATE}" << std::endl;
+    }
+    else if (event.GetEventType() == wxEVT_DESTROY) std::cout << "event {WM_DESTROY}" << std::endl;
+    else if (event.GetEventType() == wxEVT_LEFT_DOWN) std::cout << "event {WM_LBUTTON_DOWN}" << std::endl;
+    else if (event.GetEventType() == wxEVT_LEFT_UP) std::cout << "event {WM_LBUTTON_UP}" << std::endl;
+
+    //else std::cout << "event {" << std::hex << event.GetEventType() << "}" << std::endl;
+    
+    return this->wxFrame::ProcessEvent(event);
   }
 
 private:
-  wxPanel* panel = new wxPanel(this);
+  wxScrolledWindow* panel = new wxScrolledWindow(this);
+  wxStaticText* label1 = new wxStaticText(this, wxID_ANY, "Label1", wxPoint(10, 150));
+  wxButton* label2 = new wxButton(this, wxID_ANY, "Button", wxPoint(10, 200));
 };
 
 class Application : public wxApp {
-  bool OnInit() override {return (new Form())->Show();}
+  bool OnInit() override {return true; /*(new Form())->Show();*/}
 };
 
 wxIMPLEMENT_APP(Application);
