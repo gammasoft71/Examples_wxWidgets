be508c976a6c46e083a29b12382bc700abf8bf6b
Gammasoft <gammasoft71@gmail.com>
2019-08-05 23:58:06 +0200

    Update AppInitializer.h

--- src/Application/ApplicationAndMain2/AppInitializer.h
+++ src/Application/ApplicationAndMain2/AppInitializer.h
@@ -1,14 +1,19 @@
 #pragma once
+#include <memory>
 #include <wx/wx.h>
 
 struct AppIinitializer {
   AppIinitializer() {
     if (wxTheApp) return;
     wxApp::SetInstance(new wx_application());
+    this->wxinitializer = make_unique<wxInitializer>();
     wxTheApp->CallOnInit();
   }
   
   ~AppIinitializer() {
     wxTheApp->OnExit();
+    this->wxinitializer = nullptr;
   }
+  
+  std::unique_ptr<wxInitializer> wxinitializer;
 };
