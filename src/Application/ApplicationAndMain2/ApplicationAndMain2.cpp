5532b253c3f13a3bd808f8fa47e78b419dcba4a0
Gammasoft <gammasoft71@gmail.com>
2019-08-05 23:59:31 +0200

    Update ApplicationAndMain2.cpp

--- src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
+++ src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
   AppInitializer appIinitializer;
-  wxFrame form1 = new wxFrame(nullptr, "Form1", wxDefaultPosition, wxSize(300, 300));
+  wxFrame* form1 = new wxFrame(nullptr, "Form1", wxDefaultPosition, wxSize(300, 300));
   form1->Show();
   wxTheApp->OnRun();
 }
