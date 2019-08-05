ed1639cd2ae53640d43df5ba59cf4d560f922425
Gammasoft <gammasoft71@gmail.com>
2019-08-05 21:21:16 +0200

    Update ApplicationAndMain2.cpp

--- src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
+++ src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
@@ -1,2 +1,7 @@
 #include <wx/wx.h>
 
+int main(int argc, char* argv[]) {
+  wxFrame form1 = new wxFrame(nullptr, "Form1", wxDefaultPosition, wxSize(300, 300));
+  form1->Show();
+  wxTheApp->DoEvent();
+}
