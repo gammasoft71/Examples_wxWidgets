e488975c6bd72a289a92af2298d48b84f096328e
Gammasoft <gammasoft71@gmail.com>
2019-08-05 23:43:15 +0200

    Create AppInitializer.h

---
+++ src/Application/ApplicationAndMain2/AppInitializer.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <wx/wx.h>
+
+struct AppIinitializer {
+  AppIinitializer() {
+    if (wxTheApp) return;
+    wxApp::SetInstance(new wx_application());
+    wxTheApp->CallOnInit();
+  }
+  
+  ~AppIinitializer() {
+    wxTheApp->OnExit();
+  }
+};
