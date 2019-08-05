99fe1de19b6183cb166a1a8cc0716a383433c9bd
Gammasoft <gammasoft71@gmail.com>
2019-08-05 23:35:42 +0200

    Update ApplicationAndMain2.cpp

--- src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
+++ src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
@@ -1,6 +1,8 @@
 #include <wx/wx.h>
+#include "AppIinitializer.h"
 
 int main(int argc, char* argv[]) {
+  AppInitializer appIinitializer;
   wxFrame form1 = new wxFrame(nullptr, "Form1", wxDefaultPosition, wxSize(300, 300));
   form1->Show();
   wxTheApp->OnRun();
