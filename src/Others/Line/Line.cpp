f0a37c7985b6765b4fa40729a0ebf872adc3c674
Gammasoft <gammasoft71@gmail.com>
2019-08-06 10:07:00 +0200

    Update Line.cpp

--- src/Others/Line/Line.cpp
+++ src/Others/Line/Line.cpp
@@ -2,24 +2,29 @@
 #include <wx/statline.h>
 
 namespace Examples {
+  class wxColoredLine : public wxPanel {
+  public:
+    wxColoredLine(wxWindow* parent, wxWindowID id, const wxColour& colour, const wxPoint& position = wxDefaultPosition, const wxSize& size = wxDefaultSize) : wxPanel(parent, id, position, size) {
+      this->SetColour(colour);
+    }
+    
+    void SetColour(const wxColour& colour) {this->SetBackgroundColour(colour);}
+    const wxColour& GetColour() const {return this->GetBackgroundColour();}
+  };
+  
   class Form : public wxFrame {
   public:
     Form() : wxFrame(nullptr, wxID_ANY, "Lines example", wxDefaultPosition, wxSize(300, 300)) {
       this->SetClientSize(wxSize(300, 300));
-      
-      this->lineRed->SetBackgroundColour(wxColour(255, 0, 0));
-      this->lineGreen->SetBackgroundColour(wxColour(0, 143, 0));
-      this->lineBlue->SetBackgroundColour(wxColour(0, 0, 255));
-      this->lineSeparator2->SetBackgroundColour(wxColour(96, 96, 96));
     }
     
   private:
     wxPanel* panel = new wxPanel(this);
     wxStaticLine* lineSeparator1 = new wxStaticLine(this->panel, wxID_ANY, wxPoint(10, 10), wxSize(280, 2), wxLI_HORIZONTAL);
-    wxPanel* lineRed = new wxPanel(this->panel, wxID_ANY, wxPoint(10, 20), wxSize(2, 250));
-    wxPanel* lineGreen = new wxPanel(this->panel, wxID_ANY, wxPoint(149, 20), wxSize(2, 250));
-    wxPanel* lineBlue = new wxPanel(this->panel, wxID_ANY, wxPoint(288, 20), wxSize(2, 250));
-    wxPanel* lineSeparator2 = new wxPanel(this->panel, wxID_ANY, wxPoint(10, 278), wxSize(280, 2));
+    wxColoredLine* lineRed = new wxColoredLine(this->panel, wxID_ANY, wxColour(255, 0, 0), wxPoint(10, 20), wxSize(2, 250));
+    wxColoredLine* lineGreen = new wxColoredLine(this->panel, wxID_ANY, wxColour(0, 143, 0), wxPoint(149, 20), wxSize(2, 250));
+    wxColoredLine* lineBlue = new wxColoredLine(this->panel, wxID_ANY, wxColour(0, 0, 255), wxPoint(288, 20), wxSize(2, 250));
+    wxColoredLine* lineSeparator2 = new wxColoredLine(this->panel, wxID_ANY, wxColour(96, 96, 96), wxPoint(10, 278), wxSize(280, 2));
   };
   
   class Application : public wxApp {
