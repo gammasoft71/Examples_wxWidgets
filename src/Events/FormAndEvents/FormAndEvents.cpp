9e383c6328b36a8670b0a978e241e643e2f07322
Gammasoft <gammasoft71@gmail.com>
2019-08-06 19:28:22 +0200

    Update FormAndEvents.cpp

--- src/Events/FormAndEvents/FormAndEvents.cpp
+++ src/Events/FormAndEvents/FormAndEvents.cpp
@@ -7,18 +7,18 @@ public:
   
   bool ProcessEvent (wxEvent &event) override {
     wxWindow* window = ((wxWindow*)event.GetEventObject());
-    if (event.GetEventType() == wxEVT_ACTIVATE_APP) wxMessageOutputDebug.Prinf("WM_ACTIVATEAPP");
-    if (event.GetEventType() == wxEVT_CLOSE_WINDOW) wxMessageOutputDebug.Prinf("WM_CLOSE");
-    if (event.GetEventType() == wxEVT_CREATE) wxMessageOutputDebug.Prinf("WM_CREATE");
-    if (event.GetEventType() == wxEVT_DESTROY) wxMessageOutputDebug.Prinf("WM_DESTROY");
-    //if (event.GetEventType() == wxEVT_ENABLE) wxMessageOutputDebug.Prinf("WM_ENABLE");
-    if (event.GetEventType() == wxEVT_COMMAND_ENTER) wxMessageOutputDebug.Prinf("WM_COMMAND");
-    //if (event.GetEventType() == wxEVT_ENTER_SIZEMOVE) wxMessageOutputDebug.Prinf("WM_ENTERSIZEMOVE");
-    //if (event.GetEventType() == wxEVT_EXIT_SIZEMOVE) wxMessageOutputDebug.Prinf("WM_EXITSIZEMOVE");
+    if (event.GetEventType() == wxEVT_ACTIVATE_APP) wxMessageOutputDebug.Output("WM_ACTIVATEAPP");
+    if (event.GetEventType() == wxEVT_CLOSE_WINDOW) wxMessageOutputDebug.Output("WM_CLOSE");
+    if (event.GetEventType() == wxEVT_CREATE) wxMessageOutputDebug.Output("WM_CREATE");
+    if (event.GetEventType() == wxEVT_DESTROY) wxMessageOutputDebug.Output("WM_DESTROY");
+    //if (event.GetEventType() == wxEVT_ENABLE) wxMessageOutputDebug.Output("WM_ENABLE");
+    if (event.GetEventType() == wxEVT_COMMAND_ENTER) wxMessageOutputDebug.Output("WM_COMMAND");
+    //if (event.GetEventType() == wxEVT_ENTER_SIZEMOVE) wxMessageOutputDebug.Output("WM_ENTERSIZEMOVE");
+    //if (event.GetEventType() == wxEVT_EXIT_SIZEMOVE) wxMessageOutputDebug.Output("WM_EXITSIZEMOVE");
     if (event.GetEventType() == wxEVT_MOVE) wxMessageOutputDebug.Prinf("WM_MOVE {x = %d, y= %d}", window->GetPosition().x, window->GetPosition().y);
-    if (event.GetEventType() == wxEVT_NULL) wxMessageOutputDebug.Prinf("WM_MNULL");
-    //if (event.GetEventType() == wxEVT_QUIT) wxMessageOutputDebug.Prinf("WM_QUIT");
-    if (event.GetEventType() == wxEVT_TEXT) wxMessageOutputDebug.Prinf("WM_SETTEXT");
+    if (event.GetEventType() == wxEVT_NULL) wxMessageOutputDebug.Output("WM_MNULL");
+    //if (event.GetEventType() == wxEVT_QUIT) wxMessageOutputDebug.Output("WM_QUIT");
+    if (event.GetEventType() == wxEVT_TEXT) wxMessageOutputDebug.Output("WM_SETTEXT");
     if (event.GetEventType() == wxEVT_SHOW) wxMessageOutputDebug.Prinf("WM_SHOWWINDOW show = %d", window->IsShown());
     if (event.GetEventType() == wxEVT_SIZE) wxMessageOutputDebug.Prinf("WM_SIZE {width = %d, height= %d}", window->GetSize().GetWidth(), window->GetSize().GetHeight());
 
