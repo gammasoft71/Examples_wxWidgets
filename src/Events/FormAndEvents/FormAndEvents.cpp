ce806d98720634695f010124602fd4448788eb23
gammasoft71 <gammasoft71@gmail.com>
2019-08-15 21:55:52 +0200

    Update FormAndEvents.cpp

--- src/Events/FormAndEvents/FormAndEvents.cpp
+++ src/Events/FormAndEvents/FormAndEvents.cpp
@@ -15,12 +15,12 @@ public:
     if (event.GetEventType() == wxEVT_COMMAND_ENTER) wxMessageOutputDebug().Output("WM_COMMAND");
     //if (event.GetEventType() == wxEVT_ENTER_SIZEMOVE) wxMessageOutputDebug().Output("WM_ENTERSIZEMOVE");
     //if (event.GetEventType() == wxEVT_EXIT_SIZEMOVE) wxMessageOutputDebug().Output("WM_EXITSIZEMOVE");
-    if (event.GetEventType() == wxEVT_MOVE) wxMessageOutputDebug().Prinf("WM_MOVE {x = %d, y= %d}", window->GetPosition().x, window->GetPosition().y);
+    if (event.GetEventType() == wxEVT_MOVE) wxMessageOutputDebug().Printf("WM_MOVE {x = %d, y= %d}", window->GetPosition().x, window->GetPosition().y);
     if (event.GetEventType() == wxEVT_NULL) wxMessageOutputDebug().Output("WM_MNULL");
     //if (event.GetEventType() == wxEVT_QUIT) wxMessageOutputDebug().Output("WM_QUIT");
     if (event.GetEventType() == wxEVT_TEXT) wxMessageOutputDebug().Output("WM_SETTEXT");
-    if (event.GetEventType() == wxEVT_SHOW) wxMessageOutputDebug().Prinf("WM_SHOWWINDOW show = %d", window->IsShown());
-    if (event.GetEventType() == wxEVT_SIZE) wxMessageOutputDebug().Prinf("WM_SIZE {width = %d, height= %d}", window->GetSize().GetWidth(), window->GetSize().GetHeight());
+    if (event.GetEventType() == wxEVT_SHOW) wxMessageOutputDebug().Printf("WM_SHOWWINDOW show = %d", window->IsShown());
+    if (event.GetEventType() == wxEVT_SIZE) wxMessageOutputDebug().Printf("WM_SIZE {width = %d, height= %d}", window->GetSize().GetWidth(), window->GetSize().GetHeight());
 
     return this->wxFrame::ProcessEvent(event);
   }
