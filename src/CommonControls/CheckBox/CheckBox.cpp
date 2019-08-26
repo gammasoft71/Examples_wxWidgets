9d2cad6675d112c765eeaec9358f9040075613c2
Gammasoft71 <gammasoft71@gmail.com>
2019-08-26 14:29:12 +0200

    Update CheckBox.cpp

--- src/CommonControls/CheckBox/CheckBox.cpp
+++ src/CommonControls/CheckBox/CheckBox.cpp
@@ -1,14 +1,16 @@
 #include <wx/wx.h>
+#include <wx/tglbtn.h>
 
 class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "CheckBox example", wxDefaultPosition, wxSize(300, 300)) {
-    this->checkBox1->Set3StateValue(wxCHK_CHECKED);
-    this->checkBox2->Set3StateValue(wxCHK_UNCHECKED);
+    this->checkBox1->SetValue(false);
+    this->checkBox2->SetValue(true);
     this->checkBox3->Set3StateValue(wxCHK_UNDETERMINED);
     this->checkBox3->Bind(wxEVT_CHECKBOX, [&](wxCommandEvent& event) {
       this->label1->SetLabel(wxString::Format("checkBox3 state = %s", ToString(this->checkBox3->Get3StateValue())));
     });
+    this->checkBox4->SetValue(true);
 
     this->label1->SetLabel(wxString::Format("checkBox3 state = %s", ToString(this->checkBox3->Get3StateValue())));
   }
@@ -24,10 +26,11 @@ private:
   }
   
   wxPanel* panel = new wxPanel(this);
-  wxCheckBox* checkBox1 = new wxCheckBox(this->panel, wxID_ANY, "Checked", wxPoint(30, 30), wxDefaultSize, wxCHK_3STATE);
-  wxCheckBox* checkBox2 = new wxCheckBox(this->panel, wxID_ANY, "Unchecked", wxPoint(30, 60), wxDefaultSize, wxCHK_3STATE);
+  wxCheckBox* checkBox1 = new wxCheckBox(this->panel, wxID_ANY, "Unchecked", wxPoint(30, 30), wxDefaultSize, wxCHK_3STATE);
+  wxCheckBox* checkBox2 = new wxCheckBox(this->panel, wxID_ANY, "Checked", wxPoint(30, 60), wxDefaultSize, wxCHK_3STATE);
   wxCheckBox* checkBox3 = new wxCheckBox(this->panel, wxID_ANY, "Indeterminate", wxPoint(30, 90), wxDefaultSize, wxCHK_3STATE);
-  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, wxEmptyString, wxPoint(30, 150));
+  wxToggleButton* checkBox4 = new wxToggleButton(this->panel, wxID_ANY, "Button", wxPoint(30, 120), wxDefaultSize);
+  wxStaticText* label1 = new wxStaticText(this->panel, wxID_ANY, wxEmptyString, wxPoint(30, 180));
 };
 
 class Application : public wxApp {
