388ca1a9eb99fb35c05756141e496138c41e79ea
Gammasoft71 <gammasoft71@gmail.com>
2019-08-26 14:28:30 +0200

    Update ListBox.cpp

--- src/CommonControls/ListBox/ListBox.cpp
+++ src/CommonControls/ListBox/ListBox.cpp
@@ -8,14 +8,14 @@ public:
     this->listBoxLeft->SetSize(150, 200);
     this->listBoxLeft->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
       this->listBoxRight->Append(this->listBoxLeft->GetStringSelection());
-      this->listBoxRight->SetSelection(this->listBoxRight->GetCount() - 1);
+      this->listBoxRight->SetSelection(0);
       this->listBoxLeft->Delete(this->listBoxLeft->GetSelection());
     });
 
     this->listBoxRight->SetSize(150, 200);
     this->listBoxRight->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
       this->listBoxLeft->Append(this->listBoxRight->GetStringSelection());
-      this->listBoxLeft->SetSelection(this->listBoxLeft->GetCount() - 1);
+      this->listBoxLeft->SetSelection(0);
       this->listBoxRight->Delete(this->listBoxRight->GetSelection());
     });
     
@@ -27,7 +27,7 @@ public:
 private:
   wxPanel* panel = new wxPanel(this);
   wxListBox* listBoxLeft = new wxListBox(this->panel, wxID_ANY, wxPoint(20, 20));
-  wxListBox* listBoxRight = new wxListBox(this->panel, wxID_ANY, wxPoint(190, 20));
+  wxListBox* listBoxRight = new wxListBox(this->panel, wxID_ANY, wxPoint(190, 20), wxDefaultSize, 0, nullptr, wxLB_SORT);
 };
 
 class Application : public wxApp {
