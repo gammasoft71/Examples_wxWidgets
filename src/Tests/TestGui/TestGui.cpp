ff959c48c02a540acb056798a36084904f79d566
Gammasoft71 <gammasoft71@gmail.com>
2019-08-31 14:09:39 +0200

    Update TestGui.cpp

--- src/Tests/TestGui/TestGui.cpp
+++ src/Tests/TestGui/TestGui.cpp
@@ -6,47 +6,21 @@
 #include <wx/menu.h>
 #include <wx/panel.h>
 #include <wx/settings.h>
+#include <wx/dcscreen.h>
 
 class Form1 : public wxFrame {
 public:
-  Form1() : wxFrame(nullptr, wxID_ANY, "Test gui", wxDefaultPosition, wxDefaultSize) {
+  Form1() : wxFrame(nullptr, wxID_ANY, "Test gui") {
     this->SetBackgroundColour(this->panel1->GetBackgroundColour());
     this->SetForegroundColour(this->panel1->GetForegroundColour());
     this->panel1->Hide();
-
-    wxFont sysFont = wxSystemSettings::GetFont(wxSystemFont::wxSYS_DEFAULT_GUI_FONT);
-    sysFont.SetPointSize(sysFont.GetPointSize() * 4.0 / 3.0);
-
-    this->button1->SetFont(wxSystemSettings::GetFont(wxSystemFont::wxSYS_DEFAULT_GUI_FONT));
-    this->button3->SetFont(wxFont(wxOSXSystemFont::wxOSX_SYSTEM_FONT_VIEWS));
-    this->button4->SetFont(sysFont);
-    
   }
 private:
   wxPanel* panel1 = new wxPanel(this);
-  wxButton* button1 = new wxButton(this, wxID_ANY, "Button", { 10, 10 });
-  wxButton* button2 = new wxButton(this, wxID_ANY, "Button", { 10, 40 });
-  wxButton* button3 = new wxButton(this, wxID_ANY, "Button", { 10, 70 });
-  wxButton* button4 = new wxButton(this, wxID_ANY, "Button", { 10, 100 });
 };
 
 class Application : public wxApp {
-  bool OnInit() override {
-    wxMenu* menuWindow = new wxMenu();
-    wxMenuItem* aboutMenuItem = new wxMenuItem(menuWindow, wxID_ANY, "About");
-
-    wxMenuBar* menubar = new wxMenuBar();
-    menubar->Append(menuWindow, "Window");
-    menubar->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
-      if (event.GetId() == wxID_ABOUT) wxAboutBox(wxAboutDialogInfo());
-      else event.Skip();
-    });
-
-    wxApp::s_macAboutMenuItemId = aboutMenuItem->GetId();
-    wxApp::s_macWindowMenuTitleName = "Window";
-    wxMenuBar::MacSetCommonMenuBar(menubar);
-    return (new Form1())->Show();
-  }
+  bool OnInit() override {return (new Form1())->Show();}
 };
 
 wxIMPLEMENT_APP(Application);
