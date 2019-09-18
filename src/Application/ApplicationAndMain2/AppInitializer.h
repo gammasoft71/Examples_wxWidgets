547efcb4fc0dd0a04b6d4b4be46be7b843c96cc0
Gammasoft71 <gammasoft71@gmail.com>
2019-09-18 14:27:06 +0200

    fix AppInitializer on macOS

--- src/Application/ApplicationAndMain2/AppInitializer.h
+++ src/Application/ApplicationAndMain2/AppInitializer.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <memory>
 #include <wx/wx.h>
+#include <wx/aboutdlg.h>
 
 struct AppInitializer {
   AppInitializer() {
@@ -9,6 +10,31 @@ struct AppInitializer {
     int argc = 0;
     wxEntryStart(argc, (wxChar**)NULL);
     wxTheApp->CallOnInit();
+    wxTheApp->SetExitOnFrameDelete(false);
+#if __WXOSX__
+    wxMenuBar* menubar = new wxMenuBar();
+    wxMenu* menuWindow = new wxMenu();
+    wxMenuItem* aboutMenuItem = new wxMenuItem(menuWindow, wxID_ANY, "About");
+    
+    menubar->Append(menuWindow, "Window");
+    menubar->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
+      if (event.GetId() == wxID_ABOUT) wxAboutBox(wxAboutDialogInfo());
+      if (event.GetId() == wxID_EXIT) {
+        bool cancel_quit = false;
+        for (wxWindow* window : wxTopLevelWindows) {
+          cancel_quit = !window->Close();
+          if (cancel_quit) break;
+        }
+        if (!cancel_quit) wxTheApp->ExitMainLoop();
+      } else event.Skip();
+    });
+    
+    wxApp::s_macAboutMenuItemId = aboutMenuItem->GetId();
+#if wxMAJOR_VERSION >= 3 && wxMINOR_VERSION >= 1
+    wxApp::s_macWindowMenuTitleName = "Window";
+#endif
+    wxMenuBar::MacSetCommonMenuBar(menubar);
+#endif
   }
   
   ~AppInitializer() {
