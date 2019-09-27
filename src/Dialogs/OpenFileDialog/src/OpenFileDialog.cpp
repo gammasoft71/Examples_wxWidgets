36a7920db193a5a615b3c2e874644c448ab26700
Gammasoft71 <gammasoft71@gmail.com>
2019-09-27 22:08:01 +0200

    add OpenFileDialog example

---
+++ src/Dialogs/OpenFileDialog/src/OpenFileDialog.cpp
@@ -0,0 +1,25 @@
+#include <wx/wx.h>
+#include <wx/filedlg.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "ColorDialog example", wxDefaultPosition, wxSize(300, 300)) {
+    this->button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+      wxFileDialog openFileDialog(this, wxEmptyString, "", "", "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+      if (openFileDialog.ShowModal()) {
+        this->label->SetLabelText(openFileDialog.GetPath());
+      }
+    });
+  }
+
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxButton* button = new wxButton(this->panel, wxID_ANY, "Open...", wxPoint(10, 10));
+  wxStaticText* label = new wxStaticText(this->panel, wxID_ANY, "", wxPoint(10, 40));
+};
+
+class Application : public wxApp {
+  bool OnInit() override {return (new Form())->Show();}
+};
+
+wxIMPLEMENT_APP(Application);
