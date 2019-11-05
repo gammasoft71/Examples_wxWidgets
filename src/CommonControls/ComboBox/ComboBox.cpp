9a97949c6a9c4d88c743c2b2d63d44c1b79b9f65
Gammasoft71 <gammasoft71@gmail.com>
2019-11-05 11:11:06 +0100

    Update ComboBox.cpp

--- src/CommonControls/ComboBox/ComboBox.cpp
+++ src/CommonControls/ComboBox/ComboBox.cpp
@@ -6,16 +6,19 @@ public:
     comboBox1->Append("item1");
     comboBox1->Append("item2");
     comboBox1->Append("item3");
+    comboBox1->Append("item4");
     comboBox1->Select(0);
 
     comboBox2->Append("item1");
     comboBox2->Append("item2");
     comboBox2->Append("item3");
+    comboBox2->Append("item4");
     comboBox2->Select(1);
 
     comboBox3->Append("item1");
     comboBox3->Append("item2");
     comboBox3->Append("item3");
+    comboBox3->Append("item4");
     comboBox3->Select(2);
   }
   
@@ -23,7 +26,7 @@ private:
   wxPanel* panel = new wxPanel(this);
   wxComboBox* comboBox1 = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(10, 10), wxDefaultSize);
   wxComboBox* comboBox2 = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(10, 50), wxDefaultSize, 0, nullptr, wxCB_READONLY);
-  wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(10, 90), wxSize(121, 150), 0, nullptr, wxCB_SIMPLE);
+  wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, wxPoint(10, 90), wxSize(110, 150), 0, nullptr, wxCB_SIMPLE);
 };
 
 class Application : public wxApp {
