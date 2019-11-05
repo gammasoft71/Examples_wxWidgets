f29eb8e868d4d51575000ee6d96f928021c4cc15
Gammasoft71 <gammasoft71@gmail.com>
2019-11-05 11:02:21 +0100

    Update TestGui.cpp

--- src/Tests/TestGui/TestGui.cpp
+++ src/Tests/TestGui/TestGui.cpp
@@ -2,67 +2,24 @@
 #include <wx/wx.h>
 #include "AppInitializer.h"
 
-class StdInputStreamAdapter : public wxInputStream {
-public:
-  StdInputStreamAdapter(std::istream& stream): stream_{stream} {}
-
-protected:
-  bool IsSeekable()  const override {return true;}
-  
-  size_t OnSysRead(void *buffer, size_t bufsize) override {
-    stream_.peek();
-    if (stream_.fail() || stream_.bad()) m_lasterror = wxSTREAM_READ_ERROR;
-    else if (stream_.eof()) m_lasterror = wxSTREAM_EOF;
-    else return stream_.readsome(static_cast<std::istream::char_type *>(buffer), bufsize);
-    return 0;
-  }
-  
-  wxFileOffset OnSysSeek(wxFileOffset pos, wxSeekMode mode) override {
-    switch (mode) {
-      case wxFromStart: stream_.seekg(pos, std::ios::beg); break;
-      case wxFromEnd: stream_.seekg(pos, std::ios::end); break;
-      case wxFromCurrent: stream_.seekg(pos, std::ios::cur); break;
-    }
-    return stream_.tellg();
-  }
-    
-  wxFileOffset OnSysTell() const override {return stream_.tellg();}
-
-private:
-  std::istream &stream_;
-};
-
 int main(int argc, char* argv[]) {
   AppInitializer appInitializer;
-  wxFrame* form1 = new wxFrame(nullptr, wxID_ANY, "TestGui");
+  wxFrame* form1 = new wxFrame(nullptr, wxID_ANY, "TestGui", wxDefaultPosition, {300, 300});
 
-  /*
-  wxImage* image = new wxImage(190, 190);
-  
-  for (int x = 10; x < 90; x++)
-    for (int y = 10; y < 90; y++)
-      image->SetRGB(x, y, 255, 0, 0);
-  
-  for (int x = 100; x < 180; x++)
-    for (int y = 10; y < 90; y++)
-      image->SetRGB(x, y, 00, 128, 0);
-  
-  for (int x = 10; x < 90; x++)
-    for (int y = 100; y < 180; y++)
-      image->SetRGB(x, y, 0, 0, 255);
-  
-  for (int x = 100; x < 180; x++)
-    for (int y = 100; y < 180; y++)
-      image->SetRGB(x, y, 255, 255, 0);
-   */
- // wxImage* image = new wxImage("/Users/yves/Desktop/test.png");
-
-  std::fstream stream("/Users/yves/Desktop/test.png", std::ios::in | std::ios::binary);
-  StdInputStreamAdapter std_stream(stream);
-  wxImage* image = new wxImage(std_stream);
-
-  wxStaticBitmap* pictureBox1 = new wxStaticBitmap(form1, wxID_ANY, wxBitmap(*image), wxPoint(20,10), form1->GetClientSize());
+  wxScrolled<wxPanel>* panel = new wxScrolled<wxPanel>(form1, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
+  //wxPanel* panel = new wxPanel(form1, wxID_ANY, wxDefaultPosition, wxDefaultSize, 0);
 
   form1->Show();
+
+  //panel->ShowScrollbars(wxScrollbarVisibility::wxSHOW_SB_DEFAULT, wxScrollbarVisibility::wxSHOW_SB_DEFAULT);
+  //panel->SetVirtualSize({0, 0});
+  panel->SetVirtualSize({450, 375});
+  panel->SetScrollRate(10, 10);
+  
+  wxPanel* panel1 = new wxPanel(panel, wxID_ANY, {350, 350}, {200, 100});
+  panel1->SetBackgroundColour(wxColour(0, 128, 0));
+  
+  //panel->Scroll(30, 0);
+  
   wxTheApp->OnRun();
 }
