361d07a5a2a83b99f6ee8dfa9a883c3ce92182f0
Gammasoft71 <gammasoft71@gmail.com>
2019-11-09 12:11:55 +0100

    Update ColoredTabPages.cpp

--- src/Others/ColoredTabPages/ColoredTabPages.cpp
+++ src/Others/ColoredTabPages/ColoredTabPages.cpp
@@ -5,26 +5,27 @@ class Form : public wxFrame {
 public:
   Form() : wxFrame(nullptr, wxID_ANY, "Colored TabPages example", wxDefaultPosition) {
     SetClientSize(wxSize(390, 270));
-    tabControl1->AddPage(tabPageRed, "Red");
-    tabPageRed->SetBackgroundColour(wxTheColourDatabase->Find("Red"));
+    
+    tabControl1->AddPage(tabPageLightPink, "Light red");
+    tabPageLightPink->SetBackgroundColour(wxColour(0xFF, 0xB6, 0xC1, 0xFF));
 
-    tabControl1->AddPage(tabPageGreen, "Green");
-    tabPageGreen->SetBackgroundColour(wxColour(0, 0x80, 0));
+    tabControl1->AddPage(tabPageLightGreen, "Light green");
+    tabPageLightGreen->SetBackgroundColour(wxColour(0x90, 0xEE, 0x90, 0xFF));
 
-    tabControl1->AddPage(tabPageBlue, "Blue");
-    tabPageBlue->SetBackgroundColour(wxTheColourDatabase->Find("Blue"));
+    tabControl1->AddPage(tabPageLightBlue, "Light blue");
+    tabPageLightBlue->SetBackgroundColour(wxColour(0xAD, 0xD8, 0xE6, 0xFF));
 
-    tabControl1->AddPage(tabPageYellow, "Yellow");
-    tabPageYellow->SetBackgroundColour(wxTheColourDatabase->Find("Yellow"));
+    tabControl1->AddPage(tabPageLightYellow, "Light yellow");
+    tabPageLightYellow->SetBackgroundColour(wxColour(0xFF, 0xFF, 0xE0, 0xFF));
   }
 
 private:
   wxPanel* panel = new wxPanel(this);
-  wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, wxPoint(10, 10), wxSize(370, 250));
-  wxNotebookPage* tabPageRed = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageGreen = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageBlue = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageYellow = new wxNotebookPage(tabControl1, wxID_ANY);
+  wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, wxPoint(10, 10), wxSize(370, 250), wxNB_BOTTOM);
+  wxNotebookPage* tabPageLightPink = new wxNotebookPage(tabControl1, wxID_ANY);
+  wxNotebookPage* tabPageLightGreen = new wxNotebookPage(tabControl1, wxID_ANY);
+  wxNotebookPage* tabPageLightBlue = new wxNotebookPage(tabControl1, wxID_ANY);
+  wxNotebookPage* tabPageLightYellow = new wxNotebookPage(tabControl1, wxID_ANY);
 };
 
 class Application : public wxApp {
