d990f5abe6664d93ad18cecac3929bd2b7c5aa77
Gammasoft71 <gammasoft71@gmail.com>
2019-11-10 21:20:12 +0100

    add ColorPicker example

---
+++ src/PickerControls/ColorPicker/ColorPicker.cpp
@@ -0,0 +1,23 @@
+#include <wx/wx.h>
+#include <wx/clrpicker.h>
+
+class Form : public wxFrame {
+public:
+  Form() : wxFrame(nullptr, wxID_ANY, "ColorPicker example", wxDefaultPosition, wxSize(300, 300)) {
+    picker->Bind(wxEVT_COLOURPICKER_CHANGED, [this](wxColourPickerEvent& event) {
+      test_zone->SetBackgroundColour(event.GetColour());
+      test_zone->Refresh();
+    });
+  }
+  
+private:
+  wxPanel* panel = new wxPanel(this);
+  wxPanel* test_zone = new wxPanel(panel, wxID_ANY, {10, 50}, {200, 100}, wxTAB_TRAVERSAL | wxBORDER_SIMPLE);
+  wxColourPickerCtrl* picker = new wxColourPickerCtrl(panel, wxID_ANY, test_zone->GetBackgroundColour(), {10, 10});
+};
+
+class Application : public wxApp {
+  bool OnInit() override {(new Form())->Show(); return true;}
+};
+
+wxIMPLEMENT_APP(Application);
