94570474f7370008574ba90d1dc4ed0aaf836318
Gammasoft71 <gammasoft71@gmail.com>
2019-11-12 16:50:58 +0100

    Update AboutDialog.cpp

--- src/OtherDialogs/AboutDialog/AboutDialog.cpp
+++ src/OtherDialogs/AboutDialog/AboutDialog.cpp
@@ -2,15 +2,16 @@
 #include <wx/aboutdlg.h>
 
 class Frame : public wxFrame {
+  #include "Gammasoft.xpm"
 public:
   Frame() : wxFrame(nullptr, wxID_ANY, "About example", wxDefaultPosition, wxSize(300, 300)) {
+    SetIcon(gammasoft_32x32);
     button->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
       wxAboutDialogInfo aboutInfo;
       aboutInfo.SetName(wxTheApp->GetAppName());
       aboutInfo.SetDescription("AboutBox example demonstrates how to use wxAboutBox.");
       aboutInfo.SetVersion("1.0", "1.0.0");
       aboutInfo.SetCopyright("Copyright © Gammasoft 219");
-      aboutInfo.SetWebSite("https://gammasoft71.wixsite.com/gammasoft");
       wxAboutBox(aboutInfo);
     });
   }
