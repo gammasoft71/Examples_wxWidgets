f86eac788ad71d9fe04d3113488f131f7e9c7c26
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:19:40 +0100

    Update AboutBox.cpp

--- src/CommonDialogs/AboutBox/AboutBox.cpp
+++ src/CommonDialogs/AboutBox/AboutBox.cpp
@@ -1,27 +1,31 @@
 #include <wx/wx.h>
 #include <wx/aboutdlg.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "AboutBox example", wxDefaultPosition, {300, 300}) {
-    wxInitAllImageHandlers();
-    button->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
-      wxAboutDialogInfo aboutInfo;
-      aboutInfo.SetName(wxTheApp->GetAppName());
-      aboutInfo.SetDescription("About dialog description.");
-      aboutInfo.SetVersion("1.0", "1.0.0");
-      aboutInfo.SetCopyright(u8"Copyright \u00A9 2019 Gammasoft.\nAll rights reserved.");
-      wxAboutBox(aboutInfo);
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ABOUT, wxEmptyString, {10, 10});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "AboutBox example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
+        wxAboutDialogInfo aboutInfo;
+        aboutInfo.SetName(wxTheApp->GetAppName());
+        aboutInfo.SetDescription("About dialog description.");
+        aboutInfo.SetVersion("1.0", "1.0.0");
+        aboutInfo.SetCopyright(u8"Copyright \u00A9 2019 Gammasoft.\nAll rights reserved.");
+        wxAboutBox(aboutInfo);
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ABOUT, wxEmptyString, {10, 10});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
