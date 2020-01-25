56eb659d20d39b7039c8402a2fd569e9e135743c
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:19:58 +0100

    Update Control.cpp

--- src/CommonControls/Control/Control.cpp
+++ src/CommonControls/Control/Control.cpp
@@ -3,23 +3,28 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Control example", wxDefaultPosition, {300, 300}) {
-    control1->SetBackgroundColour(wxTheColourDatabase->Find("Spring Green"));
-    control1->Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent& event) {
-      control1->SetBackgroundColour(control1->GetBackgroundColour() == wxTheColourDatabase->Find("Spring Green") ? wxTheColourDatabase->Find("Orange Red") : wxTheColourDatabase->Find("Spring Green"));
-      control1->Refresh();
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxControl* control1 = new wxControl(panel, wxID_ANY, {50, 50}, {100, 50});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Control example", wxDefaultPosition, {300, 300}) {
+      control1->SetBackgroundColour(wxTheColourDatabase->Find("Spring Green"));
+      control1->Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent& event) {
+        control1->SetBackgroundColour(control1->GetBackgroundColour() == wxTheColourDatabase->Find("Spring Green") ? wxTheColourDatabase->Find("Orange Red") : wxTheColourDatabase->Find("Spring Green"));
+        control1->Refresh();
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxControl* control1 = new wxControl(panel, wxID_ANY, {50, 50}, {100, 50});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
