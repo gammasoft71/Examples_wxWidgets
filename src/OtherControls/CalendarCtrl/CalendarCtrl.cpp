bb5f34180e1226244bff1827711d604439303374
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:32:53 +0100

    Update CalendarCtrl.cpp

--- src/OtherControls/CalendarCtrl/CalendarCtrl.cpp
+++ src/OtherControls/CalendarCtrl/CalendarCtrl.cpp
@@ -1,21 +1,29 @@
+#include <chrono>
 #include <wx/app.h>
 #include <wx/calctrl.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "MonthCalendar example", wxDefaultPosition, {300, 300}) {
-    calendarCtrl1->SetDateRange(wxDateTime(5, wxDateTime::Jan, 1971), wxDateTime(5, wxDateTime::Jan, 2071));
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxCalendarCtrl* calendarCtrl1 = new wxCalendarCtrl(panel, wxID_ANY, wxDateTime::Now(), {10, 10});
-};
+using namespace std::chrono;
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "MonthCalendar example", wxDefaultPosition, {300, 300}) {
+      calendarCtrl1->SetDateRange({5, wxDateTime::Jan, 1971}, {5, wxDateTime::Jan, 2071});
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxCalendarCtrl* calendarCtrl1 = new wxCalendarCtrl(panel, wxID_ANY, {system_clock::to_time_t(system_clock::now())}, {10, 10});
+  };
 
-wxIMPLEMENT_APP(Application);
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
