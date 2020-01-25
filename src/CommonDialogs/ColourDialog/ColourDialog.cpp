e991697d52139f8de1cbeb77a30df6535211dfb5
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:21:00 +0100

    Update ColourDialog.cpp

--- src/CommonDialogs/ColourDialog/ColourDialog.cpp
+++ src/CommonDialogs/ColourDialog/ColourDialog.cpp
@@ -1,27 +1,32 @@
 #include <wx/wx.h>
 #include <wx/colordlg.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ColourDialog example", wxDefaultPosition, {300, 300}) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxColourData colourData;
-      colourData.SetColour(GetBackgroundColour());
-      wxColourDialog colourDialog(this, &colourData);
-      if (colourDialog.ShowModal()) {
-        SetBackgroundColour(colourDialog.GetColourData().GetColour());
-        Refresh();
-      }
-    });
-  }
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ColourDialog example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxColourData colourData;
+        colourData.SetColour(GetBackgroundColour());
+        wxColourDialog colourDialog(this, &colourData);
+        if (colourDialog.ShowModal()) {
+          SetBackgroundColour(colourDialog.GetColourData().GetColour());
+          Refresh();
+        }
+      });
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Color...", {10, 10});
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Color...", {10, 10});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
