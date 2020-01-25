630ea6f686aca9bb266f07ceb691e0d9d34b000e
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:23:25 +0100

    Update FilePickerCtrl.cpp

--- src/PickerControls/FilePickerCtrl/FilePickerCtrl.cpp
+++ src/PickerControls/FilePickerCtrl/FilePickerCtrl.cpp
@@ -1,22 +1,27 @@
 #include <wx/wx.h>
 #include <wx/filepicker.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "FilePickerCtrl example", wxDefaultPosition, {300, 300}) {
-    picker->Bind(wxEVT_FILEPICKER_CHANGED, [&](wxFileDirPickerEvent& event) {
-      label->SetLabel(wxString::Format("File = %s", event.GetPath()));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxStaticText* label = new wxStaticText(panel, wxID_ANY, "File = ", wxPoint(10, 50));
-  wxFilePickerCtrl* picker = new wxFilePickerCtrl(panel, wxID_ANY, wxEmptyString, wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", {10, 10}, wxDefaultSize, wxFLP_DEFAULT_STYLE|wxFLP_SMALL);
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "FilePickerCtrl example", wxDefaultPosition, {300, 300}) {
+      picker->Bind(wxEVT_FILEPICKER_CHANGED, [&](wxFileDirPickerEvent& event) {
+        label->SetLabel(wxString::Format("File = %s", event.GetPath()));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "File = ", wxPoint(10, 50));
+    wxFilePickerCtrl* picker = new wxFilePickerCtrl(panel, wxID_ANY, wxEmptyString, wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", {10, 10}, wxDefaultSize, wxFLP_DEFAULT_STYLE|wxFLP_SMALL);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
