fced5f8958ed1393b6b6719c8cf6d5f6095c6814
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:12:17 +0100

    Update TreeCtrl.cpp

--- src/CommonControls/TreeCtrl/TreeCtrl.cpp
+++ src/CommonControls/TreeCtrl/TreeCtrl.cpp
@@ -1,27 +1,29 @@
 #include <wx/wx.h>
 #include <wx/treectrl.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "TreeCtrl example", wxDefaultPosition, {300, 300}) {
-    wxTreeItemId rootId = treeCtrl->AddRoot("Root");
-    wxTreeItemId firstId = treeCtrl->AppendItem(rootId, "First");
-    treeCtrl->AppendItem(firstId, "Second");
-    treeCtrl->AppendItem(firstId, "Third");
-    wxTreeItemId fourthId = treeCtrl->AppendItem(rootId, "Fourth");
-    treeCtrl->AppendItem(fourthId, "Fifth");
-    treeCtrl->AppendItem(fourthId, "Sixth");
-    treeCtrl->AppendItem(fourthId, "Seventh");
-    treeCtrl->ExpandAll();
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxTreeCtrl* treeCtrl = new wxTreeCtrl(panel, wxID_ANY, {10, 10}, {150, 200});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "TreeCtrl example", wxDefaultPosition, {300, 300}) {
+      auto rootId = treeCtrl->AddRoot("Root");
+      auto firstId = treeCtrl->AppendItem(rootId, "First");
+      treeCtrl->AppendItem(firstId, "Second");
+      treeCtrl->AppendItem(firstId, "Third");
+      auto fourthId = treeCtrl->AppendItem(rootId, "Fourth");
+      treeCtrl->AppendItem(fourthId, "Fifth");
+      treeCtrl->AppendItem(fourthId, "Sixth");
+      treeCtrl->AppendItem(fourthId, "Seventh");
+      treeCtrl->ExpandAll();
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxTreeCtrl* treeCtrl = new wxTreeCtrl(panel, wxID_ANY, {10, 10}, {150, 200});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {(new Frame())->Show(); return true;}
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
