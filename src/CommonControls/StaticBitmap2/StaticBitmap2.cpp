4a199d3ccc01c85bf71d8e31bab09cb2bebffc4e
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:57:06 +0100

    Update StaticBitmap2.cpp

--- src/CommonControls/StaticBitmap2/StaticBitmap2.cpp
+++ src/CommonControls/StaticBitmap2/StaticBitmap2.cpp
@@ -3,22 +3,27 @@
 #include <wx/statbmp.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "StaticBitmap2 example") {
-    SetClientSize({300, 300});
-    wxInitAllImageHandlers();
-    staticBitmap1->SetBitmap({"Resources/Logo.png", wxBITMAP_TYPE_ANY});
-    staticBitmap1->SetWindowStyle(wxBORDER_SIMPLE);
-  }
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "StaticBitmap2 example") {
+      SetClientSize(300, 300);
+      wxInitAllImageHandlers();
+      staticBitmap1->SetBitmap({"Resources/Logo.png", wxBITMAP_TYPE_ANY});
+      staticBitmap1->SetWindowStyle(wxBORDER_SIMPLE);
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxStaticBitmap* staticBitmap1 = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap, {20, 20}, {100, 50});
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxStaticBitmap* staticBitmap1 = new wxStaticBitmap(this, wxID_ANY, wxNullBitmap, {20, 20}, {100, 50});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
