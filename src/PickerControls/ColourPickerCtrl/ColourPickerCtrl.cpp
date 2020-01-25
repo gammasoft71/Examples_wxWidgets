afbf1761ff9be48ddc9dc0b616fcc21e7ff47a22
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:20:32 +0100

    Update ColourPickerCtrl.cpp

--- src/PickerControls/ColourPickerCtrl/ColourPickerCtrl.cpp
+++ src/PickerControls/ColourPickerCtrl/ColourPickerCtrl.cpp
@@ -1,25 +1,30 @@
 #include <wx/wx.h>
 #include <wx/clrpicker.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ColourPicker example", wxDefaultPosition, {300, 300}) {
-    testZone->SetBackgroundColour(wxTheColourDatabase->Find("Red"));
-    picker->SetColour(testZone->GetBackgroundColour());
-    picker->Bind(wxEVT_COLOURPICKER_CHANGED, [&](wxColourPickerEvent& event) {
-      testZone->SetBackgroundColour(event.GetColour());
-      testZone->Refresh();
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxPanel* testZone = new wxPanel(panel, wxID_ANY, {10, 50}, {200, 100}, wxTAB_TRAVERSAL | wxBORDER_SUNKEN);
-  wxColourPickerCtrl* picker = new wxColourPickerCtrl(panel, wxID_ANY, wxColour(0, 0, 0), {10, 10}, {100, 25});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ColourPicker example", wxDefaultPosition, {300, 300}) {
+      testZone->SetBackgroundColour(wxTheColourDatabase->Find("Red"));
+      picker->SetColour(testZone->GetBackgroundColour());
+      picker->Bind(wxEVT_COLOURPICKER_CHANGED, [&](wxColourPickerEvent& event) {
+        testZone->SetBackgroundColour(event.GetColour());
+        testZone->Refresh();
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxPanel* testZone = new wxPanel(panel, wxID_ANY, {10, 50}, {200, 100}, wxTAB_TRAVERSAL | wxBORDER_SUNKEN);
+    wxColourPickerCtrl* picker = new wxColourPickerCtrl(panel, wxID_ANY, wxColour(0, 0, 0), {10, 10}, {100, 25});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
