55a0363612d776ccc75c94703d17ea12f5ca7566
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 17:55:55 +0100

    Update Application.cpp

--- src/Application/Application/Application.cpp
+++ src/Application/Application/Application.cpp
@@ -1,7 +1,12 @@
 #include <wx/wx.h>
 
-class Application : public wxApp {
-  bool OnInit() override {(new wxFrame(nullptr, wxID_ANY, wxEmptyString))->Show(); return true;}
-};
+namespace Examples {
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new wxFrame(nullptr, wxID_ANY, wxEmptyString))->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
