335d9d7d9588a9df741f878faf31bd0f03831bd5
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:12:33 +0100

    Update Panel.cpp

--- src/Containers/Panel/Panel.cpp
+++ src/Containers/Panel/Panel.cpp
@@ -1,19 +1,24 @@
 #include <wx/wx.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Panel example", wxDefaultPosition) {
-    SetClientSize(wxSize(640, 480));
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxPanel* panel1 = new wxPanel(panel, wxID_ANY, {10, 10}, wxSize(305, 460), wxTAB_TRAVERSAL | wxBORDER_SIMPLE);
-  wxPanel* panel2 = new wxPanel(panel, wxID_ANY, wxPoint(325, 10), wxSize(305, 460), wxTAB_TRAVERSAL | wxBORDER_THEME);
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Panel example") {
+      SetClientSize(640, 480);
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxPanel* panel1 = new wxPanel(panel, wxID_ANY, {10, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_SIMPLE);
+    wxPanel* panel2 = new wxPanel(panel, wxID_ANY, {325, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_THEME);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
