e938e7cec2241c577ec2233335c65b7170002fca
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:31:42 +0100

    Update HyperlinkCtrl.cpp

--- src/CommonControls/HyperlinkCtrl/HyperlinkCtrl.cpp
+++ src/CommonControls/HyperlinkCtrl/HyperlinkCtrl.cpp
@@ -4,20 +4,25 @@
 #include <wx/panel.h>
 #include <wx/stattext.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "HyperlinkCtrl example", wxDefaultPosition, {300, 300}) {}
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxHyperlinkCtrl* staticText1 = new wxHyperlinkCtrl(panel, wxID_ANY, "Gammasoft", "https://gammasoft71.wixsite.com/gammasoft", {10, 10});
-  wxStaticText* staticText2 = new wxStaticText(panel, wxID_ANY, " present ", {staticText1->GetPosition().x + staticText1->GetSize().GetWidth(), 10});
-  wxHyperlinkCtrl* staticText3 = new wxHyperlinkCtrl(panel, wxID_ANY, "wxWidgets", "https://gammasoft71.wixsite.com/gammasoft/wxwidgets", {staticText2->GetPosition().x + staticText2->GetSize().GetWidth(), 10});
-  wxStaticText* staticText4 = new wxStaticText(panel, wxID_ANY, " examples", {staticText3->GetPosition().x + staticText3->GetSize().GetWidth(), 10});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "HyperlinkCtrl example", wxDefaultPosition, {300, 300}) {}
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxHyperlinkCtrl* staticText1 = new wxHyperlinkCtrl(panel, wxID_ANY, "Gammasoft", "https://gammasoft71.wixsite.com/gammasoft", {10, 10});
+    wxStaticText* staticText2 = new wxStaticText(panel, wxID_ANY, " present ", {staticText1->GetPosition().x + staticText1->GetSize().GetWidth(), 10});
+    wxHyperlinkCtrl* staticText3 = new wxHyperlinkCtrl(panel, wxID_ANY, "wxWidgets", "https://gammasoft71.wixsite.com/gammasoft/wxwidgets", {staticText2->GetPosition().x + staticText2->GetSize().GetWidth(), 10});
+    wxStaticText* staticText4 = new wxStaticText(panel, wxID_ANY, " examples", {staticText3->GetPosition().x + staticText3->GetSize().GetWidth(), 10});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
