b56bb5cf5fef14c6f2c1690676a5bde9d3fbebad
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:47:52 +0100

    Update RichToolTip.cpp

--- src/CommonControls/RichToolTip/RichToolTip.cpp
+++ src/CommonControls/RichToolTip/RichToolTip.cpp
@@ -1,28 +1,33 @@
 #include <wx/wx.h>
 #include <wx/richtooltip.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "TooTip example", wxDefaultPosition, {300, 300}) {
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "TooTip example", wxDefaultPosition, {300, 300}) {
 
-    button->Bind(wxEVT_MOTION, [&](wxMouseEvent& event) {
-      wxRichToolTip toolTipButton {"ToolTip title", "button toolTip..."};
-      toolTipButton.ShowFor(button);
-    });
+      button->Bind(wxEVT_MOTION, [&](wxMouseEvent& event) {
+        wxRichToolTip toolTipButton {"ToolTip title", "button toolTip..."};
+        toolTipButton.ShowFor(button);
+      });
+      
+      check->SetToolTip("check toolTip...");
+      label->SetToolTip("label toolTip...");
+    }
     
-    check->SetToolTip("check toolTip...");
-    label->SetToolTip("label toolTip...");
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "button", {10, 10});
-  wxCheckBox* check = new wxCheckBox(panel, wxID_ANY, "check", {10, 40});
-  wxStaticText* label = new wxStaticText(panel, wxID_ANY, "label", {10, 70});
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "button", {10, 10});
+    wxCheckBox* check = new wxCheckBox(panel, wxID_ANY, "check", {10, 40});
+    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "label", {10, 70});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
