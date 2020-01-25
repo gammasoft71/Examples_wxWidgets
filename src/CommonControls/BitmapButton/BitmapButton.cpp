93c680d61b46ba01e0039283e359e49dbda16cbb
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 18:09:10 +0100

    Update BitmapButton.cpp

--- src/CommonControls/BitmapButton/BitmapButton.cpp
+++ src/CommonControls/BitmapButton/BitmapButton.cpp
@@ -7,33 +7,38 @@
 #include "gammasoft_32x32.xpm"
 #include "gammasoft_64x64.xpm"
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "BitmapButton example", wxDefaultPosition, {300, 300}) {
-    button1->SetSize(100, 25);
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "BitmapButton example", wxDefaultPosition, {300, 300}) {
+      button1->SetSize(100, 25);
+      
+      button2->SetBitmapLabel({gammasoft_16x16_xpm});
+      button2->SetBitmapPosition(wxLEFT);
+      button2->SetSize(120, 25);
+      
+      button3->SetBitmapLabel({gammasoft_32x32_xpm});
+      button3->SetBitmapPosition(wxLEFT);
+      button3->SetSize(140, 40);
+
+      button4->SetBitmapLabel({gammasoft_64x64_xpm});
+      button4->SetSize(70, 70);
+    }
     
-    button2->SetBitmapLabel({gammasoft_16x16_xpm});
-    button2->SetBitmapPosition(wxLEFT);
-    button2->SetSize(120, 25);
-    
-    button3->SetBitmapLabel({gammasoft_32x32_xpm});
-    button3->SetBitmapPosition(wxLEFT);
-    button3->SetSize(140, 40);
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button1 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 50});
+    wxButton* button2 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 80});
+    wxButton* button3 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 110});
+    wxButton* button4 = new wxButton(panel, wxID_ANY, wxEmptyString, {50, 155});
+  };
 
-    button4->SetBitmapLabel({gammasoft_64x64_xpm});
-    button4->SetSize(70, 70);
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button1 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 50});
-  wxButton* button2 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 80});
-  wxButton* button3 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 110});
-  wxButton* button4 = new wxButton(panel, wxID_ANY, wxEmptyString, {50, 155});
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
-
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
