928f6dbab5f033ca0318694c69839ba6a27292a1
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:17:04 +0100

    Update WebView.cpp

--- src/CommonControls/WebView/WebView.cpp
+++ src/CommonControls/WebView/WebView.cpp
@@ -1,23 +1,21 @@
 #include <wx/wx.h>
 #include <wx/webview.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "WebView example") {
-    Bind(wxEVT_SIZING, [&](wxSizeEvent& event) {
-      webView->SetSize(GetClientSize());
-    });
-    SetClientSize(1024, 768);
-    webView->SetSize(GetClientSize());
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxWebView* webView = wxWebView::New(panel, wxID_ANY, "https://gammasoft71.wixsite.com/gammasoft");
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "WebView example", wxDefaultPosition, {1024, 768}) {}
+    
+  private:
+    wxWebView* webView = wxWebView::New(this, wxID_ANY, "https://gammasoft71.wixsite.com/gammasoft");
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
