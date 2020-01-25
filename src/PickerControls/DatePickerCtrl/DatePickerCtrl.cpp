a15bfa1477191c1ec5d4e7ac17453eb4b597103d
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 17:52:38 +0100

    Update DatePickerCtrl.cpp

--- src/PickerControls/DatePickerCtrl/DatePickerCtrl.cpp
+++ src/PickerControls/DatePickerCtrl/DatePickerCtrl.cpp
@@ -1,12 +1,15 @@
+#include <chrono>
 #include <wx/wx.h>
 #include <wx/datectrl.h>
 #include <wx/dateevt.h>
 
+using namespace std::chrono;
+
 class Frame : public wxFrame {
 public:
   Frame() : wxFrame(nullptr, wxID_ANY, "DatePickerCtrl example", wxDefaultPosition, {300, 300}) {
-    datePicker1->SetRange(wxDateTime(4, wxDateTime::Apr, 1975), wxDateTime::Now());
-    datePicker1->SetValue(wxDateTime(4, wxDateTime::Apr, 1975));
+    datePicker1->SetRange({4, wxDateTime::Apr, 1975}, {system_clock::to_time_t(system_clock::now())});
+    datePicker1->SetValue({4, wxDateTime::Apr, 1975});
     datePicker1->Bind(wxEVT_DATE_CHANGED, [&](wxDateEvent& event) {
       staticText1->SetLabelText(datePicker1->GetValue().FormatDate());
     });
@@ -15,8 +18,8 @@ public:
   
 private:
   wxPanel* panel = new wxPanel(this);
-  wxDatePickerCtrl* datePicker1 = new wxDatePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, wxPoint(30, 30), wxDefaultSize, wxDP_DROPDOWN | wxDP_SHOWCENTURY);
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, wxPoint(30, 70));
+  wxDatePickerCtrl* datePicker1 = new wxDatePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, {30, 30}, wxDefaultSize, wxDP_DROPDOWN | wxDP_SHOWCENTURY);
+  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {30, 70});
 };
 
 class Application : public wxApp {
