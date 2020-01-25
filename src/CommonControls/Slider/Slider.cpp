f106bdd617d9271745a652ca42a2527e692c23f1
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:50:48 +0100

    Update Slider.cpp

--- src/CommonControls/Slider/Slider.cpp
+++ src/CommonControls/Slider/Slider.cpp
@@ -4,28 +4,33 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Slider example") {
-    SetClientSize(300, 300);
-    
-    gauge1->SetValue(100);
-    staticText1->SetLabel(wxString::Format("%d", slider1->GetValue()));
-    slider1->Bind(wxEVT_SLIDER, [&](wxCommandEvent& event) {
-      gauge1->SetValue(slider1->GetValue());
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Slider example") {
+      SetClientSize(300, 300);
+      
+      gauge1->SetValue(100);
       staticText1->SetLabel(wxString::Format("%d", slider1->GetValue()));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxSlider* slider1 = new wxSlider(panel, wxID_ANY, 100, 0, 200, {50, 50}, {25, 200}, wxSL_VERTICAL|wxSL_INVERSE);
-  wxGauge* gauge1 = new wxGauge(panel, wxID_ANY, 200, {80, 50}, {25, 200}, wxGA_VERTICAL);
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {150, 50});
-};
+      slider1->Bind(wxEVT_SLIDER, [&](wxCommandEvent& event) {
+        gauge1->SetValue(slider1->GetValue());
+        staticText1->SetLabel(wxString::Format("%d", slider1->GetValue()));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxSlider* slider1 = new wxSlider(panel, wxID_ANY, 100, 0, 200, {50, 50}, {25, 200}, wxSL_VERTICAL|wxSL_INVERSE);
+    wxGauge* gauge1 = new wxGauge(panel, wxID_ANY, 200, {80, 50}, {25, 200}, wxGA_VERTICAL);
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {150, 50});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
