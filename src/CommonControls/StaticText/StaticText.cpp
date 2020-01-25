c6edfa08479fc938b97b4b970100650fac5158be
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:01:19 +0100

    Update StaticText.cpp

--- src/CommonControls/StaticText/StaticText.cpp
+++ src/CommonControls/StaticText/StaticText.cpp
@@ -3,17 +3,22 @@
 #include <wx/panel.h>
 #include <wx/stattext.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "StaticText example", wxDefaultPosition, {300, 300}) {}
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "staticText1", {10, 10});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "StaticText example", wxDefaultPosition, {300, 300}) {}
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "staticText1", {10, 10});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
