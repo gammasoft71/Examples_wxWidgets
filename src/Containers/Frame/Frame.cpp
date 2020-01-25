5a925eb101ba80fa0a3c0599978d532036fd64c5
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:08:45 +0100

    Update Frame.cpp

--- src/Containers/Frame/Frame.cpp
+++ src/Containers/Frame/Frame.cpp
@@ -1,26 +1,31 @@
 #include <wx/wx.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Form example", wxDefaultPosition, wxSize(640, 480)) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      Close();
-    });
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Form example", wxDefaultPosition, {640, 480}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        Close();
+      });
 
-    Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
-      bool can_cloase = wxMessageBox("Are you sure you want exit?", "Close Form", wxICON_QUESTION | wxYES_NO) == wxYES;
-      event.Veto(!can_cloase);
-      event.Skip(can_cloase);
-    });
-  }
+      Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
+        bool can_cloase = wxMessageBox("Are you sure you want exit?", "Close Form", wxICON_QUESTION|wxYES_NO) == wxYES;
+        event.Veto(!can_cloase);
+        event.Skip(can_cloase);
+      });
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Close", {10, 10});
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Close", {10, 10});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
