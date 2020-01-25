bfe54d6696ca0d487d28cc6b841f28e730a1e156
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:17:58 +0100

    Update ComboBox.cpp

--- src/CommonControls/ComboBox/ComboBox.cpp
+++ src/CommonControls/ComboBox/ComboBox.cpp
@@ -3,46 +3,49 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-using namespace std;
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ComboBox example", wxDefaultPosition, {300, 300}) {
+      comboBox1->Append({"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
+      comboBox1->Select(0);
+      comboBox1->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
+        comboBox2->SetSelection(comboBox1->GetSelection());
+        comboBox3->SetSelection(comboBox1->GetSelection());
+      });
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ComboBox example", wxDefaultPosition, {300, 300}) {
-    comboBox1->Append(vector<wxString> {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
-    comboBox1->Select(0);
-    comboBox1->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
-      comboBox2->SetSelection(comboBox1->GetSelection());
-      comboBox3->SetSelection(comboBox1->GetSelection());
-    });
+      comboBox2->Append({"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
+      comboBox2->Select(0);
+      comboBox2->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
+        comboBox1->SetSelection(comboBox2->GetSelection());
+        comboBox3->SetSelection(comboBox2->GetSelection());
+      });
 
-    comboBox2->Append(vector<wxString> {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
-    comboBox2->Select(0);
-    comboBox2->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
-      comboBox1->SetSelection(comboBox2->GetSelection());
-      comboBox3->SetSelection(comboBox2->GetSelection());
-    });
-
-    comboBox3->Append(vector<wxString> {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
-    comboBox3->Select(0);
-    comboBox3->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
-      comboBox1->SetSelection(comboBox3->GetSelection());
-      comboBox2->SetSelection(comboBox3->GetSelection());
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxComboBox* comboBox1 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 10});
-  wxComboBox* comboBox2 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 50}, wxDefaultSize, 0, nullptr, wxCB_READONLY);
+      comboBox3->Append({"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
+      comboBox3->Select(0);
+      comboBox3->Bind(wxEVT_COMBOBOX, [&](wxCommandEvent& e) {
+        comboBox1->SetSelection(comboBox3->GetSelection());
+        comboBox2->SetSelection(comboBox3->GetSelection());
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxComboBox* comboBox1 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 10});
+    wxComboBox* comboBox2 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 50}, wxDefaultSize, 0, nullptr, wxCB_READONLY);
 #if defined(__WXMSW__)
-  wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 90}, {110, 163}, 0, nullptr, wxCB_SIMPLE);
+    wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 90}, {110, 163}, 0, nullptr, wxCB_SIMPLE);
 #else
-  wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 90}, wxDefaultSize, 0, nullptr, wxCB_SIMPLE);
+    wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 90}, wxDefaultSize, 0, nullptr, wxCB_SIMPLE);
 #endif
-};
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
