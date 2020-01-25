682ea87a5c4987c1f2843b4cc3b9e9e333811b45
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:49:32 +0100

    Update ScrollBar.cpp

--- src/CommonControls/ScrollBar/ScrollBar.cpp
+++ src/CommonControls/ScrollBar/ScrollBar.cpp
@@ -1,27 +1,32 @@
 #include <wx/wx.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ScrollBar example") {
-    SetClientSize(300, 300);
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ScrollBar example") {
+      SetClientSize(300, 300);
+      
+      scrollBar1->SetScrollbar(100, 1, 200, 1);
+      scrollBar1->SetSize(220, scrollBar1->GetSize().GetHeight());
+
+      scrollBar3->SetScrollbar(100, 1, 200, 1);
+      scrollBar3->SetSize(scrollBar3->GetSize().GetWidth(), 220);
+    }
     
-    scrollBar1->SetScrollbar(100, 1, 200, 1);
-    scrollBar1->SetSize(220, scrollBar1->GetSize().GetHeight());
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxScrollBar* scrollBar1 = new wxScrollBar(panel, wxID_ANY, {10, 10}, wxDefaultSize, wxSB_HORIZONTAL);
+    wxScrollBar* scrollBar2 = new wxScrollBar(panel, wxID_ANY, {10, 40}, wxDefaultSize, wxSB_HORIZONTAL);
+    wxScrollBar* scrollBar3 = new wxScrollBar(panel, wxID_ANY, {270, 70}, wxDefaultSize, wxSB_VERTICAL);
+    wxScrollBar* scrollBar4 = new wxScrollBar(panel, wxID_ANY, {240, 190}, wxDefaultSize, wxSB_VERTICAL);
+  };
 
-    scrollBar3->SetScrollbar(100, 1, 200, 1);
-    scrollBar3->SetSize(scrollBar3->GetSize().GetWidth(), 220);
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxScrollBar* scrollBar1 = new wxScrollBar(panel, wxID_ANY, {10, 10}, wxDefaultSize, wxSB_HORIZONTAL);
-  wxScrollBar* scrollBar2 = new wxScrollBar(panel, wxID_ANY, {10, 40}, wxDefaultSize, wxSB_HORIZONTAL);
-  wxScrollBar* scrollBar3 = new wxScrollBar(panel, wxID_ANY, {270, 70}, wxDefaultSize, wxSB_VERTICAL);
-  wxScrollBar* scrollBar4 = new wxScrollBar(panel, wxID_ANY, {240, 190}, wxDefaultSize, wxSB_VERTICAL);
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
-
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
