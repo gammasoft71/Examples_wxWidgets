e9052009d43d6ff489c618ba0bbb6a54b151685a
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:17:31 +0100

    Update NoteBook.cpp

--- src/BookControls/NoteBook/NoteBook.cpp
+++ src/BookControls/NoteBook/NoteBook.cpp
@@ -1,25 +1,30 @@
 #include <wx/wx.h>
 #include <wx/notebook.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Notebook example", wxDefaultPosition) {
-    SetClientSize(wxSize(390, 270));
-    tabControl1->AddPage(tabpage1, "page 1");
-    tabControl1->AddPage(tabpage2, "page 2");
-    tabControl1->AddPage(tabpage3, "page 3");
-  }
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Notebook example") {
+      SetClientSize(390, 270);
+      tabControl1->AddPage(tabpage1, "page 1");
+      tabControl1->AddPage(tabpage2, "page 2");
+      tabControl1->AddPage(tabpage3, "page 3");
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, {10, 10}, wxSize(370, 250));
-  wxNotebookPage* tabpage1 = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabpage2 = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabpage3 = new wxNotebookPage(tabControl1, wxID_ANY);
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, {10, 10}, {370, 250});
+    wxNotebookPage* tabpage1 = new wxNotebookPage(tabControl1, wxID_ANY);
+    wxNotebookPage* tabpage2 = new wxNotebookPage(tabControl1, wxID_ANY);
+    wxNotebookPage* tabpage3 = new wxNotebookPage(tabControl1, wxID_ANY);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
