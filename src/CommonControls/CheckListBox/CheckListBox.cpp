1f5173d6804e216fe2b4a53b783f8a3d9450393a
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 18:20:01 +0100

    Update CheckListBox.cpp

--- src/CommonControls/CheckListBox/CheckListBox.cpp
+++ src/CommonControls/CheckListBox/CheckListBox.cpp
@@ -3,33 +3,38 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "CheckListBox example") {
-    SetClientSize({200, 240});
-    for (int i = 1; i <= 20; ++i) {
-      checkListBox->Append(wxString::Format("Item %d", i));
-      checkListBox->Check(i - 1, i % 2 != 0);
-      checkListBox->EnsureVisible(0);
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "CheckListBox example") {
+      SetClientSize({200, 240});
+      for (int i = 1; i <= 20; ++i) {
+        checkListBox->Append(wxString::Format("Item %d", i));
+        checkListBox->Check(i - 1, i % 2 != 0);
+        checkListBox->EnsureVisible(0);
+      }
+      checkListBox->SetSelection(0);
+      checkListBox->Bind(wxEVT_CHECKLISTBOX, [&](wxCommandEvent& e) {
+        wxArrayInt checkedIndexes;
+        checkListBox->GetCheckedItems(checkedIndexes);
+        wxString checkedItems;
+        for (auto index : checkedIndexes)
+          checkedItems.Append(wxString::Format("%s%s", checkedItems != wxEmptyString ? ", " : "", checkListBox->GetString(index)));
+        wxMessageOutputDebug().Printf("checked items = {%s}", checkedItems);
+      });
     }
-    checkListBox->SetSelection(0);
-    checkListBox->Bind(wxEVT_CHECKLISTBOX, [&](wxCommandEvent& e) {
-      wxArrayInt checkedIndexes;
-      checkListBox->GetCheckedItems(checkedIndexes);
-      wxString checkedItems;
-      for (int index : checkedIndexes)
-        checkedItems.Append(wxString::Format("%s%s", checkedItems != wxEmptyString ? ", " : "", checkListBox->GetString(index)));
-      wxMessageOutputDebug().Printf("checked items = {%s}", checkedItems);
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxCheckListBox* checkListBox = new wxCheckListBox(panel, wxID_ANY, {20, 20}, {160, 200});
-};
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxCheckListBox* checkListBox = new wxCheckListBox(panel, wxID_ANY, {20, 20}, {160, 200});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
