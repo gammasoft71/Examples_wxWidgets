4246780cf4f1c4c613c79a89aaff1f8cf3863920
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:08:16 +0100

    Update TextCtrl.cpp

--- src/CommonControls/TextCtrl/TextCtrl.cpp
+++ src/CommonControls/TextCtrl/TextCtrl.cpp
@@ -5,31 +5,36 @@
 #include <wx/radiobut.h>
 #include <wx/textctrl.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "TextCtrl example", wxDefaultPosition, {300, 300}) {
-    textCtrl1->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
-      if (textCtrl2->GetValue() != textCtrl1->GetValue())
-        textCtrl2->SetValue(textCtrl1->GetValue());
-    });
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "TextCtrl example", wxDefaultPosition, {300, 300}) {
+      textCtrl1->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
+        if (textCtrl2->GetValue() != textCtrl1->GetValue())
+          textCtrl2->SetValue(textCtrl1->GetValue());
+      });
 
-    textCtrl2->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
-      if (textCtrl1->GetValue() != textCtrl2->GetValue())
-        textCtrl1->SetValue(textCtrl2->GetValue());
-    });
+      textCtrl2->Bind(wxEVT_TEXT, [&](wxCommandEvent& event) {
+        if (textCtrl1->GetValue() != textCtrl2->GetValue())
+          textCtrl1->SetValue(textCtrl2->GetValue());
+      });
+      
+      Show();
+      textCtrl2->SetFocus();
+    }
     
-    Show();
-    textCtrl2->SetFocus();
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxTextCtrl* textCtrl1 = new wxTextCtrl(panel, wxID_ANY, "textBox", {10, 10});
-  wxTextCtrl* textCtrl2 = new wxTextCtrl(panel, wxID_ANY, "textBox", {10, 50});
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxTextCtrl* textCtrl1 = new wxTextCtrl(panel, wxID_ANY, "textBox", {10, 10});
+    wxTextCtrl* textCtrl2 = new wxTextCtrl(panel, wxID_ANY, "textBox", {10, 50});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
