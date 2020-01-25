fb3c65201cb96bf9d805128185c011f86f6559f4
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:15:30 +0100

    Update SplitterWindow.cpp

--- src/Containers/SplitterWindow/SplitterWindow.cpp
+++ src/Containers/SplitterWindow/SplitterWindow.cpp
@@ -1,39 +1,44 @@
 #include <wx/wx.h>
 #include <wx/splitter.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "SplitterWindow example", wxDefaultPosition) {
-    SetClientSize(300, 300);
-    splitterWindowLeft->SetSplitMode(wxSPLIT_VERTICAL);
-    splitterWindowLeft->SplitVertically(leftPanel1, leftPanel2, 70);
-    leftPanel1->SetBackgroundColour(wxTheColourDatabase->Find("Light Blue"));
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "SplitterWindow example", wxDefaultPosition) {
+      SetClientSize(300, 300);
+      splitterWindowLeft->SetSplitMode(wxSPLIT_VERTICAL);
+      splitterWindowLeft->SplitVertically(leftPanel1, leftPanel2, 70);
+      leftPanel1->SetBackgroundColour(wxTheColourDatabase->Find("Light Blue"));
+      
+      splitterWindowRight->SetSplitMode(wxSPLIT_VERTICAL);
+      splitterWindowRight->SplitVertically(rightPanel1, rightPanel2, 160);
+      rightPanel2->SetBackgroundColour({0xFF, 0xFF, 0xE0});
+      
+      splitterWindowCenter->SetSplitMode(wxSPLIT_HORIZONTAL);
+      splitterWindowCenter->SplitHorizontally(centerPanel1, centerPanel2, 147);
+      centerPanel1->SetBackgroundColour(wxTheColourDatabase->Find("Pink"));
+      centerPanel2->SetBackgroundColour({0x90, 0xEE, 0x90});
+    }
     
-    splitterWindowRight->SetSplitMode(wxSPLIT_VERTICAL);
-    splitterWindowRight->SplitVertically(rightPanel1, rightPanel2, 160);
-    rightPanel2->SetBackgroundColour(wxColour(0xFF, 0xFF, 0xE0));
-    
-    splitterWindowCenter->SetSplitMode(wxSPLIT_HORIZONTAL);
-    splitterWindowCenter->SplitHorizontally(centerPanel1, centerPanel2, 147);
-    centerPanel1->SetBackgroundColour(wxTheColourDatabase->Find("Pink"));
-    centerPanel2->SetBackgroundColour(wxColour(0x90, 0xEE, 0x90));
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxSplitterWindow* splitterWindowLeft = new wxSplitterWindow(panel, wxID_ANY, wxPoint(0, 0), {300, 300}, wxSP_NOBORDER);
-  wxPanel* leftPanel1 = new wxPanel(splitterWindowLeft);
-  wxPanel* leftPanel2 = new wxPanel(splitterWindowLeft);
-  wxSplitterWindow* splitterWindowRight = new wxSplitterWindow(leftPanel2, wxID_ANY, wxPoint(0, 0), wxSize(230, 300), wxSP_NOBORDER);
-  wxPanel* rightPanel1 = new wxPanel(splitterWindowRight);
-  wxPanel* rightPanel2 = new wxPanel(splitterWindowRight);
-  wxSplitterWindow* splitterWindowCenter = new wxSplitterWindow(rightPanel1, wxID_ANY, wxPoint(0, 0), wxSize(160, 300), wxSP_NOBORDER);
-  wxPanel* centerPanel1 = new wxPanel(splitterWindowCenter);
-  wxPanel* centerPanel2 = new wxPanel(splitterWindowCenter);
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxSplitterWindow* splitterWindowLeft = new wxSplitterWindow(panel, wxID_ANY, {0, 0}, {300, 300}, wxSP_NOBORDER);
+    wxPanel* leftPanel1 = new wxPanel(splitterWindowLeft);
+    wxPanel* leftPanel2 = new wxPanel(splitterWindowLeft);
+    wxSplitterWindow* splitterWindowRight = new wxSplitterWindow(leftPanel2, wxID_ANY, {0, 0}, {230, 300}, wxSP_NOBORDER);
+    wxPanel* rightPanel1 = new wxPanel(splitterWindowRight);
+    wxPanel* rightPanel2 = new wxPanel(splitterWindowRight);
+    wxSplitterWindow* splitterWindowCenter = new wxSplitterWindow(rightPanel1, wxID_ANY, {0, 0}, {160, 300}, wxSP_NOBORDER);
+    wxPanel* centerPanel1 = new wxPanel(splitterWindowCenter);
+    wxPanel* centerPanel2 = new wxPanel(splitterWindowCenter);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
