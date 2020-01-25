b0d5ae3c7518e00b5d04e6390bcbd4a861a3b869
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 18:00:45 +0100

    Update ApplicationIcon.cpp

--- src/Application/ApplicationIcon/ApplicationIcon.cpp
+++ src/Application/ApplicationIcon/ApplicationIcon.cpp
@@ -1,12 +1,14 @@
 #include <wx/wx.h>
 
-class Application : public wxApp {
-  bool OnInit() override {
-    wxFrame* frame = new wxFrame(nullptr, wxID_ANY, wxEmptyString);
-    wxImage::AddHandler(new wxPNGHandler());
-    frame->SetIcon(wxIcon("Resources/Gammasoft.png", wxBITMAP_TYPE_PNG));
-    return frame->Show();
-  }
-};
+namespace Examples {
+  class Application : public wxApp {
+    bool OnInit() override {
+      wxFrame* frame = new wxFrame(nullptr, wxID_ANY, wxEmptyString);
+      wxImage::AddHandler(new wxPNGHandler());
+      frame->SetIcon({"Resources/Gammasoft.png", wxBITMAP_TYPE_PNG});
+      return frame->Show();
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
