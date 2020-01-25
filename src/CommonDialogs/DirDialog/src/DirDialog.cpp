109e42233f353fefb2bd4786bee43f2d84286c81
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:24:12 +0100

    Update DirDialog.cpp

--- src/CommonDialogs/DirDialog/src/DirDialog.cpp
+++ src/CommonDialogs/DirDialog/src/DirDialog.cpp
@@ -2,25 +2,27 @@
 #include <wx/dirdlg.h>
 #include <wx/stdpaths.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "DirDialog example", wxDefaultPosition, {300, 300}) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxDirDialog folderBrowserDialog(this, wxEmptyString);
-      folderBrowserDialog.SetPath(wxStandardPaths::Get().GetDocumentsDir());
-      if (folderBrowserDialog.ShowModal())
-        label->SetLabel(wxString::Format("Path = %s", folderBrowserDialog.GetPath()));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Folder...", {10, 10});
-  wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Path = ", wxPoint(10, 40));
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "DirDialog example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxDirDialog folderBrowserDialog(this, wxEmptyString);
+        folderBrowserDialog.SetPath(wxStandardPaths::Get().GetDocumentsDir());
+        if (folderBrowserDialog.ShowModal() == wxID_OK)
+          label->SetLabel(wxString::Format("Path = %s", folderBrowserDialog.GetPath()));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Folder...", {10, 10});
+    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Path = ", {10, 40});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {(new Frame())->Show(); return true;}
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
