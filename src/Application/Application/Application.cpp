b434d91422678d5f9748ba69de166227f552c54f
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 17:54:21 +0100

    Update Application.cpp

--- src/Application/Application/Application.cpp
+++ src/Application/Application/Application.cpp
@@ -2,12 +2,6 @@
 
 class Application : public wxApp {
   bool OnInit() override {(new wxFrame(nullptr, wxID_ANY, wxEmptyString))->Show(); return true;}
-  
-  int FilterEvent(wxEvent &event) override {
-    static int counter = 0;
-    wxMessageOutputDebug().Printf("coutner = %d", ++counter);
-    return wxApp::FilterEvent(event);
-  }
 };
 
 wxIMPLEMENT_APP(Application);
