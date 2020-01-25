0287d5199c7e5a73aeaa885bbbed709a9dbbc4d0
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 23:12:37 +0100

    Update StaticBox.cpp

--- src/Containers/StaticBox/StaticBox.cpp
+++ src/Containers/StaticBox/StaticBox.cpp
@@ -1,19 +1,24 @@
 #include <wx/wx.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "StaticBox example", wxDefaultPosition) {
-    SetClientSize(wxSize(640, 480));
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxStaticBox* staticBox1 = new wxStaticBox(panel, wxID_ANY, "GroupBox 1", {10, 10}, wxSize(305, 460));
-  wxStaticBox* staticBox2 = new wxStaticBox(panel, wxID_ANY, wxEmptyString, wxPoint(325, 10), wxSize(305, 460));
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "StaticBox example") {
+      SetClientSize(640, 480);
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxStaticBox* staticBox1 = new wxStaticBox(panel, wxID_ANY, "GroupBox 1", {10, 10}, {305, 460});
+    wxStaticBox* staticBox2 = new wxStaticBox(panel, wxID_ANY, wxEmptyString, {325, 10}, {305, 460});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
