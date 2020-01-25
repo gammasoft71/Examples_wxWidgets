e8de4af1a2e3cdbf0086b479e93065474f6d4975
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:17:55 +0100

    Update Choice.cpp

--- src/CommonControls/Choice/Choice.cpp
+++ src/CommonControls/Choice/Choice.cpp
@@ -3,32 +3,35 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-using namespace std;
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Choice example", wxDefaultPosition, {300, 300}) {
+      choice1->Append({"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
+      choice1->SetSelection(0);
+      choice1->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
+        choice2->SetSelection(choice1->GetSelection());
+      });
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Choice example", wxDefaultPosition, {300, 300}) {
-    choice1->Append(vector<wxString> {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
-    choice1->SetSelection(0);
-    choice1->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
-      choice2->SetSelection(choice1->GetSelection());
-    });
+      choice2->Append({"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
+      choice2->SetSelection(0);
+      choice2->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
+        choice1->SetSelection(choice2->GetSelection());
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxChoice* choice1 = new wxChoice(panel, wxID_ANY, {10, 10});
+    wxChoice* choice2 = new wxChoice(panel, wxID_ANY, {10, 50});
+  };
 
-    choice2->Append(vector<wxString> {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"});
-    choice2->SetSelection(0);
-    choice2->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
-      choice1->SetSelection(choice2->GetSelection());
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxChoice* choice1 = new wxChoice(panel, wxID_ANY, {10, 10});
-  wxChoice* choice2 = new wxChoice(panel, wxID_ANY, {10, 50});
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
-
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
