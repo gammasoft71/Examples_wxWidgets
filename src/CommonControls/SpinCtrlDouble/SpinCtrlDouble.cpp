ef4c198a261876b61b5316726a394719edb38880
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:04:56 +0100

    Update SpinCtrlDouble.cpp

--- src/CommonControls/SpinCtrlDouble/SpinCtrlDouble.cpp
+++ src/CommonControls/SpinCtrlDouble/SpinCtrlDouble.cpp
@@ -5,25 +5,30 @@
 #include <wx/panel.h>
 #include <wx/stattext.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "SpinCtrlDouble example", wxDefaultPosition, {300, 300}) {
-    spinCtrlDouble1->SetRange(10.0, 11.0);
-    spinCtrlDouble1->SetDigits(2);
-    spinCtrlDouble1->SetIncrement(0.01);
-    spinCtrlDouble1->Bind(wxEVT_SPIN, [&](wxCommandEvent& EVENT) {
-      text->SetLabel(wxString::Format("value = %.02f", spinCtrlDouble1->GetValue()));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxSpinCtrlDouble* spinCtrlDouble1 = new wxSpinCtrlDouble(panel, wxID_ANY, wxEmptyString, {80, 50});
-  wxStaticText* text = new wxStaticText(panel, wxID_ANY, "value = 10.00", {80, 100});
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "SpinCtrlDouble example", wxDefaultPosition, {300, 300}) {
+      spinCtrlDouble1->SetRange(10.0, 11.0);
+      spinCtrlDouble1->SetDigits(2);
+      spinCtrlDouble1->SetIncrement(0.01);
+      spinCtrlDouble1->Bind(wxEVT_SPIN, [&](wxCommandEvent& event) {
+        text->SetLabel(wxString::Format("value = %.02f", spinCtrlDouble1->GetValue()));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxSpinCtrlDouble* spinCtrlDouble1 = new wxSpinCtrlDouble(panel, wxID_ANY, wxEmptyString, {80, 50});
+    wxStaticText* text = new wxStaticText(panel, wxID_ANY, "value = 10.00", {80, 100});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
