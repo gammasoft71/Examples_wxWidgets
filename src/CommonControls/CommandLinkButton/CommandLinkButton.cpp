c8fc2bee7b478d37ee5d12c19e1dcab060386837
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:22:56 +0100

    Update CommandLinkButton.cpp

--- src/CommonControls/CommandLinkButton/CommandLinkButton.cpp
+++ src/CommonControls/CommandLinkButton/CommandLinkButton.cpp
@@ -4,23 +4,28 @@
 #include <wx/panel.h>
 #include <wx/stattext.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "CommandLinkButton example", wxDefaultPosition, {300, 300}) {
-    commandLinkButton1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      staticText1->SetLabel(wxString::Format("commandLinkButton1 clicked %d times", ++commandLinkButton1Clicked));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxCommandLinkButton* commandLinkButton1 = new wxCommandLinkButton(panel, wxID_ANY, "Link", "Information text", {30, 30}, {150, 40});
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "commandLinkButton1 clicked 0 times", {30, 100}, {200, 20});
-  int commandLinkButton1Clicked = 0;
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "CommandLinkButton example", wxDefaultPosition, {300, 300}) {
+      commandLinkButton1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        staticText1->SetLabel(wxString::Format("commandLinkButton1 clicked %d times", ++commandLinkButton1Clicked));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxCommandLinkButton* commandLinkButton1 = new wxCommandLinkButton(panel, wxID_ANY, "Link", "Information text", {30, 30}, {150, 40});
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "commandLinkButton1 clicked 0 times", {30, 100}, {200, 20});
+    int commandLinkButton1Clicked = 0;
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
