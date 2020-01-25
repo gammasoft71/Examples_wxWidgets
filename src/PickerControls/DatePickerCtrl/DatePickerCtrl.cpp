60a2e8d7811214bb65ded47337d2deeeef255ce1
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:20:36 +0100

    Update DatePickerCtrl.cpp

--- src/PickerControls/DatePickerCtrl/DatePickerCtrl.cpp
+++ src/PickerControls/DatePickerCtrl/DatePickerCtrl.cpp
@@ -5,25 +5,30 @@
 
 using namespace std::chrono;
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "DatePickerCtrl example", wxDefaultPosition, {300, 300}) {
-    datePicker1->SetRange({4, wxDateTime::Apr, 1975}, {system_clock::to_time_t(system_clock::now())});
-    datePicker1->SetValue({4, wxDateTime::Apr, 1975});
-    datePicker1->Bind(wxEVT_DATE_CHANGED, [&](wxDateEvent& event) {
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "DatePickerCtrl example", wxDefaultPosition, {300, 300}) {
+      datePicker1->SetRange({4, wxDateTime::Apr, 1975}, {system_clock::to_time_t(system_clock::now())});
+      datePicker1->SetValue({4, wxDateTime::Apr, 1975});
+      datePicker1->Bind(wxEVT_DATE_CHANGED, [&](wxDateEvent& event) {
+        staticText1->SetLabelText(datePicker1->GetValue().FormatDate());
+      });
       staticText1->SetLabelText(datePicker1->GetValue().FormatDate());
-    });
-    staticText1->SetLabelText(datePicker1->GetValue().FormatDate());
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxDatePickerCtrl* datePicker1 = new wxDatePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, {30, 30}, wxDefaultSize, wxDP_DROPDOWN | wxDP_SHOWCENTURY);
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {30, 70});
-};
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxDatePickerCtrl* datePicker1 = new wxDatePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, {30, 30}, wxDefaultSize, wxDP_DROPDOWN | wxDP_SHOWCENTURY);
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {30, 70});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
