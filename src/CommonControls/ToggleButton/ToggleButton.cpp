730c15d8a8ef094f2f5422804d224ad4a725afd2
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:10:13 +0100

    Update ToggleButton.cpp

--- src/CommonControls/ToggleButton/ToggleButton.cpp
+++ src/CommonControls/ToggleButton/ToggleButton.cpp
@@ -3,27 +3,32 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ToggleButton example", wxDefaultPosition, {300, 300}) {
-    toggleButton1->SetValue(true);
-    toggleButton1->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
-      toggleButton1->SetLabel(toggleButton1->GetValue() ? "Checked" : "Unchecked");
-    });
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ToggleButton example", wxDefaultPosition, {300, 300}) {
+      toggleButton1->SetValue(true);
+      toggleButton1->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
+        toggleButton1->SetLabel(toggleButton1->GetValue() ? "Checked" : "Unchecked");
+      });
 
-    toggleButton2->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
-      toggleButton2->SetValue(false);
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxToggleButton* toggleButton1 = new wxToggleButton(panel, wxID_ANY, "Checked", {30, 30});
-  wxToggleButton* toggleButton2 = new wxToggleButton(panel, wxID_ANY, "Unchecked", {30, 60});
-};
+      toggleButton2->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
+        toggleButton2->SetValue(false);
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxToggleButton* toggleButton1 = new wxToggleButton(panel, wxID_ANY, "Checked", {30, 30});
+    wxToggleButton* toggleButton2 = new wxToggleButton(panel, wxID_ANY, "Unchecked", {30, 60});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
