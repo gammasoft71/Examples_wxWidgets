f73405c85ae52efc7bc68b71784be3e0d0f0e818
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:34:00 +0100

    Update ListBox.cpp

--- src/CommonControls/ListBox/ListBox.cpp
+++ src/CommonControls/ListBox/ListBox.cpp
@@ -5,37 +5,42 @@
 
 using namespace std;
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ListBox example") {
-    SetClientSize(360, 240);
-    
-    listBoxLeft->SetSize(150, 200);
-    listBoxLeft->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
-      listBoxRight->Append(listBoxLeft->GetStringSelection());
-      listBoxRight->SetSelection(0);
-      listBoxLeft->Delete(listBoxLeft->GetSelection());
-    });
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ListBox example") {
+      SetClientSize(360, 240);
+      
+      listBoxLeft->SetSize(150, 200);
+      listBoxLeft->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
+        listBoxRight->Append(listBoxLeft->GetStringSelection());
+        listBoxRight->SetSelection(0);
+        listBoxLeft->Delete(listBoxLeft->GetSelection());
+      });
 
-    listBoxRight->SetSize(150, 200);
-    listBoxRight->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
-      listBoxLeft->Append(listBoxRight->GetStringSelection());
+      listBoxRight->SetSize(150, 200);
+      listBoxRight->Bind(wxEVT_LISTBOX_DCLICK, [&](wxCommandEvent& event) {
+        listBoxLeft->Append(listBoxRight->GetStringSelection());
+        listBoxLeft->SetSelection(0);
+        listBoxRight->Delete(listBoxRight->GetSelection());
+      });
+      
+      listBoxLeft->Append({"draw", "cut", "paste", "delete", "open", "close", "remove", "edit", "find", "increment", "decrement", "write", "read", "post", "build", "make", "release", "create", "choose", "erase"});
       listBoxLeft->SetSelection(0);
-      listBoxRight->Delete(listBoxRight->GetSelection());
-    });
+    }
     
-    listBoxLeft->Append(vector<wxString> {"draw", "cut", "paste", "delete", "open", "close", "remove", "edit", "find", "increment", "decrement", "write", "read", "post", "build", "make", "release", "create", "choose", "erase"});
-    listBoxLeft->SetSelection(0);
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxListBox* listBoxLeft = new wxListBox(panel, wxID_ANY, wxPoint(20, 20));
-  wxListBox* listBoxRight = new wxListBox(panel, wxID_ANY, wxPoint(190, 20), wxDefaultSize, 0, nullptr, wxLB_SORT);
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxListBox* listBoxLeft = new wxListBox(panel, wxID_ANY, wxPoint(20, 20));
+    wxListBox* listBoxRight = new wxListBox(panel, wxID_ANY, wxPoint(190, 20), wxDefaultSize, 0, nullptr, wxLB_SORT);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
