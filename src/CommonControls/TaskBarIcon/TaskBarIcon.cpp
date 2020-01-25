fe4a7fdf990594dee9c60bd15dc564c1d03ed0ac
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:06:49 +0100

    Update TaskBarIcon.cpp

--- src/CommonControls/TaskBarIcon/TaskBarIcon.cpp
+++ src/CommonControls/TaskBarIcon/TaskBarIcon.cpp
@@ -1,23 +1,28 @@
 #include "Gammasoft.xpm"
 #include "wxNotifyIcon.h"
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, {300, 300}) {
-    notifyIcon1.SetIcon({GammasoftIcon});
-    notifyIcon1.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
-      Show(!IsShown());
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this frame.", {10, 10});
-  wxNotifyIcon notifyIcon1;
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, {300, 300}) {
+      notifyIcon1.SetIcon({GammasoftIcon});
+      notifyIcon1.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
+        Show(!IsShown());
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this frame.", {10, 10});
+    wxNotifyIcon notifyIcon1;
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
