21410594f3fee2a0c34d22d0a52614c3186d1f7c
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 21:45:44 +0100

    Update RichTextCtrl.cpp

--- src/CommonControls/RichTextCtrl/RichTextCtrl.cpp
+++ src/CommonControls/RichTextCtrl/RichTextCtrl.cpp
@@ -4,33 +4,32 @@
 #include <wx/sizer.h>
 #include <wx/richtext/richtextctrl.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "RichTextCtrl example", wxDefaultPosition, {300, 300}) {
-    Bind(wxEVT_SIZING, [&](wxSizeEvent& event) {
-      richTextCtrl->SetSize(GetClientSize());
-    });
-    richTextCtrl->SetSize(GetClientSize());
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "RichTextCtrl example", wxDefaultPosition, {300, 300}) {
+      richTextCtrl->WriteText("Colored text :\n");
+      richTextCtrl->BeginTextColour({255, 0, 0});
+      richTextCtrl->WriteText("  Red\n");
+      richTextCtrl->EndTextColour();
+      richTextCtrl->BeginTextColour({0, 139, 0});
+      richTextCtrl->WriteText("  Green\n");
+      richTextCtrl->EndTextColour();
+      richTextCtrl->BeginTextColour({0, 0, 255});
+      richTextCtrl->WriteText("  Blue\n");
+      richTextCtrl->EndTextColour();
+    }
+    
+  private:
+    wxRichTextCtrl* richTextCtrl = new wxRichTextCtrl(this);
+  };
 
-    richTextCtrl->WriteText("Colored text :\n");
-    richTextCtrl->BeginTextColour({255, 0, 0});
-    richTextCtrl->WriteText("  Red\n");
-    richTextCtrl->EndTextColour();
-    richTextCtrl->BeginTextColour({0, 139, 0});
-    richTextCtrl->WriteText("  Green\n");
-    richTextCtrl->EndTextColour();
-    richTextCtrl->BeginTextColour({0, 0, 255});
-    richTextCtrl->WriteText("  Blue\n");
-    richTextCtrl->EndTextColour();
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxRichTextCtrl* richTextCtrl = new wxRichTextCtrl(panel);
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
-
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
