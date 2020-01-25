968ed50cc7dc38fc270741b6a56d7d0bbe6d0a53
Gammasoft71 <gammasoft71@gmail.com>
2020-01-25 22:36:16 +0100

    Update Line.cpp

--- src/CustomControls/Line/Line.cpp
+++ src/CustomControls/Line/Line.cpp
@@ -4,7 +4,9 @@
 namespace Examples {
   class wxColouredLine : public wxPanel {
   public:
-    wxColouredLine(wxWindow* parent, const wxColour& colour, const wxPoint& position = wxDefaultPosition, const wxSize& size = wxDefaultSize) : wxPanel(parent, wxID_ANY, position, size) {SetColour(colour);}
+    wxColouredLine(wxWindow* parent, const wxColour& colour, const wxPoint& position = wxDefaultPosition, const wxSize& size = wxDefaultSize) : wxPanel(parent, wxID_ANY, position, size) {
+      SetColour(colour);
+    }
     
     void SetColour(const wxColour& colour) {SetBackgroundColour(colour);}
     wxColour GetColour() const {return GetBackgroundColour();}
@@ -13,20 +15,23 @@ namespace Examples {
   class Form : public wxFrame {
   public:
     Form() : wxFrame(nullptr, wxID_ANY, "Lines example", wxDefaultPosition, {300, 300}) {
-      SetClientSize({300, 300});
+      SetClientSize(300, 300);
     }
     
   private:
     wxPanel* panel = new wxPanel(this);
-    wxColouredLine* lineSeparator = new wxColouredLine(panel, wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW), {10, 10}, wxSize(280, 2));
-    wxColouredLine* lineRed = new wxColouredLine(panel, wxColour(255, 0, 0), wxPoint(10, 20), wxSize(2, 250));
-    wxColouredLine* lineGreen = new wxColouredLine(panel, wxColour(0, 143, 0), wxPoint(149, 20), wxSize(2, 250));
-    wxColouredLine* lineBlue = new wxColouredLine(panel, wxColour(0, 0, 255), wxPoint(288, 20), wxSize(2, 250));
-    wxColouredLine* lineSeparator2 = new wxColouredLine(panel, wxSystemSettings::GetColour(wxSYS_COLOUR_BTNTEXT), wxPoint(10, 278), wxSize(280, 2));
+    wxColouredLine* lineSeparator = new wxColouredLine(panel, wxSystemSettings::GetColour(wxSYS_COLOUR_WINDOW), {10, 10}, {280, 2});
+    wxColouredLine* lineRed = new wxColouredLine(panel, {255, 0, 0}, {10, 20}, {2, 250});
+    wxColouredLine* lineGreen = new wxColouredLine(panel, {0, 143, 0}, {149, 20}, {2, 250});
+    wxColouredLine* lineBlue = new wxColouredLine(panel, {0, 0, 255}, {288, 20}, {2, 250});
+    wxColouredLine* lineSeparator2 = new wxColouredLine(panel, wxSystemSettings::GetColour(wxSYS_COLOUR_BTNTEXT), {10, 278}, {280, 2});
   };
   
   class Application : public wxApp {
-    bool OnInit() override {(new Form())->Show(); return true;}
+    bool OnInit() override {
+      (new Form())->Show();
+      return true;
+    }
   };
 }
 
