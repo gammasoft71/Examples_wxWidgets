a90c2ea25b1013571a3a25f31f756fabc4a7ab03
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 13:01:50 +0100

    Update FrameClick.cpp

--- src/Events/FrameClick/FrameClick.cpp
+++ src/Events/FrameClick/FrameClick.cpp
@@ -1,19 +1,24 @@
 #include <wx/wx.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Click anywhere on the frame", wxDefaultPosition, {300, 300}) {
-    panel->Bind(wxEVT_LEFT_UP, [&](wxMouseEvent& event) {
-      wxMessageBox("The frame is clicked", "FrameClick", wxOK) ;
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-};
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Click anywhere on the frame", wxDefaultPosition, {300, 300}) {
+      panel->Bind(wxEVT_LEFT_UP, [&](wxMouseEvent& event) {
+        wxMessageBox("The frame is clicked", "FrameClick", wxOK) ;
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
