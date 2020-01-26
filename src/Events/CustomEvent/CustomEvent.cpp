122bd440621c1e502421d09f1a819a8afcc5cc4c
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 13:00:03 +0100

    Update CustomEvent.cpp

--- src/Events/CustomEvent/CustomEvent.cpp
+++ src/Events/CustomEvent/CustomEvent.cpp
@@ -1,44 +1,50 @@
 #include <any>
 #include <wx/wx.h>
 
-class CustomEvent;
-wxDECLARE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
+namespace Examples {
+  class CustomEvent;
+  wxDECLARE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
 
-class CustomEvent : public wxCommandEvent {
-public:
-  CustomEvent(wxEventType commandType = WX_EVT_CUSTOM_EVENT, int id = 0) : wxCommandEvent(commandType, id) { }
-  
-  // You *must* copy here the data to be transported
-  CustomEvent(const CustomEvent& event) : wxCommandEvent(event) {}
-  
-  std::any tag() const {return tag_;}
-  void tag(const std::any& value) {tag_ = value;}
-  
-private:
-  std::any tag_;
-};
-
-wxDEFINE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
-
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Custom event example", wxDefaultPosition, {300, 300}) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxPostEvent(this, CustomEvent());
-    });
+  class CustomEvent : public wxCommandEvent {
+  public:
+    CustomEvent(wxEventType commandType = WX_EVT_CUSTOM_EVENT, int id = 0) : wxCommandEvent(commandType, id) { }
+    CustomEvent(const CustomEvent& event) : wxCommandEvent(event), tag_(event.tag_) {}
     
-    Bind(WX_EVT_CUSTOM_EVENT, [&](CustomEvent& event) {
-      wxMessageBox("Receive CustomEvent event", "CustomEVentk", wxOK);
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Send Event", {10, 10});
-};
+    std::any tag() const {return tag_;}
+    void tag(const std::any& value) {tag_ = value;}
+    
+  private:
+    std::any tag_;
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  wxDEFINE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
 
-wxIMPLEMENT_APP(Application);
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Custom event example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        static auto counter = 0;
+        auto customEvent = new CustomEvent();
+        customEvent->tag(++counter);
+        wxQueueEvent(this, customEvent);
+      });
+      
+      Bind(WX_EVT_CUSTOM_EVENT, [&](CustomEvent& event) {
+        wxMessageBox(wxString::Format("Receive CustomEvent event (%d)", std::any_cast<int>(event.tag())), "CustomEVent", wxOK);
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Send Event", {10, 10});
+  };
+
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
