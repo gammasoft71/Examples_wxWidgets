6067ca596560f02c39191d0e1040688e23f7f6a5
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 14:07:47 +0100

    Update ColoredTabPages.cpp

--- src/Others/ColoredTabPages/ColoredTabPages.cpp
+++ src/Others/ColoredTabPages/ColoredTabPages.cpp
@@ -1,35 +1,40 @@
 #include <wx/wx.h>
 #include <wx/notebook.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "Colored TabPages example", wxDefaultPosition) {
-    SetClientSize(wxSize(390, 270));
-    
-    tabControl1->AddPage(tabPageLightPink, "Light red");
-    tabPageLightPink->SetBackgroundColour(wxColour(0xFF, 0xB6, 0xC1, 0xFF));
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Colored TabPages example") {
+      SetClientSize(390, 270);
+      
+      tabControl1->AddPage(tabPageLightPink, "Pink");
+      tabPageLightPink->SetBackgroundColour({0xFF, 0xB6, 0xC1, 0xFF});
 
-    tabControl1->AddPage(tabPageLightGreen, "Light green");
-    tabPageLightGreen->SetBackgroundColour(wxColour(0x90, 0xEE, 0x90, 0xFF));
+      tabControl1->AddPage(tabPageLightGreen, "Green");
+      tabPageLightGreen->SetBackgroundColour({0x90, 0xEE, 0x90, 0xFF});
 
-    tabControl1->AddPage(tabPageLightBlue, "Light blue");
-    tabPageLightBlue->SetBackgroundColour(wxColour(0xAD, 0xD8, 0xE6, 0xFF));
+      tabControl1->AddPage(tabPageLightBlue, "Blue");
+      tabPageLightBlue->SetBackgroundColour({0xAD, 0xD8, 0xE6, 0xFF});
 
-    tabControl1->AddPage(tabPageLightYellow, "Light yellow");
-    tabPageLightYellow->SetBackgroundColour(wxColour(0xFF, 0xFF, 0xE0, 0xFF));
-  }
+      tabControl1->AddPage(tabPageLightYellow, "Yellow");
+      tabPageLightYellow->SetBackgroundColour({0xFF, 0xFF, 0xE0, 0xFF});
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, {10, 10}, wxSize(370, 250), wxNB_BOTTOM);
-  wxNotebookPage* tabPageLightPink = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageLightGreen = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageLightBlue = new wxNotebookPage(tabControl1, wxID_ANY);
-  wxNotebookPage* tabPageLightYellow = new wxNotebookPage(tabControl1, wxID_ANY);
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxNotebook* tabControl1 = new wxNotebook(panel, wxID_ANY, {10, 10}, {370, 250}, wxNB_BOTTOM);
+    wxNotebookPage* tabPageLightPink = new wxNotebookPage(tabControl1, wxID_ANY);
+    wxNotebookPage* tabPageLightGreen = new wxNotebookPage(tabControl1, wxID_ANY);
+    wxNotebookPage* tabPageLightBlue = new wxNotebookPage(tabControl1, wxID_ANY);
+    wxNotebookPage* tabPageLightYellow = new wxNotebookPage(tabControl1, wxID_ANY);
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
