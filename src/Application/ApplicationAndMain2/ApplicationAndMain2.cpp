542278cc134df8b0f641445a25bdcec405327018
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 17:31:46 +0100

    Update ApplicationAndMain2.cpp

--- src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
+++ src/Application/ApplicationAndMain2/ApplicationAndMain2.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[]) {
   AppInitializer appInitializer;
-  wxFrame* frame1 = new wxFrame(nullptr, wxID_ANY, "Frame1", wxDefaultPosition, {300, 300});
+  auto frame1 = new wxFrame(nullptr, wxID_ANY, "Frame1", wxDefaultPosition, {300, 300});
   frame1->Show();
   wxTheApp->OnRun();
 }
