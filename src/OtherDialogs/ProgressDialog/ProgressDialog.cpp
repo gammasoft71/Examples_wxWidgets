e6bd8b3432aa66f8a4c03aac7539cd12a125f781
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 10:05:25 +0100

    Update ProgressDialog.cpp

--- src/OtherDialogs/ProgressDialog/ProgressDialog.cpp
+++ src/OtherDialogs/ProgressDialog/ProgressDialog.cpp
@@ -1,37 +1,45 @@
+#include <thread>
 #include <wx/wx.h>
 #include <wx/progdlg.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "ProgressDialog example", wxDefaultPosition, {300, 300}) {
-    buttonProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxProgressDialog* progressDialog = new wxProgressDialog("Process running", "Please wait...", 100, this, wxPD_APP_MODAL | wxPD_AUTO_HIDE);
-      for (int step = 1; step <= progressDialog->GetRange(); step++) {
-        progressDialog->Update(step, wxString::Format("Step %d/100", step));
-        wxMicroSleep(100000);
-      }
-      progressDialog->Destroy();
-    });
+using namespace std;
+using namespace std::literals;
 
-    buttonIndeterminateProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxProgressDialog* progressDialog = new wxProgressDialog("Indeterminate process running", "Click \"Cancel\" to abort", 10, this, wxPD_APP_MODAL | wxPD_AUTO_HIDE  | wxPD_CAN_ABORT);
-      while(true) {
-        if (!progressDialog->Pulse())
-          break;
-        wxMicroSleep(100000);
-      }
-      progressDialog->Destroy();
-    });
-  }
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "ProgressDialog example", wxDefaultPosition, {300, 300}) {
+      buttonProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxProgressDialog* progressDialog = new wxProgressDialog("Process running", "Please wait...", 100, this, wxPD_APP_MODAL|wxPD_AUTO_HIDE|wxPD_CAN_ABORT);
+        for (int step = 1; step <= progressDialog->GetRange(); step++) {
+          if (!progressDialog->Update(step, wxString::Format("Step %d/100", step))) break;
+          this_thread::sleep_for(100ms);
+        }
+        progressDialog->Destroy();
+      });
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* buttonProcess = new wxButton(panel, wxID_ANY, "Process", {10, 10});
-  wxButton* buttonIndeterminateProcess = new wxButton(panel, wxID_ANY, "Indeterminate Process", wxPoint(100, 10));
-};
+      buttonIndeterminateProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxProgressDialog* progressDialog = new wxProgressDialog("Indeterminate process running", "Click \"Cancel\" to abort", 10, this, wxPD_APP_MODAL|wxPD_AUTO_HIDE|wxPD_CAN_ABORT);
+        while(true) {
+          if (!progressDialog->Pulse()) break;
+          this_thread::sleep_for(100ms);
+        }
+        progressDialog->Destroy();
+      });
+    }
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* buttonProcess = new wxButton(panel, wxID_ANY, "Process", {10, 10});
+    wxButton* buttonIndeterminateProcess = new wxButton(panel, wxID_ANY, "Indeterminate Process", {100, 10});
+  };
 
-wxIMPLEMENT_APP(Application);
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
