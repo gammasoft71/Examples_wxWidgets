318637960659f6dfdce2225a97a2a1094c57f61f
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 09:46:27 +0100

    Update OpenFileDialog.cpp

--- src/CommonDialogs/OpenFileDialog/src/OpenFileDialog.cpp
+++ src/CommonDialogs/OpenFileDialog/src/OpenFileDialog.cpp
@@ -1,25 +1,30 @@
 #include <wx/wx.h>
 #include <wx/filedlg.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "OpenFileDialog example", wxDefaultPosition, {300, 300}) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      wxFileDialog openFileDialog(this, wxEmptyString, wxEmptyString, wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
-      if (openFileDialog.ShowModal() == wxID_OK) {
-        label->SetLabelText(wxString::Format("File = %s",  openFileDialog.GetPath()));
-      }
-    });
-  }
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "OpenFileDialog example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxFileDialog openFileDialog(this, wxEmptyString, wxEmptyString, wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFD_OPEN|wxFD_FILE_MUST_EXIST);
+        if (openFileDialog.ShowModal() == wxID_OK) {
+          label->SetLabelText(wxString::Format("File = %s",  openFileDialog.GetPath()));
+        }
+      });
+    }
 
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Open...", {10, 10});
-  wxStaticText* label = new wxStaticText(panel, wxID_ANY, "", wxPoint(10, 40));
-};
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Open...", {10, 10});
+    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "", {10, 40});
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
 wxIMPLEMENT_APP(Application);
