eb29b071dbd2b83b9c23795c1c5b2e0f6888db65
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 15:16:20 +0100

    Update MdiFrame.cpp

--- src/Others/MdiFrame/MdiFrame.cpp
+++ src/Others/MdiFrame/MdiFrame.cpp
@@ -2,59 +2,64 @@
 #include <wx/wx.h>
 #include <wx/richtext/richtextctrl.h>
 
-class MdiChildFrame : public wxMDIChildFrame {
-public:
-  MdiChildFrame(wxMDIParentFrame* parent) : wxMDIChildFrame(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, {300, 300}) {
-  }
-  
-private:
-  wxRichTextCtrl* richTextBox = new wxRichTextCtrl(this);
-};
-
-class MdiParentFrame : public wxMDIParentFrame {
-public:
-  MdiParentFrame() : wxMDIParentFrame(nullptr, wxID_ANY, "Mdi example", wxDefaultPosition, wxSize(800, 600)) {
-    menuFile->Append(wxID_NEW, "New\tCtrl+N");
-    menuFile->Append(wxID_CLOSE, "Close\tCtrl+W");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT, "Quit\tAlt+F4");
+namespace Examples {
+  class MdiChildFrame : public wxMDIChildFrame {
+  public:
+    MdiChildFrame(wxMDIParentFrame* parent) : wxMDIChildFrame(parent, wxID_ANY, wxEmptyString, wxDefaultPosition, {300, 300}) {
+    }
     
-    mainMenu->Append(menuFile, "&File");
-    mainMenu->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
-      switch (event.GetId()) {
-        case wxID_NEW: OnMenuFileNewClick(event); break;
-        case wxID_CLOSE: OnMenuFileCloseClick(event); break;
-        case wxID_EXIT: OnMenuFileQuitClick(event); break;
-        default: break;
-      }
-    });
+  private:
+    wxRichTextCtrl* richTextBox = new wxRichTextCtrl(this);
+  };
 
-    SetMenuBar(mainMenu);
-  }
-  
-private:
-  void OnMenuFileNewClick(wxCommandEvent& event) {
-    MdiChildFrame* child = new MdiChildFrame(this);
-    child->SetLabel(wxString::Format("Child %d", ++childCounter));
-    child->Show();
-  }
-  
-  void OnMenuFileCloseClick(wxCommandEvent& event) {
-    if (GetActiveChild())
-      GetActiveChild()->Close();
-  }
-  
-  void OnMenuFileQuitClick(wxCommandEvent& event) {
-    Close();
-  }
-  
-  wxMenuBar* mainMenu = new wxMenuBar();
-  wxMenu* menuFile = new wxMenu();
-  int childCounter = 0;
-};
+  class MdiParentFrame : public wxMDIParentFrame {
+  public:
+    MdiParentFrame() : wxMDIParentFrame(nullptr, wxID_ANY, "Mdi example", wxDefaultPosition, {800, 600}) {
+      menuFile->Append(wxID_NEW, "New\tCtrl+N");
+      menuFile->Append(wxID_CLOSE, "Close\tCtrl+W");
+      menuFile->AppendSeparator();
+      menuFile->Append(wxID_EXIT, "Quit\tAlt+F4");
+      
+      mainMenu->Append(menuFile, "&File");
+      mainMenu->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
+        switch (event.GetId()) {
+          case wxID_NEW: OnMenuFileNewClick(event); break;
+          case wxID_CLOSE: OnMenuFileCloseClick(event); break;
+          case wxID_EXIT: OnMenuFileQuitClick(event); break;
+          default: break;
+        }
+      });
 
-class Application : public wxApp {
-  bool OnInit() override {(new MdiParentFrame())->Show(); return true;}
-};
+      SetMenuBar(mainMenu);
+    }
+    
+  private:
+    void OnMenuFileNewClick(wxCommandEvent& event) {
+      auto child = new MdiChildFrame(this);
+      child->SetLabel(wxString::Format("Child %d", ++childCounter));
+      child->Show();
+    }
+    
+    void OnMenuFileCloseClick(wxCommandEvent& event) {
+      if (GetActiveChild())
+        GetActiveChild()->Close();
+    }
+    
+    void OnMenuFileQuitClick(wxCommandEvent& event) {
+      Close();
+    }
+    
+    wxMenuBar* mainMenu = new wxMenuBar();
+    wxMenu* menuFile = new wxMenu();
+    int childCounter = 0;
+  };
 
-wxIMPLEMENT_APP(Application);
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new MdiParentFrame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
