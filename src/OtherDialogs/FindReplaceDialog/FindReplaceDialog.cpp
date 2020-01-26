10e0bbe509e2739a491b9c6f7fc8d29a59c92c0e
Gammasoft71 <gammasoft71@gmail.com>
2020-01-26 09:57:44 +0100

    Update FindReplaceDialog.cpp

--- src/OtherDialogs/FindReplaceDialog/FindReplaceDialog.cpp
+++ src/OtherDialogs/FindReplaceDialog/FindReplaceDialog.cpp
@@ -1,43 +1,48 @@
 #include <wx/wx.h>
 #include <wx/fdrepdlg.h>
 
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame(nullptr, wxID_ANY, "FindReplaceDialog example", wxDefaultPosition, {300, 300}) {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      findReplaceData.SetFlags(wxFR_WHOLEWORD | wxFR_MATCHCASE | wxFR_DOWN);
-      findReplaceData.SetFindString("Gammasoft");
-      findReplaceData.SetReplaceString("Gammasoft71");
-      findReplaceDialog = new wxFindReplaceDialog(this, &findReplaceData, "Find and replace", wxFR_REPLACEDIALOG);
-      findReplaceDialog->Bind(wxEVT_FIND_CLOSE, [&](wxFindDialogEvent& event) {
-        findReplaceDialog->Destroy();
-      });
-      findReplaceDialog->Bind(wxEVT_FIND, [](wxFindDialogEvent& event) {
-        wxMessageOutputDebug().Printf("Find {%s}", event.GetFindString());
-      });
-      findReplaceDialog->Bind(wxEVT_FIND_NEXT, [](wxFindDialogEvent& event) {
-        wxMessageOutputDebug().Printf("Find next {%s}", event.GetFindString());
-      });
-      findReplaceDialog->Bind(wxEVT_FIND_REPLACE, [](wxFindDialogEvent& event) {
-        wxMessageOutputDebug().Printf("Find and Replace {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
-      });
-      findReplaceDialog->Bind(wxEVT_FIND_REPLACE_ALL, [](wxFindDialogEvent& event) {
-        wxMessageOutputDebug().Printf("Find and Replace all {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
-      });
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "FindReplaceDialog example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        findReplaceData.SetFlags(wxFR_WHOLEWORD | wxFR_MATCHCASE | wxFR_DOWN);
+        findReplaceData.SetFindString("Gammasoft");
+        findReplaceData.SetReplaceString("Gammasoft71");
+        findReplaceDialog = new wxFindReplaceDialog(this, &findReplaceData, "Find and replace", wxFR_REPLACEDIALOG);
+        findReplaceDialog->Bind(wxEVT_FIND_CLOSE, [&](wxFindDialogEvent& event) {
+          findReplaceDialog->Destroy();
+        });
+        findReplaceDialog->Bind(wxEVT_FIND, [](wxFindDialogEvent& event) {
+          wxMessageOutputDebug().Printf("Find {%s}", event.GetFindString());
+        });
+        findReplaceDialog->Bind(wxEVT_FIND_NEXT, [](wxFindDialogEvent& event) {
+          wxMessageOutputDebug().Printf("Find next {%s}", event.GetFindString());
+        });
+        findReplaceDialog->Bind(wxEVT_FIND_REPLACE, [](wxFindDialogEvent& event) {
+          wxMessageOutputDebug().Printf("Find and Replace {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
+        });
+        findReplaceDialog->Bind(wxEVT_FIND_REPLACE_ALL, [](wxFindDialogEvent& event) {
+          wxMessageOutputDebug().Printf("Find and Replace all {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
+        });
 
-      findReplaceDialog->Show();
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel(this);
-  wxButton* button = new wxButton(panel, wxID_ANY, "Find...", {10, 10});
-  wxFindReplaceData findReplaceData;
-  wxFindReplaceDialog* findReplaceDialog;
-};
+        findReplaceDialog->Show();
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Find...", {10, 10});
+    wxFindReplaceData findReplaceData;
+    wxFindReplaceDialog* findReplaceDialog;
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {(new Frame())->Show(); return true;}
-};
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
 
-wxIMPLEMENT_APP(Application);
+wxIMPLEMENT_APP(Examples::Application);
