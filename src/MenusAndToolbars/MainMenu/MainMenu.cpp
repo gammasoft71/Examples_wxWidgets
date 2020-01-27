c78356f033322cd2877dc8d8b0649be17894c30a
Gammasoft71 <gammasoft71@gmail.com>
2020-01-27 10:23:13 +0100

    Update MainMenu.cpp

--- src/MenusAndToolbars/MainMenu/MainMenu.cpp
+++ src/MenusAndToolbars/MainMenu/MainMenu.cpp
@@ -19,12 +19,16 @@ namespace Examples {
       auto menuItemFileOpen = menuFile->Append(wxID_OPEN);
       menuItemFileOpen->SetBitmap(wxArtProvider::GetBitmap(wxART_FILE_OPEN));
       menuFile->Append(wxID_OPENRECENT, "Open recent");
-      menuFile->Append(wxID_CLOSE, "Close\tCtrl+W");
+      menuFile->Append(wxID_CLOSE, "Close\tCTRL+W");
       menuFile->AppendSeparator();
       auto menuItemFileSave = menuFile->Append(wxID_SAVE);
       menuItemFileSave->SetBitmap(wxArtProvider::GetBitmap(wxART_FILE_SAVE));
       auto menuItemFileSaveAs = menuFile->Append(wxID_SAVEAS);
       menuItemFileSaveAs->SetBitmap(wxArtProvider::GetBitmap(wxART_FILE_SAVE_AS));
+      menuFile->AppendSeparator();
+      menuFile->Append(wxID_PAGE_SETUP, "Page Setup...\tCTRL+SHIFT+P");
+      auto menuItemFilePrint = menuFile->Append(wxID_PRINT);
+      menuItemFilePrint->SetBitmap(wxArtProvider::GetBitmap(wxART_PRINT));
       menuFile->AppendSeparator();
       auto menuItemFileQuit = menuFile->Append(wxID_EXIT);
       menuItemFileQuit->SetBitmap(wxArtProvider::GetBitmap(wxART_QUIT));
@@ -42,28 +46,29 @@ namespace Examples {
       menuItemEditPaste->SetBitmap(wxArtProvider::GetBitmap(wxART_PASTE));
       menuEdit->AppendSeparator();
       menuEdit->Append(wxID_SELECTALL);
+      menuEdit->AppendSeparator();
+      menuEdit->Append(wxID_PREFERENCES);
 
+      auto menuItemViewBackward = menuView->Append(wxID_BACKWARD);
+      menuItemViewBackward->SetBitmap(wxArtProvider::GetBitmap(wxART_GO_BACK));
+      auto menuItemViewForward = menuView->Append(wxID_FORWARD);
+      menuItemViewForward->SetBitmap(wxArtProvider::GetBitmap(wxART_GO_FORWARD));
+      menuView->AppendSeparator();
       menuView->Append(wxID_SHOW, "Show");
       menuView->Append(wxID_HIDE, "Hide");
+
+      menuHelp->Append(wxID_ABOUT);
 
       mainMenu->Append(menuFile, "&File");
       mainMenu->Append(menuEdit, "&Edit");
       mainMenu->Append(menuView, "&View");
-
-  #if defined(__WXOSX__)
       mainMenu->Append(menuWindow, "&Window");
-      menuWindow->Append(wxID_ABOUT, "About");
-  #endif
-
       mainMenu->Append(menuHelp, "&Help");
-  #if !defined(__WXOSX__)
-      menuHelp->Append(wxID_ABOUT, "About");
-  #endif
 
       SetMenuBar(mainMenu);
 
       mainMenu->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
-        static std::map<int, std::string> names = {{wxID_NEW, "File/New"}, {wxID_OPEN, "File/Open..."}, {wxID_OPENRECENT, "File/Open recent"}, {wxID_CLOSE, "File/Close"}, {wxID_SAVE, "File/Save"}, {wxID_SAVEAS, "File/Save as..."}, {wxID_EXIT, "File/Quit"}, {wxID_UNDO, "Edit/Undo"}, {wxID_REDO, "Edit/Redo"}, {wxID_CUT, "Edit/Cut"}, {wxID_COPY, "Edit/Copy"}, {wxID_PASTE, "Edit/Paste"}, {wxID_SELECTALL, "Edit/Select All"}, {wxID_SHOW, "View/Show"}, {wxID_HIDE, "View/Hide"}, {wxID_ABOUT, "Help/About"}, {wxID_ANY, "Any"}};
+        static std::map<int, std::string> names = {{wxID_NEW, "File/New"}, {wxID_OPEN, "File/Open..."}, {wxID_OPENRECENT, "File/Open recent"}, {wxID_CLOSE, "File/Close"}, {wxID_SAVE, "File/Save"}, {wxID_SAVEAS, "File/Save as..."}, {wxID_EXIT, "File/Quit"}, {wxID_UNDO, "Edit/Undo"}, {wxID_REDO, "Edit/Redo"}, {wxID_CUT, "Edit/Cut"}, {wxID_COPY, "Edit/Copy"}, {wxID_PASTE, "Edit/Paste"}, {wxID_SELECTALL, "Edit/Select All"}, {wxID_BACKWARD, "View/Back"}, {wxID_FORWARD, "View/Forward"}, {wxID_SHOW, "View/Show"}, {wxID_HIDE, "View/Hide"}, {wxID_ABOUT, "Help/About"}, {wxID_ANY, "Any"}};
         listBox1->Append(names.find(event.GetId()) != names.end() ? names[event.GetId()] : "Unknown");
         if (event.GetId() == wxID_EXIT) event.Skip();
       });
