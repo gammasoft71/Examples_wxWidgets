f398f3c18e79edcb58e625df2b9535f5c22c248b
Gammasoft71 <gammasoft71@gmail.com>
2020-02-16 16:02:52 +0100

    add HelloWorldMessageDialog

---
+++ src/HelloWorld/HelloWorldMessageDialog/HelloWorldMessageDialog.cpp
@@ -0,0 +1,19 @@
+#include <wx/wx.h>
+
+class Application : public wxApp {
+  bool OnInit() override {
+    auto frame1 = new wxFrame(nullptr, wxID_ANY, "Hello World Frame");
+    
+    auto panel1 = new wxPanel(frame1, wxID_ANY);
+    
+    auto button1 = new wxButton(panel1, wxID_ANY, "Click me", {10, 10});
+    button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
+      wxMessageDialog(nullptr, "Hello, World!").ShowModal();
+    });
+    
+    frame1->Show();
+    return true;
+  }
+};
+
+wxIMPLEMENT_APP(Application);
