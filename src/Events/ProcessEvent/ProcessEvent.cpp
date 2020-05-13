bc5c1682be925bbfcdbe7820a431796e7f7bf12f
Gammasoft71 <gammasoft71@gmail.com>
2020-05-13 10:04:04 +0200

    Update ProcessEvent.cpp

--- src/Events/ProcessEvent/ProcessEvent.cpp
+++ src/Events/ProcessEvent/ProcessEvent.cpp
@@ -2,25 +2,37 @@
 #include <wx/msgout.h>
 
 namespace Examples {
+#if defined(__WXOSX__)
+  int PointsToNativeFontGraphicsUntit(int size) {
+    return static_cast<float>(size) / wxScreenDC().GetPPI().GetHeight() * 96.0f;  // font is in pixels and not in points
+  }
+#else
+  int PointsToNativeFontGraphicsUntit(int size) {
+    return size;  // font is in points
+  }
+#endif
+
   class Frame1 : public wxFrame {
   public:
     Frame1() : wxFrame(nullptr, wxID_ANY, "Frame1") {
       SetClientSize(300, 300);
+      auto font = GetFont();
+      font.SetPointSize(PointsToNativeFontGraphicsUntit(18));
+      font.SetWeight(wxFontWeight::wxFONTWEIGHT_BOLD);
+      SetFont(font);
       Bind(wxEVT_PAINT, [&](wxPaintEvent& event) {
         wxPaintDC dc(this);
+        dc.SetPen(*wxTRANSPARENT_PEN);
         if (AppActive()) {
           dc.SetBrush({wxSystemSettings::GetColour(wxSystemColour::wxSYS_COLOUR_MENUHILIGHT)});
-          dc.SetPen(*wxTRANSPARENT_PEN);
           dc.DrawRectangle({10, 10, 280, 50});
           dc.DrawText("Application is active", 10, 10);
         } else {
           dc.SetBrush({wxSystemSettings::GetColour(wxSystemColour::wxSYS_COLOUR_BTNFACE)});
-          dc.SetPen(*wxTRANSPARENT_PEN);
           dc.DrawRectangle({10, 10, 280, 50});
           dc.DrawText("Application is inactive", 10, 10);
         }
       });
-      
     }
 
     bool AppActive() const {return appActive;}
