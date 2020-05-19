bcc1daef6a07010babbef2d4a0e7f6658323042c
Gammasoft71 <gammasoft71@gmail.com>
2020-05-19 22:14:20 +0200

    Update StaticBitmap.cpp

--- src/CommonControls/StaticBitmap/StaticBitmap.cpp
+++ src/CommonControls/StaticBitmap/StaticBitmap.cpp
@@ -34,7 +34,7 @@ namespace Examples {
     wxPanel* panel = new wxPanel(this);
     wxBoxSizer* boxSizer = new wxBoxSizer(wxVERTICAL);
     wxStaticBitmap* staticBitmap1 = new wxStaticBitmap(panel, wxID_ANY, wxNullBitmap);
-    wxChoice* choice1 = new wxChoice(panel, wxID_ANY, {20, 20});
+    wxChoice* choice1 = new wxChoice(panel, wxID_ANY);
   };
 
   class Application : public wxApp {
