ba4bff284c2b9ea1bbbe31863ed69a421aeafcf9
Gammasoft71 <gammasoft71@gmail.com>
2020-06-12 13:41:16 +0200

    Update ToolBar.cpp

--- src/MenusAndToolbars/ToolBar/ToolBar.cpp
+++ src/MenusAndToolbars/ToolBar/ToolBar.cpp
@@ -7,22 +7,28 @@ namespace Examples {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "Toolbar example") {
       SetClientSize(640, 480);
-      SetToolBar(toolBar);
-    
-      toolBar->AddTool(wxID_NEW, "&New", wxArtProvider::GetBitmap(wxART_NEW));
-      toolBar->AddTool(wxID_OPEN, "&Open...", wxArtProvider::GetBitmap(wxART_FILE_OPEN));
-      toolBar->AddTool(wxID_SAVE, "&Save", wxArtProvider::GetBitmap(wxART_FILE_SAVE));
+
+#if defined(__WXOSX__)
+      wxSize bitmapSize(-1, -1);
+#else
+      wxSize bitmapSize(16, 16);
+#endif
+      toolBar->SetToolBitmapSize(bitmapSize);
+      toolBar->AddTool(wxID_NEW, "&New", wxArtProvider::GetBitmap(wxART_NEW, wxART_TOOLBAR, bitmapSize));
+      toolBar->AddTool(wxID_OPEN, "&Open...", wxArtProvider::GetBitmap(wxART_FILE_OPEN, wxART_TOOLBAR, bitmapSize));
+      toolBar->AddTool(wxID_SAVE, "&Save", wxArtProvider::GetBitmap(wxART_FILE_SAVE, wxART_TOOLBAR, bitmapSize));
       toolBar->AddSeparator();
-      toolBar->AddTool(wxID_CUT, "Cu&t", wxArtProvider::GetBitmap(wxART_CUT));
-      toolBar->AddTool(wxID_COPY, "&Copy", wxArtProvider::GetBitmap(wxART_COPY));
-      toolBar->AddTool(wxID_PASTE, "&Paste", wxArtProvider::GetBitmap(wxART_PASTE));
+      toolBar->AddTool(wxID_CUT, "Cu&t", wxArtProvider::GetBitmap(wxART_CUT, wxART_TOOLBAR, bitmapSize));
+      toolBar->AddTool(wxID_COPY, "&Copy", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR, bitmapSize));
+      toolBar->AddTool(wxID_PASTE, "&Paste", wxArtProvider::GetBitmap(wxART_PASTE, wxART_TOOLBAR, bitmapSize));
       toolBar->AddSeparator();
-      toolBar->AddTool(wxID_PRINT, "&Print...", wxArtProvider::GetBitmap(wxART_PRINT));
+      toolBar->AddTool(wxID_PRINT, "&Print...", wxArtProvider::GetBitmap(wxART_PRINT, wxART_TOOLBAR, bitmapSize));
       toolBar->AddSeparator();
       toolBar->AddControl(choice);
       toolBar->AddStretchableSpace();
-      toolBar->AddTool(wxID_ABOUT, "&About...", wxArtProvider::GetBitmap(wxART_HELP));
+      toolBar->AddTool(wxID_ABOUT, "&About...", wxArtProvider::GetBitmap(wxART_HELP, wxART_TOOLBAR, bitmapSize));
 
+      SetToolBar(toolBar);
       toolBar->Realize();
       toolBar->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
         static std::map<int, std::string> names = {{wxID_NEW, "New"}, {wxID_OPEN, "Open..."}, {wxID_SAVE, "Save"}, {wxID_EXIT, "Quit"}, {wxID_CUT, "Cut"}, {wxID_COPY, "Copy"}, {wxID_PASTE, "Paste"}, {wxID_PRINT, "Print..."}, {wxID_ABOUT, "About..."}, {wxID_ANY, "Any"}};
@@ -41,7 +47,7 @@ namespace Examples {
 
   private:
     wxListBox* listBox1 = new wxListBox(this, wxID_ANY, {10, 10});
-    wxToolBar* toolBar = new wxToolBar(this, wxID_ANY, wxDefaultPosition, {100, 25});
+    wxToolBar* toolBar = new wxToolBar(this, wxID_ANY);
     wxChoice* choice = new wxChoice(toolBar, wxID_ANY);
   };
 
