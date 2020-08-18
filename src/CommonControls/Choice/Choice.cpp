79b4552c8f9177f20f35aba699cfe1287f026a03
Gammasoft71 <gammasoft71@gmail.com>
2020-08-18 23:38:02 +0200

    Update Choice.cpp

--- src/CommonControls/Choice/Choice.cpp
+++ src/CommonControls/Choice/Choice.cpp
@@ -7,22 +7,20 @@ namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "Choice example", wxDefaultPosition, {300, 300}) {
-      for (auto item : {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"})
-        choice1->Append(item);
+      choice1->Append({"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
       choice1->SetSelection(0);
-      choice1->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
-        choice2->SetSelection(choice1->GetSelection());
-      });
+      choice1->Bind(wxEVT_CHOICE, &Frame::OnChoicClick, this);
 
-      for (auto item : {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"})
-        choice2->Append(item);
+      choice2->Append({"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
       choice2->SetSelection(0);
-      choice2->Bind(wxEVT_CHOICE, [&](wxCommandEvent& e) {
-        choice1->SetSelection(choice2->GetSelection());
-      });
+      choice2->Bind(wxEVT_CHOICE, &Frame::OnChoicClick, this);
     }
     
   private:
+    void OnChoicClick(wxCommandEvent& e) {
+      choice1->SetSelection(static_cast<wxChoice*>(e.GetEventObject())->GetSelection());
+      choice2->SetSelection(static_cast<wxChoice*>(e.GetEventObject())->GetSelection());
+    }
     wxPanel* panel = new wxPanel(this);
     wxChoice* choice1 = new wxChoice(panel, wxID_ANY, {10, 10});
     wxChoice* choice2 = new wxChoice(panel, wxID_ANY, {10, 50});
