0f66c849c3b4e61057eab253262d1d4034621548
Gammasoft71 <gammasoft71@gmail.com>
2020-08-20 15:51:49 +0200

    fix task bar icon example

--- src/CommonControls/TaskBarIcon/TaskBarIcon.cpp
+++ src/CommonControls/TaskBarIcon/TaskBarIcon.cpp
@@ -1,12 +1,40 @@
 #include "Gammasoft.xpm"
-#include "wxNotifyIcon.h"
+#include <wx/wx.h>
+#include <wx/taskbar.h>
+
+// Workaround : with wxWidgets version <= 3.1.4 wxTaskBarIcon mouse double click doesn't work on macOS
+class TaskBarIcon : public wxTaskBarIcon {
+public:
+  TaskBarIcon(wxTaskBarIconType iconType = wxTBI_DEFAULT_TYPE) : wxTaskBarIcon(iconType) {
+    timer.Bind(wxEVT_TIMER, [&](wxTimerEvent& event) {
+      wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_DOWN, this));
+      wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_UP, this));
+    });
+  }
+  
+protected:
+  wxMenu* CreatePopupMenu() override {
+    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") {
+      if (!timer.IsRunning()) {
+        timer.StartOnce(wxSystemSettings::GetMetric(wxSYS_DCLICK_MSEC));
+      } else {
+        timer.Stop();
+        wxPostEvent(this, wxTaskBarIconEvent(wxEVT_TASKBAR_LEFT_DCLICK, this));
+      }
+    }
+    return wxTaskBarIcon::CreatePopupMenu();
+  }
+  
+private:
+  wxTimer timer;
+};
 
 namespace Examples {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "NotifyIcon example", wxDefaultPosition, {300, 300}) {
-      notifyIcon1.SetIcon({GammasoftIcon});
-      notifyIcon1.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
+    Frame() : wxFrame(nullptr, wxID_ANY, "TaskBarIcon example", wxDefaultPosition, {300, 300}) {
+      taskBarIcon.SetIcon({GammasoftIcon});
+      taskBarIcon.Bind(wxEVT_TASKBAR_LEFT_DCLICK, [&](wxTaskBarIconEvent& event) {
         Show(!IsShown());
       });
     }
@@ -14,7 +42,8 @@ namespace Examples {
   private:
     wxPanel* panel = new wxPanel(this);
     wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "Double click on Gammasoft tray icon\nto show or hide this frame.", {10, 10});
-    wxNotifyIcon notifyIcon1;
+    //wxTaskBarIcon taskBarIcon;
+    TaskBarIcon taskBarIcon;
   };
 
   class Application : public wxApp {
