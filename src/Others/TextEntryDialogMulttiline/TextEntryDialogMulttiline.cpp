b4c2af3e0f184f97fc8eda424abe820c7c59cf47
Gammasoft71 <gammasoft71@gmail.com>
2020-10-17 17:22:10 +0200

    add TextEntryDialogMultiline example

---
+++ src/Others/TextEntryDialogMulttiline/TextEntryDialogMulttiline.cpp
@@ -0,0 +1,29 @@
+#include <wx/wx.h>
+#include <wx/textdlg.h>
+
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "TextEntryDialog multiline example", wxDefaultPosition, {300, 300}) {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        wxTextEntryDialog textEntryDialog(this, "Message text", "Caption text", label->GetLabel(), wxTextEntryDialogStyle|wxTE_MULTILINE);
+        if (textEntryDialog.ShowModal() == wxID_OK)
+          label->SetLabel(textEntryDialog.GetValue());
+      });
+    }
+
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Text...", {10, 10});
+    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "line 1\nline 2", {10, 50});
+  };
+
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
