20a9f244fe52c7063dc8425db5e2b3e0784210b3
Gammasoft71 <gammasoft71@gmail.com>
2020-10-23 21:29:51 +0200

    Add StatusBar example

---
+++ src/MenusAndToolbars/StatusBar/StatusBar.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <wx/wx.h>
+#include <wx/artprov.h>
+
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "Statusbar example") {
+      SetClientSize(640, 480);
+      SetStatusBar(statusBar);
+    
+      std::vector statusWidths = {80, 80, -1};
+      std::vector statusStyles = {wxSB_SUNKEN, wxSB_SUNKEN, wxSB_SUNKEN};
+      
+      statusBar->SetFieldsCount(statusWidths.size());
+      statusBar->SetStatusWidths(statusWidths.size(), statusWidths.data());
+      statusBar->SetStatusStyles(statusStyles.size(), statusStyles.data());
+      
+      statusBar->SetStatusText("Status One");
+      statusBar->SetStatusText("Status Two", 1);
+      statusBar->SetStatusText("Status Three", 2);
+    }
+
+  private:
+    wxStatusBar* statusBar = new wxStatusBar(this);
+  };
+
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
