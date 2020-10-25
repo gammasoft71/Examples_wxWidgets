72d94d32e0cf11a8ba3fda39fd682385b157a2c1
Gammasoft71 <gammasoft71@gmail.com>
2020-10-25 13:08:10 +0100

    Update BitmapButton.cpp

--- src/CommonControls/BitmapButton/BitmapButton.cpp
+++ src/CommonControls/BitmapButton/BitmapButton.cpp
@@ -1,36 +1,32 @@
 #include <wx/app.h>
 #include <wx/bmpbuttn.h>
-#include <wx/button.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
+#include <wx/stattext.h>
 #include "gammasoft_16x16.xpm"
-#include "gammasoft_32x32.xpm"
 #include "gammasoft_64x64.xpm"
 
 namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "BitmapButton example", wxDefaultPosition, {300, 300}) {
-      button1->SetSize(100, 25);
+      button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        staticText1->SetLabel(wxString::Format("button1 clicked %d times", ++button1Clicked));
+      });
       
-      button2->SetBitmapLabel({gammasoft_16x16_xpm});
-      button2->SetBitmapPosition(wxLEFT);
-      button2->SetSize(120, 25);
-      
-      button3->SetBitmapLabel({gammasoft_32x32_xpm});
-      button3->SetBitmapPosition(wxLEFT);
-      button3->SetSize(140, 40);
-
-      button4->SetBitmapLabel({gammasoft_64x64_xpm});
-      button4->SetSize(70, 70);
+      button2->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        staticText2->SetLabel(wxString::Format("button2 clicked %d times", ++button2Clicked));
+      });
     }
     
   private:
     wxPanel* panel = new wxPanel(this);
-    wxButton* button1 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 50});
-    wxButton* button2 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 80});
-    wxButton* button3 = new wxButton(panel, wxID_ANY, "Gammasoft", {50, 110});
-    wxButton* button4 = new wxButton(panel, wxID_ANY, wxEmptyString, {50, 155});
+    wxBitmapButton* button1 = new wxBitmapButton(panel, wxID_ANY, {gammasoft_16x16_xpm}, {50, 50});
+    wxBitmapButton* button2 = new wxBitmapButton(panel, wxID_ANY, {gammasoft_64x64_xpm}, {50, 100});
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "button1 clicked 0 times", {50, 200}, {200, 20});
+    wxStaticText* staticText2 = new wxStaticText(panel, wxID_ANY, "button2 clicked 0 times", {50, 230}, {200, 20});
+    int button1Clicked = 0;
+    int button2Clicked = 0;
   };
 
   class Application : public wxApp {
