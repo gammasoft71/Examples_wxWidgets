e3472e16823cfd229f798e9444485244aed21b0b
Gammasoft71 <gammasoft71@gmail.com>
2020-11-01 00:25:42 +0100

    Update GenericProgressDialog.cpp

--- src/Dialogs/GenericProgressDialog/GenericProgressDialog.cpp
+++ src/Dialogs/GenericProgressDialog/GenericProgressDialog.cpp
@@ -12,7 +12,7 @@ namespace Examples {
       buttonProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         wxGenericProgressDialog progressDialog("Process running", "Please wait...", 100, this, wxPD_APP_MODAL|wxPD_AUTO_HIDE|wxPD_CAN_SKIP|wxPD_REMAINING_TIME);
         for (auto step = 1; step <= progressDialog.GetRange(); ++step) {
-          progressDialog.Update(step, wxString::Format("Step %d/%d ==> %s", step, progressDialog.GetRange(), progressDialog.WasSkipped() ? "skipped" : "done"));
+          progressDialog.Update(step, wxString::Format("Step %d/%d ==> %s", progressDialog.GetValue(), progressDialog.GetRange(), progressDialog.WasSkipped() ? "skipped" : "done"));
           this_thread::sleep_for(100ms);
         }
       });
