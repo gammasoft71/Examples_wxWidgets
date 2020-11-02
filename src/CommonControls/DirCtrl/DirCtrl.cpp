9097a363ab2fea68fc90b1fa569202add23d3f5c
Gammasoft71 <gammasoft71@gmail.com>
2020-11-02 20:58:04 +0100

    add DirCtrl example

---
+++ src/CommonControls/DirCtrl/DirCtrl.cpp
@@ -0,0 +1,35 @@
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
+#include <wx/stdpaths.h>
+#include <wx/dirctrl.h>
+
+namespace Examples {
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame(nullptr, wxID_ANY, "DirCtrl example") {
+      SetClientSize({ 350, 450 });
+      dirCtrl1->SetPosition({10, 10});
+      dirCtrl1->SetSize({330, 390});
+      dirCtrl1->Bind(wxEVT_DIRCTRL_SELECTIONCHANGED, [&](wxTreeEvent& event) {
+        staticText1->SetLabel(dirCtrl1->GetPath());
+      });
+      dirCtrl1->SetPath(wxStandardPaths::Get().GetDocumentsDir());
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel(this);
+    wxGenericDirCtrl* dirCtrl1 = new wxGenericDirCtrl(panel, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxDIRCTRL_DIR_ONLY|wxDIRCTRL_EDIT_LABELS);
+    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "", {10, 410});
+  };
+
+  class Application : public wxApp {
+    bool OnInit() override {
+      (new Frame())->Show();
+      return true;
+    }
+  };
+}
+
+wxIMPLEMENT_APP(Examples::Application);
