d58270923b62c836c8d2eea7cd8c07022d6e0000
Gammasoft71 <gammasoft71@gmail.com>
2020-11-03 12:36:44 +0100

    Update ToggleButton.cpp

--- src/CommonControls/ToggleButton/ToggleButton.cpp
+++ src/CommonControls/ToggleButton/ToggleButton.cpp
@@ -7,20 +7,20 @@ namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "ToggleButton example", wxDefaultPosition, {300, 300}) {
-      toggleButton1->SetValue(true);
       toggleButton1->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
-        toggleButton1->SetLabel(toggleButton1->GetValue() ? "Checked" : "Unchecked");
+        toggleButton1->SetValue(false);
       });
 
+      toggleButton2->SetValue(true);
       toggleButton2->Bind(wxEVT_TOGGLEBUTTON, [&](wxCommandEvent& event) {
-        toggleButton2->SetValue(false);
+        toggleButton2->SetLabel(toggleButton2->GetValue() ? "Checked" : "Unchecked");
       });
     }
     
   private:
     wxPanel* panel = new wxPanel(this);
-    wxToggleButton* toggleButton1 = new wxToggleButton(panel, wxID_ANY, "Checked", {30, 30});
-    wxToggleButton* toggleButton2 = new wxToggleButton(panel, wxID_ANY, "Unchecked", {30, 60});
+    wxToggleButton* toggleButton1 = new wxToggleButton(panel, wxID_ANY, "Unchecked", {30, 30});
+    wxToggleButton* toggleButton2 = new wxToggleButton(panel, wxID_ANY, "Checked", {30, 60});
   };
 
   class Application : public wxApp {
