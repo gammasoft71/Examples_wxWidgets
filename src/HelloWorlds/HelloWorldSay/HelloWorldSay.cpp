213f6de7dbba208208e9b2810600e17586daae52
gammasoft71 <gammasoft71@gmail.com>
2020-11-06 15:37:31 +0100

    Update HelloWorldSay.cpp

--- src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
+++ src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
@@ -8,21 +8,8 @@ using namespace std::filesystem;
 class Frame1 : public wxFrame {
 public:
   Frame1() : wxFrame(nullptr, wxID_ANY, "Hello world (say)") {
-    button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
-      if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxProcess::Open("say.bat \"Hello world\"");
-      else wxProcess::Open("say \"Hello world\"");
-      });
-  }
-
-private:
-  wxPanel* panel1 = new wxPanel(this, wxID_ANY);
-  wxButton* button1 = new wxButton(panel1, wxID_ANY, "Say...", { 10, 10 });
-};
-
-class Application : public wxApp {
-  bool OnInit() override {
     if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows")
-      wxFile("say.bat", wxFile::write).Write(
+      wxFile("say.cmd", wxFile::write).Write(
         "@echo off\n"
         "echo Dim Speak >> %TEMP%\\speak.vbs\n"
         "echo Set Speak=CreateObject(\"sapi.spvoice\") >> %TEMP%\\speak.vbs\n"
@@ -37,6 +24,31 @@ class Application : public wxApp {
       );
       permissions("say", perms::owner_all);
     }
+    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") {
+      wxFile("say", wxFile::write).Write(
+        "#!/bin/bash\n"
+        "say \"$*\""
+      );
+      permissions("say", perms::owner_all);
+    }
+
+    button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
+      if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxProcess::Open("say.cmd \"Hello world\"");
+      else wxProcess::Open("./say \"Hello world\"");
+      });
+  }
+
+  ~Frame1() {
+    remove(path(wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows" ? "say.cmd" : "say"));
+  }
+
+private:
+  wxPanel* panel1 = new wxPanel(this, wxID_ANY);
+  wxButton* button1 = new wxButton(panel1, wxID_ANY, "Say...", { 10, 10 });
+};
+
+class Application : public wxApp {
+  bool OnInit() override {
     (new Frame1)->Show();
     return true;
   }
