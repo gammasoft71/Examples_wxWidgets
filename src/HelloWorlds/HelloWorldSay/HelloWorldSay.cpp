6dc0a44bc2e362943d12eafcc4c3a76b637f6212
Gammasoft71 <gammasoft71@gmail.com>
2020-11-06 20:07:57 +0100

    Update HelloWorldSay.cpp

--- src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
+++ src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
@@ -8,25 +8,9 @@ using namespace std::filesystem;
 class Frame1 : public wxFrame {
 public:
   Frame1() : wxFrame(nullptr, wxID_ANY, "Hello world (say)") {
-    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows")
-      wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write(
-        "@echo off\n"
-        "echo Dim Speak >> %TEMP%\\speak.vbs\n"
-        "echo Set Speak=CreateObject(\"sapi.spvoice\") >> %TEMP%\\speak.vbs\n"
-        "echo Speak.Speak %*>> %TEMP%\\speak.vbs\n"
-        "%TEMP%\\speak.vbs\n"
-        "del %TEMP%\\speak.vbs"
-      );
-    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Unix")
-      wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write(
-        "#!/bin/bash\n"
-        "spd-say \"$*\""
-      );
-    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh")
-      wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write(
-        "#!/bin/bash\n"
-        "say \"$*\""
-      );
+    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak %* >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
+    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("say $*");
+    else wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("spd-say $*");
     permissions(temp_directory_path()/"say.cmd", perms::owner_all);
 
     button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
