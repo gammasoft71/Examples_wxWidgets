57e1cf7e908da41e027955d4bf34a2426601cc5a
Gammasoft71 <gammasoft71@gmail.com>
2020-12-05 17:33:49 +0100

    Update CollapsiblePane.cpp

--- src/ContainerControls/CollapsiblePane/CollapsiblePane.cpp
+++ src/ContainerControls/CollapsiblePane/CollapsiblePane.cpp
@@ -5,20 +5,46 @@ namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "CollapsiblePane example") {
-      collapsiblePane1->SetSize(200, 100);
-      button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        collapsiblePane1->Collapse();
+      boxSizer->Add(collapsiblePane1, wxSizerFlags(0).Expand());
+      boxSizer->Add(collapsiblePane2, wxSizerFlags(1).Expand());
+      panel->SetSizerAndFit(boxSizer);
+
+      buttonAdd->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        auto item = new wxStaticText(collapsiblePane2->GetPane(), wxID_ANY, wxString::Format("item %d", ++count));
+        boxSizerCollapsiblePane2->Add(item);
+        boxSizerCollapsiblePane2->Layout();
+      });
+      buttonRemove->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        if (collapsiblePane2->GetPane()->GetChildren().size() > 0) {
+          collapsiblePane2->GetPane()->GetChildren()[0]->Destroy();
+          boxSizerCollapsiblePane2->Layout();
+        }
+      });
+      buttonExpand->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        collapsiblePane2->Collapse(!collapsiblePane2->IsCollapsed());
+      });
+
+      collapsiblePane1->Bind(wxEVT_COLLAPSIBLEPANE_CHANGED, [&](wxCollapsiblePaneEvent& e) {
+        boxSizer->Layout();
       });
       
-      collapsiblePane1->Bind(wxEVT_COLLAPSIBLEPANE_CHANGED, [&](wxCollapsiblePaneEvent& e) {
-        wxMessageOutputDebug().Printf("collapsiblePane1 is %s", e.GetCollapsed() ? "collapsed" : "expanded");
+      collapsiblePane2->Bind(wxEVT_COLLAPSIBLEPANE_CHANGED, [&](wxCollapsiblePaneEvent& e) {
+        boxSizer->Layout();
       });
+      
+      collapsiblePane2->GetPane()->SetSizerAndFit(boxSizerCollapsiblePane2);
     }
     
   private:
+    int count = 0;
     wxPanel* panel = new wxPanel(this);
-    wxCollapsiblePane* collapsiblePane1 = new wxCollapsiblePane(panel, wxID_ANY, "CollapsiblePane1", {10, 10});
-    wxButton* button1 = new wxButton(collapsiblePane1->GetPane(), wxID_ANY, "button1", {10, 10});
+    wxBoxSizer* boxSizer = new wxBoxSizer(wxVERTICAL);
+    wxCollapsiblePane* collapsiblePane1 = new wxCollapsiblePane(panel, wxID_ANY, "CollapsiblePane1");
+    wxCollapsiblePane* collapsiblePane2 = new wxCollapsiblePane(panel, wxID_ANY, "CollapsiblePane2");
+    wxButton* buttonAdd = new wxButton(collapsiblePane1->GetPane(), wxID_ANY, "Add", {10, 0});
+    wxButton* buttonRemove = new wxButton(collapsiblePane1->GetPane(), wxID_ANY, "Remove", {10, 30});
+    wxButton* buttonExpand = new wxButton(collapsiblePane1->GetPane(), wxID_ANY, "Expand", {10, 60});
+    wxBoxSizer* boxSizerCollapsiblePane2 = new wxBoxSizer(wxVERTICAL);
   };
 
   class Application : public wxApp {
