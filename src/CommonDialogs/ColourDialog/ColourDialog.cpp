316e13df2c9c85d4ff59e422a8d73a9fc118452a
Gammasoft <gammasoft71@gmail.com>
2021-04-27 11:28:55 +0200

    Update ColourDialog.cpp

--- src/CommonDialogs/ColourDialog/ColourDialog.cpp
+++ src/CommonDialogs/ColourDialog/ColourDialog.cpp
@@ -1,5 +1,8 @@
+#include <vector>
 #include <wx/wx.h>
 #include <wx/colordlg.h>
+
+using namespace std;
 
 namespace Examples {
   class Frame : public wxFrame {
@@ -8,17 +11,22 @@ namespace Examples {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         wxColourData colourData;
         colourData.SetColour(GetBackgroundColour());
+        for (int index = 0; index < static_cast<int>(customColours.size()); index++)
+          colourData.SetCustomColour(index, customColours[index]);
         wxColourDialog colourDialog(this, &colourData);
         if (colourDialog.ShowModal() == wxID_OK) {
           SetBackgroundColour(colourDialog.GetColourData().GetColour());
           Refresh();
         }
+        for (int index = 0; index < static_cast<int>(customColours.size()); index++)
+          customColours[index], colourData.GetCustomColour(index);
       });
     }
 
   private:
     wxPanel* panel = new wxPanel(this);
     wxButton* button = new wxButton(panel, wxID_ANY, "Color...", {10, 10});
+    vector<wxColour> customColours {wxColour(0xFF, 0x00, 0x00), wxColour(0x00, 0xFF, 0x00), wxColour(0x00, 0x00, 0xFF), wxColour(0xFF, 0xFF, 0x00), wxColour(0xF0, 0xF0, 0xF0), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF)};
   };
 
   class Application : public wxApp {
