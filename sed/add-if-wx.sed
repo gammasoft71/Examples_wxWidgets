091c1c421f8968a573f0b3f76f0816a58b98c3e2
Gammasoft <gammasoft71@gmail.com>
2021-05-17 16:00:43 +0200

    formatting

--- sed/add-if-wx.sed
+++
@@ -1,4 +0,0 @@
-#!/bin/env sed
-
-/find_package(wx.*)/ i if("${CMAKE_CURRENT_SOURCE_DIR}" STREQUAL "${CMAKE_SOURCE_DIR}")
-/include(${wxWidgets_USE_FILE})/ a endif()
--- sed/add-if-wx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env sh
-#
-# Apply the sed script to all CMakeLists.txt (except the root CMakeLists.txt).
-#
-find src/ -type f -iname "CMakeLists.txt" | xargs sed -i -f sed/add-if-wx.sed
