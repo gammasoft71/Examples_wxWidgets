23e59d45075c90971ee30d7beca26d5778e7643f
Gammasoft <gammasoft71@gmail.com>
2021-05-27 11:14:15 +0200

    Update TestGui.cpp

--- src/Tests/TestGui/TestGui.cpp
+++ src/Tests/TestGui/TestGui.cpp
@@ -1,13 +1,19 @@
 #include "Application.h"
 #include <wx/wx.h>
+#include <wx/display.h>
+#include <wx/log.h>
 
 class MainFrame : public wxFrame {
 public:
   MainFrame() : wxFrame {nullptr, wxID_ANY, wxEmptyString} {
+    button->Bind(wxEVT_BUTTON, [](wxCommandEvent& e) {
+      wxLogMessage("Processus error");
+    });
   }
 
 private:
   wxPanel* mainPanel = new wxPanel {this};
+  wxButton* button = new wxButton {mainPanel, wxID_ANY, "Click", {10, 10}};
 };
 
 int main() {
