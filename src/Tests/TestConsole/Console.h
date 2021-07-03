13ac11b11d41f9185fe0e7dea975c1f58b197b33
Gammasoft <gammasoft71@gmail.com>
2021-07-03 11:07:24 +0200

    fix testConsole

---
+++ src/Tests/TestConsole/Console.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <wx/app.h>
+
+class wxConsole : protected wxApp {
+public:
+  wxConsole() : wxConsole(substituteArgc, nullptr) {}
+  wxConsole(int& argc, char** argv) {
+    wxDISABLE_DEBUG_SUPPORT();
+    wxApp::SetInstance(this);
+    wxEntryStart(argc, argv);
+    CallOnInit();
+    wxInitAllImageHandlers();
+  }
+  
+protected:
+  int OnExit() override {
+    wxImage::CleanUpHandlers();
+    wxEntryCleanup();
+    wxApp::SetInstance(nullptr);
+    return wxApp::OnExit();
+  }
+  
+private:
+  int substituteArgc = 0;
+};
--- src/Tests/TestConsole/TestConsole.cpp
+++ src/Tests/TestConsole/TestConsole.cpp
@@ -1,8 +1,9 @@
+#include "Console.h"
 #include <wx/wx.h>
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
+  auto console = new wxConsole;
   cout << wxString::Format("%s", "Hello, World!") << endl;
-  wxPrintf("%s", "Hello, World!\n");
 }
