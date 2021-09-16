20db369477d2842f852110616d0e14c618190e97
Gammasoft <gammasoft71@gmail.com>
2021-09-16 14:26:12 +0200

    Fix file path

--- src/CommonControls/AnimationCtrl/AnimationCtrl.cpp
+++ src/CommonControls/AnimationCtrl/AnimationCtrl.cpp
@@ -2,17 +2,25 @@
 #include <wx/frame.h>
 #include <wx/panel.h>
 #include <wx/animate.h>
+#include <wx/filename.h>
+#include <wx/stdpaths.h>
 
 namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "AnimationCtrl example") {
+      wxFileName imagePath(wxStandardPaths::Get().GetExecutablePath());
+      if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") imagePath.AppendDir("..");
+      imagePath.AppendDir("Resources");
+      imagePath.SetFullName("load");
+      imagePath.SetExt("gif");
+      animationCtrl->SetAnimation(wxAnimation(imagePath.GetFullPath()));
       SetClientSize(animationCtrl->GetSize());
       animationCtrl->Play();
     }
     
   private:
-    wxAnimationCtrl* animationCtrl = new wxAnimationCtrl(this, wxID_ANY, wxAnimation(wxPlatformInfo::Get().GetOperatingSystemFamilyName() != "Macintosh" ? "Resources/load.gif" : "../Resources/load.gif"));
+    wxAnimationCtrl* animationCtrl = new wxAnimationCtrl(this, wxID_ANY, wxAnimation());
   };
 
   class Application : public wxApp {
