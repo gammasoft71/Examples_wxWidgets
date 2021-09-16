c1c3b6e3f245a31499887da148befd1254a7b143
Gammasoft <gammasoft71@gmail.com>
2021-09-16 13:18:59 +0200

    Fix issue #9

--- src/Applications/ApplicationIcon/ApplicationIcon.cpp
+++ src/Applications/ApplicationIcon/ApplicationIcon.cpp
@@ -1,11 +1,18 @@
 #include <wx/wx.h>
+#include <wx/filename.h>
+#include <wx/stdpaths.h>
 
 namespace Examples {
   class Application : public wxApp {
     bool OnInit() override {
       wxFrame* frame = new wxFrame(nullptr, wxID_ANY, wxEmptyString);
       wxImage::AddHandler(new wxPNGHandler());
-      frame->SetIcon({"Resources/Gammasoft.png", wxBITMAP_TYPE_PNG});
+      wxFileName imagePath(wxStandardPaths::Get().GetExecutablePath());
+      if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") imagePath.AppendDir("..");
+      imagePath.AppendDir("Resources");
+      imagePath.SetFullName("Gammasoft");
+      imagePath.SetExt("png");
+      frame->SetIcon({imagePath.GetFullPath(), wxBITMAP_TYPE_PNG});
       return frame->Show();
     }
   };
