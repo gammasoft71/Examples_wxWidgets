dba37c484741c08f88ab935226d47db207885e2e
Gammasoft <gammasoft71@gmail.com>
2021-12-29 21:04:01 +0100

    Update CalendarCtrl.cpp

--- src/CommonControls/CalendarCtrl/CalendarCtrl.cpp
+++ src/CommonControls/CalendarCtrl/CalendarCtrl.cpp
@@ -10,7 +10,25 @@ namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "MonthCalendar example") {
+      wxLogDebug(wxString::Format("min_date = %s", wxDateTime(1, wxDateTime::Jan, 0, 0, 0, 0, 0).Format()));
+      wxLogDebug(wxString::Format("min_date = %ld", wxDateTime(1, wxDateTime::Jan, 0, 0, 0, 0, 0).GetValue()));
       calendarCtrl1->SetDateRange({5, wxDateTime::Jan, 1971}, {5, wxDateTime::Jan, 2071});
+      //calendarCtrl1->SetSize(227, 140);
+      calendarCtrl1->Bind(wxEVT_CALENDAR_SEL_CHANGED, [&](wxCalendarEvent& e) {
+        wxLogDebug("Calendar Selection Changed");
+      });
+      calendarCtrl1->Bind(wxEVT_CALENDAR_DOUBLECLICKED, [&](wxCalendarEvent& e) {
+        wxLogDebug("Calendar double Cliccked");
+      });
+      calendarCtrl1->Bind(wxEVT_CALENDAR_PAGE_CHANGED, [&](wxCalendarEvent& e) {
+        wxLogDebug("Calendar Page Changed");
+      });
+      calendarCtrl1->Bind(wxEVT_CALENDAR_WEEKDAY_CLICKED, [&](wxCalendarEvent& e) {
+        wxLogDebug("Calendar Weekday Clicked");
+      });
+      calendarCtrl1->Bind(wxEVT_CALENDAR_WEEK_CLICKED, [&](wxCalendarEvent& e) {
+        wxLogDebug("Calendar Week Clicked");
+      });
     }
     
   private:
