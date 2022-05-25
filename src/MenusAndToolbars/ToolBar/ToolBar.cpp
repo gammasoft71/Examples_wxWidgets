a74b1a3c257ba82844ad322e0f45de9389f55630
Gammasoft <gammasoft71@gmail.com>
2022-05-25 21:11:35 +0200

    Update ToolBar.cpp

--- src/MenusAndToolbars/ToolBar/ToolBar.cpp
+++ src/MenusAndToolbars/ToolBar/ToolBar.cpp
@@ -7,9 +7,14 @@ namespace Examples {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "Toolbar example") {
       SetClientSize(820, 500);
-
-      // On Windows the default size icon of toolbar is 16x16 and not 24x24...
-      wxSize bitmapSize = wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows" ? wxSize(16, 16) : wxSize(24, 24);
+      
+      // On Windows the default size icon of toolbar is 16x16, on macOS is 32x32 and on gtk is 24x24...
+      if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows")
+        bitmapSize = wxSize(16, 16);
+      else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh")
+        bitmapSize = wxSize(32, 32);
+      else
+        bitmapSize = wxSize(24, 24);
       toolBar->AddTool(wxID_NEW, "&New", wxArtProvider::GetBitmap(wxART_NEW, wxART_TOOLBAR, bitmapSize));
       toolBar->AddTool(wxID_OPEN, "&Open...", wxArtProvider::GetBitmap(wxART_FILE_OPEN, wxART_TOOLBAR, bitmapSize));
       toolBar->AddTool(wxID_SAVE, "&Save", wxArtProvider::GetBitmap(wxART_FILE_SAVE, wxART_TOOLBAR, bitmapSize));
@@ -19,19 +24,25 @@ namespace Examples {
       toolBar->AddTool(wxID_COPY, "&Copy", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR, bitmapSize));
       toolBar->AddTool(wxID_PASTE, "&Paste", wxArtProvider::GetBitmap(wxART_PASTE, wxART_TOOLBAR, bitmapSize));
       toolBar->AddSeparator();
-      toolBar->AddControl(choice);
+      toolBar->AddControl(choice, "Items");
       toolBar->AddStretchableSpace();
-      toolBar->AddTool(wxID_ABOUT, "&About...", wxArtProvider::GetBitmap(wxART_HELP, wxART_TOOLBAR, bitmapSize));
-
+      /// Does not works on macOS : No DropDown menu supported !
+      toolBar->AddTool(wxID_HELP, "&Help", wxArtProvider::GetBitmap(wxART_HELP, wxART_TOOLBAR, bitmapSize), wxEmptyString, wxITEM_DROPDOWN);
+      toolBar->SetDropdownMenu(wxID_HELP, menuHelp);
+      
       SetToolBar(toolBar);
       toolBar->Realize();
       toolBar->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
-        static std::map<int, std::string> names = {{wxID_NEW, "New"}, {wxID_OPEN, "Open..."}, {wxID_SAVE, "Save"}, {wxID_EXIT, "Quit"}, {wxID_CUT, "Cut"}, {wxID_COPY, "Copy"}, {wxID_PASTE, "Paste"}, {wxID_PRINT, "Print..."}, {wxID_ABOUT, "About..."}, {wxID_ANY, "Any"}};
+        static std::map<int, std::string> names = {{wxID_NEW, "New"}, {wxID_OPEN, "Open..."}, {wxID_SAVE, "Save"}, {wxID_EXIT, "Quit"}, {wxID_CUT, "Cut"}, {wxID_COPY, "Copy"}, {wxID_PASTE, "Paste"}, {wxID_PRINT, "Print..."}, {wxID_HELP, "Help"}, {wxID_HELP_CONTEXT, "Help context"}, {wxID_HELP_INDEX, "Help index"}, {wxID_HELP_SEARCH, "Help search"}, {wxID_ABOUT, "About..."}, {wxID_ANY, "Any"}};
         listBox1->Append(wxString::Format("%s clicked", names.find(event.GetId()) != names.end() ? names[event.GetId()] : "Unknown"));
         listBox1->Select(listBox1->GetCount() - 1);
         if (event.GetId() == wxID_EXIT) Close();
       });
       
+      toolBar->Bind(wxEVT_TOOL_DROPDOWN, [&](wxCommandEvent& event) {
+        listBox1->Append(wxString::Format("DropDown clicked"));
+      });
+
       for (auto item : {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"})
         choice->Append(item);
       choice->SetSelection(0);
@@ -39,14 +50,22 @@ namespace Examples {
         listBox1->Append(wxString::Format("Choose item : %s", choice->GetStringSelection()));
         listBox1->Select(listBox1->GetCount() - 1);
       });
+      
+      menuHelp->Append(wxID_HELP_CONTEXT, "Context");
+      menuHelp->Append(wxID_HELP_INDEX, "Index");
+      menuHelp->Append(wxID_HELP_SEARCH, "Search");
+      menuHelp->AppendSeparator();
+      menuHelp->Append(wxID_ABOUT, "About");
     }
-
+    
   private:
-    wxListBox* listBox1 = new wxListBox(this, wxID_ANY, {10, 10});
-    wxToolBar* toolBar = new wxToolBar(this, wxID_ANY);
+    wxSize bitmapSize {24, 24};
+    wxListBox* listBox1 = new wxListBox(this, wxID_ANY);
+    wxToolBar* toolBar = new wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_HORIZONTAL);
     wxChoice* choice = new wxChoice(toolBar, wxID_ANY);
+    wxMenu* menuHelp = new wxMenu();
   };
-
+  
   class Application : public wxApp {
     bool OnInit() override {
       (new Frame())->Show();
