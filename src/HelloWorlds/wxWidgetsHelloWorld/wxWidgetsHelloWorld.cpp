7b44d47563ef4628e02ba840461746ea86ee4f88
Gammasoft <gammasoft71@gmail.com>
2022-06-22 22:22:08 +0200

    Update wxWidgetsHelloWorld.cpp

--- src/HelloWorlds/wxWidgetsHelloWorld/wxWidgetsHelloWorld.cpp
+++ src/HelloWorlds/wxWidgetsHelloWorld/wxWidgetsHelloWorld.cpp
@@ -12,14 +12,18 @@ namespace Examples {
       menuFile->Append(ID_Hello, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
       menuFile->AppendSeparator();
       menuFile->Append(wxID_EXIT);
+      
       auto menuHelp = new wxMenu();
       menuHelp->Append(wxID_ABOUT);
       auto menuBar = new wxMenuBar();
+      
       menuBar->Append(menuFile, "&File");
       menuBar->Append(menuHelp, "&Help");
       SetMenuBar(menuBar);
+      
       CreateStatusBar();
       SetStatusText("Welcome to wxWidgets!");
+      
       menuBar->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
         if (event.GetId() == ID_Hello) wxLogMessage("Hello world from wxWidgets!");
         else if (event.GetId() == wxID_ABOUT) wxMessageBox("This is a wxWidgets Hello World example", "About Hello World", wxOK|wxICON_INFORMATION);
