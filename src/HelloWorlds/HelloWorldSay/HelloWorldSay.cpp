bdabadc355d6b477cb88d3cef35e624d54c2badd
Gammasoft <gammasoft71@gmail.com>
2022-07-29 20:58:09 +0200

    Update HelloWorldSay.cpp

--- src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
+++ src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
@@ -9,8 +9,8 @@ class Frame1 : public wxFrame {
 public:
   Frame1() : wxFrame(nullptr, wxID_ANY, "Hello world (say)") {
     if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak %* >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
-    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("say $*");
-    else wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("spd-say $*");
+    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("say \"$*\"");
+    else wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("spd-say \"$*\"");
     permissions(temp_directory_path()/"say.cmd", perms::owner_all);
 
     button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
