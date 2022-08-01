72e7b0faad426bd0b39b6532d693ab8046a38db6
Gammasoft <gammasoft71@gmail.com>
2022-08-01 09:49:17 +0200

    Update HelloWorldSay.cpp

--- src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
+++ src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
@@ -8,17 +8,18 @@ using namespace std::filesystem;
 class Frame1 : public wxFrame {
 public:
   Frame1() : wxFrame(nullptr, wxID_ANY, "Hello world (say)") {
-    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak \"%*\" >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
-    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("say \"$*\"");
-    else wxFile((temp_directory_path()/"say.cmd").string(), wxFile::write).Write("spd-say \"$*\"");
-    permissions(temp_directory_path()/"say.cmd", perms::owner_all);
+    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile(sayCmdFile.string(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak \"%*\" >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
+    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile(sayCmdFile.string(), wxFile::write).Write("say \"$*\"");
+    else wxFile(sayCmdFile.string(), wxFile::write).Write("spd-say \"$*\"");
+    permissions(sayCmdFile, perms::owner_all);
 
-    button1->Bind(wxEVT_BUTTON, [](wxCommandEvent&) {
-      wxProcess::Open(wxString::Format("%s Hello, World!", (temp_directory_path()/"say.cmd").string()));
+    button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent&) {
+      wxProcess::Open(wxString::Format("%s Hello, World!", sayCmdFile.string()));
     });
   }
 
 private:
+  path sayCmdFile = temp_directory_path() / "say.cmd";
   wxPanel* panel1 = new wxPanel(this, wxID_ANY);
   wxButton* button1 = new wxButton(panel1, wxID_ANY, "Say...", {10, 10});
 };
