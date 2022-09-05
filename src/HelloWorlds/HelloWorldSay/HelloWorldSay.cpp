aaac3db7e3a4ed9282debc0b287eabafed0036c9
Gammasoft <gammasoft71@gmail.com>
2022-09-05 19:37:16 +0200

    Update HelloWorldSay.cpp

--- src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
+++ src/HelloWorlds/HelloWorldSay/HelloWorldSay.cpp
@@ -1,25 +1,23 @@
-#include <filesystem>
-#include <wx/wx.h>
-#include <wx/file.h>
+#include <wx/filename.h>
 #include <wx/process.h>
-
-using namespace std::filesystem;
+#include <wx/stdpaths.h>
+#include <wx/wx.h>
 
 class Frame1 : public wxFrame {
 public:
   Frame1() : wxFrame(nullptr, wxID_ANY, "Hello world (say)") {
-    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile(sayCmdFile.string(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak \"%*\" >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
-    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile(sayCmdFile.string(), wxFile::write).Write("say \"$*\"");
-    else wxFile(sayCmdFile.string(), wxFile::write).Write("spd-say \"$*\"");
-    permissions(sayCmdFile, perms::owner_all);
+    if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows") wxFile(sayCmdFile.GetFullPath(), wxFile::write).Write("@echo Set Speaker=CreateObject(\"sapi.spvoice\") > %TEMP%\\say.vbs\n@echo Speaker.Speak \"%*\" >> %TEMP%\\say.vbs\n@%TEMP%\\say.vbs");
+    else if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") wxFile(sayCmdFile.GetFullPath(), wxFile::write).Write("say \"$*\"");
+    else wxFile(sayCmdFile.GetFullPath(), wxFile::write).Write("spd-say \"$*\"");
+    sayCmdFile.SetPermissions(wxPOSIX_USER_READ | wxPOSIX_USER_WRITE | wxPOSIX_USER_EXECUTE);
 
     button1->Bind(wxEVT_BUTTON, [&](wxCommandEvent&) {
-      wxProcess::Open(wxString::Format("%s Hello, World!", sayCmdFile.string()));
+      wxProcess::Open(wxString::Format("%s Hello, World!", sayCmdFile.GetFullPath()));
     });
   }
 
 private:
-  path sayCmdFile = temp_directory_path() / "say.cmd";
+  wxFileName sayCmdFile {wxStandardPaths::Get().GetTempDir(), "say", "cmd"};
   wxPanel* panel1 = new wxPanel(this, wxID_ANY);
   wxButton* button1 = new wxButton(panel1, wxID_ANY, "Say...", {10, 10});
 };
