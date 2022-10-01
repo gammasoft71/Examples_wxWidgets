c5c209ee7f430496f3239a09f23bd6716a96e815
Gammasoft <gammasoft71@gmail.com>
2022-10-01 20:51:23 +0200

    Update TestGui.cpp

--- src/Tests/TestGui/TestGui.cpp
+++ src/Tests/TestGui/TestGui.cpp
@@ -30,25 +30,27 @@ public:
 
 private:
   void CreateFrame2() {
-    if (frame2 == nullptr) frame2 = new wxFrame(nullptr, wxID_ANY, wxString::Format("CloseCount = %d", closeCount));
-    
-    frame2->Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& e) {
-      if (cancelCloseButton->IsChecked()) {
-        e.SetCanVeto(true);
-        e.Veto();
-      } else {
-        closeCount++;
-        frame2 = nullptr;
-        e.Skip();
-      }
-    });
+    if (frame2 == nullptr) {
+      frame2 = new wxFrame(nullptr, wxID_ANY, wxString::Format("CloseCount = %d", closeCount));
+      
+      frame2->Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& e) {
+        if (cancelCloseButton->IsChecked()) {
+          e.SetCanVeto(true);
+          e.Veto();
+        } else {
+          closeCount++;
+          frame2 = nullptr;
+          e.Skip();
+        }
+      });
+    }
   }
   wxPanel* panel = new wxPanel(this);
   wxButton* closeButton = new wxButton(panel, wxID_ANY, "Close", {10, 10}, {100, 40});
   wxButton* showButton = new wxButton(panel, wxID_ANY, "Show", {10, 60}, {100, 40});
   wxButton* hideButton = new wxButton(panel, wxID_ANY, "Hide", {10, 110}, {100, 40});
   wxCheckBox* cancelCloseButton = new wxCheckBox(panel, wxID_ANY, "Cancel close", {10, 160}, {100, 40});
-  wxFrame* frame2 = new wxFrame(nullptr, wxID_ANY, "CloseCount = 0");
+  wxFrame* frame2 = nullptr;
   int closeCount = 0;
 };
 
