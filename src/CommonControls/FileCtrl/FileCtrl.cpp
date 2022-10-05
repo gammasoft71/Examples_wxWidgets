f93e336d772616c92dfa835cb0a3c69b3efab889
Gammasoft <gammasoft71@gmail.com>
2022-10-05 14:23:13 +0200

    Update FileCtrl.cpp

--- src/CommonControls/FileCtrl/FileCtrl.cpp
+++ src/CommonControls/FileCtrl/FileCtrl.cpp
@@ -30,8 +30,6 @@ public:
       GetFileList()->SetItemTextColour(event.GetIndex(), GetForegroundColour());
     });
 #endif
-    //m_static->Hide();
-    //m_check->Hide();
   }
   
   void ShowHidden(bool show) override {
