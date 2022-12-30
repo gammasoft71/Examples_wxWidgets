fd539add78e5c34db6c28a6dc48ce39b436c972c
Gammasoft <gammasoft71@gmail.com>
2022-12-30 20:30:12 +0100

    Add MessageDialog "Does not work on Windows"

--- src/Dialogs/GenericFindReplaceDialog/GenericFindReplaceDialog.cpp
+++ src/Dialogs/GenericFindReplaceDialog/GenericFindReplaceDialog.cpp
@@ -7,27 +7,32 @@ namespace Examples {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "GenericFindReplaceDialog example") {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+#if !defined(__WXMSW__)
         findReplaceData.SetFlags(wxFR_WHOLEWORD | wxFR_MATCHCASE | wxFR_DOWN);
-        findReplaceData.SetFindString("Gammasoft");
-        findReplaceData.SetReplaceString("Gammasoft71");
-        findReplaceDialog = new wxGenericFindReplaceDialog(this, &findReplaceData, "Find and replace", wxFR_REPLACEDIALOG);
-        findReplaceDialog->Bind(wxEVT_FIND_CLOSE, [&](wxFindDialogEvent& event) {
-          findReplaceDialog->Destroy();
+      findReplaceData.SetFindString("Gammasoft");
+      findReplaceData.SetReplaceString("Gammasoft71");
+      findReplaceDialog = new wxGenericFindReplaceDialog(this, &findReplaceData, "Find and replace", wxFR_REPLACEDIALOG);
+      findReplaceDialog->Bind(wxEVT_FIND_CLOSE, [&](wxFindDialogEvent& event) {
+        findReplaceDialog->Destroy();
         });
-        findReplaceDialog->Bind(wxEVT_FIND, [](wxFindDialogEvent& event) {
-          wxMessageOutputDebug().Printf("Find {%s}", event.GetFindString());
+      findReplaceDialog->Bind(wxEVT_FIND, [](wxFindDialogEvent& event) {
+        wxMessageOutputDebug().Printf("Find {%s}", event.GetFindString());
         });
-        findReplaceDialog->Bind(wxEVT_FIND_NEXT, [](wxFindDialogEvent& event) {
-          wxMessageOutputDebug().Printf("Find next {%s}", event.GetFindString());
+      findReplaceDialog->Bind(wxEVT_FIND_NEXT, [](wxFindDialogEvent& event) {
+        wxMessageOutputDebug().Printf("Find next {%s}", event.GetFindString());
         });
-        findReplaceDialog->Bind(wxEVT_FIND_REPLACE, [](wxFindDialogEvent& event) {
-          wxMessageOutputDebug().Printf("Find and Replace {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
+      findReplaceDialog->Bind(wxEVT_FIND_REPLACE, [](wxFindDialogEvent& event) {
+        wxMessageOutputDebug().Printf("Find and Replace {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
         });
-        findReplaceDialog->Bind(wxEVT_FIND_REPLACE_ALL, [](wxFindDialogEvent& event) {
-          wxMessageOutputDebug().Printf("Find and Replace all {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
+      findReplaceDialog->Bind(wxEVT_FIND_REPLACE_ALL, [](wxFindDialogEvent& event) {
+        wxMessageOutputDebug().Printf("Find and Replace all {%s} by {%s}", event.GetFindString(), event.GetReplaceString());
         });
 
-        findReplaceDialog->Show();
+      findReplaceDialog->Show();
+#else
+      wxMessageDialog messageDialog(this, "Does not work on Windows!", "Error", wxOK);
+      messageDialog.ShowModal();
+#endif
       });
     }
     
