34d7b3257c906d0a760b3ef2804a26687d6b4dae
Gammasoft <gammasoft71@gmail.com>
2022-12-30 16:41:59 +0100

    Add SetClientSize

--- src/CommonControls/ComboBox/ComboBox.cpp
+++ src/CommonControls/ComboBox/ComboBox.cpp
@@ -7,6 +7,7 @@ namespace Examples {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "ComboBox example") {
+      SetClientSize(400, 270);
       comboBox1->Append({"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
       comboBox1->Select(0);
       comboBox1->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
