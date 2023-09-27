fddf04ec21143ac8bea319a645b0b260a2d8b947
LongND <nguyenduclong12a2nd@gmail.com>
2023-09-27 14:47:46 +0700

    update: cmake 3.5 and wxWigets 3.2.2.1

--- src/Controls/ComboBox/ComboBox.cpp
+++ src/Controls/ComboBox/ComboBox.cpp
@@ -4,41 +4,57 @@
 #include <wx/panel.h>
 
 namespace ComboBoxExample {
-  class Frame : public wxFrame {
-  public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "ComboBox example") {
-      SetClientSize(400, 270);
-      comboBox1->Append(std::vector {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
-      comboBox1->Select(0);
-      comboBox1->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
-
-      comboBox2->Append(std::vector {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
-      comboBox2->Select(0);
-      comboBox2->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
-
-      comboBox3->Append(std::vector {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"});
-      comboBox3->Select(0);
-      comboBox3->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
+class Frame : public wxFrame {
+public:
+  Frame() : wxFrame(nullptr, wxID_ANY, "ComboBox example") {
+    SetClientSize(400, 270);
+    for (int i = 1; i <= 10; ++i) {
+      comboBox1->Append("item " + std::to_string(i));
     }
-    
-  private:
-    void OnComboBoxClick(wxCommandEvent& e) {
-      comboBox1->SetSelection(static_cast<wxComboBox*>(e.GetEventObject())->GetSelection());
-      comboBox2->SetSelection(static_cast<wxComboBox*>(e.GetEventObject())->GetSelection());
-      comboBox3->SetSelection(static_cast<wxComboBox*>(e.GetEventObject())->GetSelection());
-    }
-    wxPanel* panel = new wxPanel(this);
-    wxComboBox* comboBox1 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 10});
-    wxComboBox* comboBox2 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 50}, wxDefaultSize, 0, nullptr, wxCB_READONLY);
-    wxComboBox* comboBox3 = new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 90}, wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows" ? wxSize(110, 163) : wxDefaultSize, 0, nullptr, wxCB_SIMPLE);
-  };
+    comboBox1->Select(0);
+    comboBox1->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
 
-  class Application : public wxApp {
-    bool OnInit() override {
-      (new Frame())->Show();
-      return true;
+    for (int i = 1; i <= 10; ++i) {
+      comboBox2->Append("item " + std::to_string(i));
     }
-  };
-}
+    comboBox2->Select(0);
+    comboBox2->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
+    for (int i = 1; i <= 10; ++i) {
+      comboBox3->Append("item " + std::to_string(i));
+    }
+    comboBox3->Select(0);
+    comboBox3->Bind(wxEVT_COMBOBOX, &Frame::OnComboBoxClick, this);
+  }
+
+private:
+  void OnComboBoxClick(wxCommandEvent &e) {
+    comboBox1->SetSelection(
+        static_cast<wxComboBox *>(e.GetEventObject())->GetSelection());
+    comboBox2->SetSelection(
+        static_cast<wxComboBox *>(e.GetEventObject())->GetSelection());
+    comboBox3->SetSelection(
+        static_cast<wxComboBox *>(e.GetEventObject())->GetSelection());
+  }
+  wxPanel *panel = new wxPanel(this);
+  wxComboBox *comboBox1 =
+      new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 10});
+  wxComboBox *comboBox2 =
+      new wxComboBox(panel, wxID_ANY, wxEmptyString, {10, 50}, wxDefaultSize, 0,
+                     nullptr, wxCB_READONLY);
+  wxComboBox *comboBox3 = new wxComboBox(
+      panel, wxID_ANY, wxEmptyString, {10, 90},
+      wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Windows"
+          ? wxSize(110, 163)
+          : wxDefaultSize,
+      0, nullptr, wxCB_SIMPLE);
+};
+
+class Application : public wxApp {
+  bool OnInit() override {
+    (new Frame())->Show();
+    return true;
+  }
+};
+} // namespace ComboBoxExample
 
 wxIMPLEMENT_APP(ComboBoxExample::Application);
