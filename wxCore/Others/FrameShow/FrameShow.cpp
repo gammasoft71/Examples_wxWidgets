1713e7b1b1cc40b762d8b50cffe0d86502727c36
Gammasoft <gammasoft71@gmail.com>
2024-01-22 20:56:39 +0100

    Update FrameShow.cpp

--- wxCore/Others/FrameShow/FrameShow.cpp
+++ wxCore/Others/FrameShow/FrameShow.cpp
@@ -5,34 +5,36 @@ namespace FrameShowExample {
   class Window : public wxFrame {
   public:
     Window() : wxFrame(nullptr, wxID_ANY, "Frame show example") {
-      SetClientSize(320, 425);
+      SetClientSize(320, 400);
       Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
         wxTheApp->Exit();
       });
 
       panel->SetSizerAndFit(boxSizer);
-      boxSizer->Add(buttonNormal, 1, wxGROW|wxALL, 5);
-      boxSizer->Add(buttonModeless, 1, wxGROW|wxALL, 5);
-      boxSizer->Add(buttonStayOnTop, 1, wxGROW|wxALL, 5);
-      boxSizer->Add(buttonModal, 1, wxGROW|wxALL, 5);
+      panel->SetBackgroundColour(wxSystemSettings::GetColour(wxSystemColour::wxSYS_COLOUR_BTNFACE));
+
+      boxSizer->Add(buttonNormal, 1, wxGROW|wxUP|wxRIGHT|wxLEFT, 5);
+      boxSizer->Add(buttonModeless, 1, wxGROW|wxUP|wxRIGHT|wxLEFT, 5);
+      boxSizer->Add(buttonStayOnTop, 1, wxGROW|wxUP|wxRIGHT|wxLEFT, 5);
+      boxSizer->Add(buttonModal, 1, wxGROW|wxUP|wxDOWN|wxRIGHT|wxLEFT, 5);
       
       buttonNormal->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "dialog show normal", wxDefaultPosition, {250, 100});
-        frame->Show();
+        auto dialog = new wxFrame(nullptr, wxID_ANY, "dialog show normal", wxDefaultPosition, {250, 100});
+        dialog->Show();
       });
       
       buttonModeless->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxFrame* frame = new wxFrame(this, wxID_ANY, "dialog show modeless", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE|wxFRAME_FLOAT_ON_PARENT);
-        frame->Show();
+        auto dialog = new wxFrame(this, wxID_ANY, "dialog show modeless", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE|wxFRAME_FLOAT_ON_PARENT);
+        dialog->Show();
       });
       
       buttonStayOnTop->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxFrame* frame = new wxFrame(nullptr, wxID_ANY, "dialog stay on top", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE|wxSTAY_ON_TOP);
-        frame->Show();
+        auto dialog = new wxFrame(nullptr, wxID_ANY, "dialog stay on top", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE|wxSTAY_ON_TOP);
+        dialog->Show();
       });
 
       buttonModal->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxDialog* dialog = new wxDialog(this, wxID_ANY, "dialog show modal", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE);
+        auto dialog = new wxDialog(this, wxID_ANY, "dialog show modal", wxDefaultPosition, {250, 100}, wxDEFAULT_FRAME_STYLE);
         dialog->ShowModal();
       });
     }
