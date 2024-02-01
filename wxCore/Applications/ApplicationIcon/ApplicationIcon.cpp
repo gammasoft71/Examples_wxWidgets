9799d877927d93b0e8c7e98ab884b385ccd09369
Gammasoft <gammasoft71@gmail.com>
2024-02-01 22:01:57 +0100

    Review code

--- wxCore/Applications/ApplicationIcon/ApplicationIcon.cpp
+++ wxCore/Applications/ApplicationIcon/ApplicationIcon.cpp
@@ -1,12 +1,14 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
 #include <wx/filename.h>
+#include <wx/image.h>
 #include <wx/stdpaths.h>
 
 namespace ApplicationIconExample {
   class Application : public wxApp {
     bool OnInit() override {
-      wxFrame* frame = new wxFrame(nullptr, wxID_ANY, wxEmptyString);
-      wxImage::AddHandler(new wxPNGHandler());
+      auto frame = new wxFrame;
+      wxImage::AddHandler(new wxPNGHandler);
       wxFileName imagePath(wxStandardPaths::Get().GetExecutablePath());
       if (wxPlatformInfo::Get().GetOperatingSystemFamilyName() == "Macintosh") imagePath.AppendDir("..");
       imagePath.AppendDir("Resources");
