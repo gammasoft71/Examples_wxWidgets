3e41364c0e4a4b67dedfebb2faed73c313269684
Gammasoft <gammasoft71@gmail.com>
2024-02-02 12:17:53 +0100

    Review code

--- wxCore/Dialogs/MultiChoiceDialog/MultiChoiceDialog.cpp
+++ wxCore/Dialogs/MultiChoiceDialog/MultiChoiceDialog.cpp
@@ -1,6 +1,11 @@
-#include <vector>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/choicdlg.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 #include <wx/textdlg.h>
+#include <vector>
 
 namespace MultiChoiceDialogExample {
   class Frame : public wxFrame {
@@ -8,8 +13,8 @@ namespace MultiChoiceDialogExample {
     Frame() : wxFrame(nullptr, wxID_ANY, "MultiChoiceDialog example") {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         label->SetLabel("");
-        std::vector<wxString> choices = {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"};
-        wxMultiChoiceDialog dialog(this, "Message text", "Caption text", static_cast<int>(choices.size()), choices.data());
+        auto choices = std::vector<wxString> {"item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8", "item 9", "item 10"};
+        auto dialog = wxMultiChoiceDialog {this, "Message text", "Caption text", static_cast<int>(choices.size()), choices.data()};
         if (dialog.ShowModal() == wxID_OK) {
           for (auto selection : dialog.GetSelections()) {
             if (!label->GetLabel().IsEmpty()) label->SetLabel(wxString::Format("%s\n", label->GetLabel()));
@@ -22,8 +27,8 @@ namespace MultiChoiceDialogExample {
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Choices...", {10, 10});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "(none)", {10, 50});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Choices...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "(none)", {10, 50}};
   };
 
   class Application : public wxApp {
