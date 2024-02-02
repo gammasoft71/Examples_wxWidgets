11381d228f030faa6f0ae792660eda0e006ea338
Gammasoft <gammasoft71@gmail.com>
2024-02-02 20:12:38 +0100

    Review code

--- wxCore/MenusAndToolbars/StatusBarWithoutPanels/StatusBarWithoutPanels.cpp
+++ wxCore/MenusAndToolbars/StatusBarWithoutPanels/StatusBarWithoutPanels.cpp
@@ -4,17 +4,14 @@ namespace StatusBarWithoutPanelsExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "StatusBar without panels example") {
-      SetBackgroundColour({0xFF, 0xFF, 0xFF, 0x10});
       SetStatusBar(statusBar);
-      Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent& e) {
-        statusBar->SetStatusText(wxString::Format("Mouse click at position : {x=%d, y=%d}", e.GetX(), e.GetY()));
-      });
+      Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent& e) {statusBar->SetStatusText(wxString::Format("Mouse click at position : {x=%d, y=%d}", e.GetX(), e.GetY()));});
       
       statusBar->SetStatusText("Click anywhere on the client frame...");
     }
 
   private:
-    wxStatusBar* statusBar = new wxStatusBar(this);
+    wxStatusBar* statusBar = new wxStatusBar {this};
   };
 
   class Application : public wxApp {
