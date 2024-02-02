a3ddf75db77a77f8651f7045f4d4b75949ac1c9a
Gammasoft <gammasoft71@gmail.com>
2024-02-02 11:38:09 +0100

    Review code

--- wxCore/Dialogs/GenericAboutBox/GenericAboutBox.cpp
+++ wxCore/Dialogs/GenericAboutBox/GenericAboutBox.cpp
@@ -1,14 +1,17 @@
-#include <wx/wx.h>
-#include <wx/aboutdlg.h>
-#include <wx/generic/aboutdlgg.h>
 #include "gammasoft_64x64.xpm"
+#include <wx/generic/aboutdlgg.h>
+#include <wx/app.h>
+#include <wx/aboutdlg.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 
 namespace GenericAboutBoxExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "GenericAboutBox example") {
       button->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
-        wxAboutDialogInfo aboutInfo;
+        auto aboutInfo = wxAboutDialogInfo {};
         aboutInfo.SetName(wxTheApp->GetAppName());
         aboutInfo.SetVersion("1.0", "1.0.0");
         aboutInfo.SetDescription("About dialog description.");
@@ -52,7 +55,7 @@ namespace GenericAboutBoxExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ABOUT, wxEmptyString, {10, 10});
+    wxButton* button = new wxButton {panel, wxID_ABOUT, wxEmptyString, {10, 10}};
   };
 
   class Application : public wxApp {
