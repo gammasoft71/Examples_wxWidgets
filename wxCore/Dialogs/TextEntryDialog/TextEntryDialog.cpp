27f174adb99afc41b130a3a21b3b7a96b36b6c0f
Gammasoft <gammasoft71@gmail.com>
2024-02-02 19:32:55 +0100

    Review code

--- wxCore/Dialogs/TextEntryDialog/TextEntryDialog.cpp
+++ wxCore/Dialogs/TextEntryDialog/TextEntryDialog.cpp
@@ -1,4 +1,8 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 #include <wx/textdlg.h>
 
 namespace TextEntryDialogExample {
@@ -6,7 +10,7 @@ namespace TextEntryDialogExample {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "TextEntryDialog example") {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxTextEntryDialog textEntryDialog(this, "Message text", "Caption text", label->GetLabel(), wxTextEntryDialogStyle);
+        auto textEntryDialog = wxTextEntryDialog {this, "Message text", "Caption text", label->GetLabel(), wxTextEntryDialogStyle};
         if (textEntryDialog.ShowModal() == wxID_OK)
           label->SetLabel(textEntryDialog.GetValue());
       });
@@ -14,8 +18,8 @@ namespace TextEntryDialogExample {
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Text...", {10, 10});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Default value", {10, 50});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Text...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "Default value", {10, 50}};
   };
 
   class Application : public wxApp {
