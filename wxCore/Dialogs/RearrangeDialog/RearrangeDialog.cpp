eea4e425ab9ab9072df93b7a696eedd608279e76
Gammasoft <gammasoft71@gmail.com>
2024-02-02 17:57:29 +0100

    Review code

--- wxCore/Dialogs/RearrangeDialog/RearrangeDialog.cpp
+++ wxCore/Dialogs/RearrangeDialog/RearrangeDialog.cpp
@@ -1,19 +1,18 @@
-#include <vector>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/rearrangectrl.h>
+#include <wx/stattext.h>
+#include <vector>
 
 namespace RearrangeDialogExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "RearangeDialog example") {
-      for (auto item : {"Red", "Green", "Blue", "Yellow"})
-        colors.push_back(item);
-      for (auto item : {3, 0, 1, 2})
-        order.push_back(item);
-
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         label->SetLabel("");
-        wxRearrangeDialog dialog(nullptr, "You can also uncheck the colors you don't like at all.", "Sort the colors in order of preference", order, colors);
+        auto dialog = wxRearrangeDialog {nullptr, "You can also uncheck the colors you don't like at all.", "Sort the colors in order of preference", order, colors};
         if (dialog.ShowModal() == wxID_OK) {
           order = dialog.GetOrder();
           for (size_t n = 0; n < order.size(); n++) {
@@ -28,11 +27,11 @@ namespace RearrangeDialogExample {
     }
     
   private:
-    wxArrayString colors;
-    wxArrayInt order;
+    wxArrayString colors = {"Red", "Green", "Blue", "Yellow"};
+    wxArrayInt order = {3, 0, 1, 2};
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Rearange...", {10, 10});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "(none)", {10, 50});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Rearange...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "(none)", {10, 50}};
   };
 
   class Application : public wxApp {
