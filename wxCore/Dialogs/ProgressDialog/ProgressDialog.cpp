568fcef237a1892ffd601acbe0d5d683418e6b45
Gammasoft <gammasoft71@gmail.com>
2024-02-02 17:22:48 +0100

    Review code

--- wxCore/Dialogs/ProgressDialog/ProgressDialog.cpp
+++ wxCore/Dialogs/ProgressDialog/ProgressDialog.cpp
@@ -1,6 +1,9 @@
-#include <thread>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/progdlg.h>
+#include <thread>
 
 using namespace std;
 using namespace std::literals;
@@ -10,7 +13,7 @@ namespace ProgressDialogExample {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "ProgressDialog example") {
       buttonProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxProgressDialog progressDialog("Process running", "Please wait...", 100, this, wxPD_APP_MODAL|wxPD_AUTO_HIDE|wxPD_CAN_SKIP|wxPD_REMAINING_TIME);
+        auto progressDialog = wxProgressDialog {"Process running", "Please wait...", 100, this, wxPD_APP_MODAL|wxPD_AUTO_HIDE|wxPD_CAN_SKIP|wxPD_REMAINING_TIME};
         for (auto step = 1; step <= progressDialog.GetRange(); ++step) {
           progressDialog.Update(step, wxString::Format("Step %d/%d ==> %s", step, progressDialog.GetRange(), progressDialog.WasSkipped() ? "skipped" : "done"));
           this_thread::sleep_for(100ms);
@@ -18,7 +21,7 @@ namespace ProgressDialogExample {
       });
 
       buttonIndeterminateProcess->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxProgressDialog progressDialog("Indeterminate process running", "Click \"Cancel\" to abort", 10, this, wxPD_APP_MODAL|wxPD_CAN_ABORT|wxPD_ELAPSED_TIME);
+        auto progressDialog = wxProgressDialog {"Indeterminate process running", "Click \"Cancel\" to abort", 10, this, wxPD_APP_MODAL|wxPD_CAN_ABORT|wxPD_ELAPSED_TIME};
         while(!progressDialog.WasCancelled()) {
           progressDialog.Pulse();
           this_thread::sleep_for(100ms);
@@ -28,8 +31,8 @@ namespace ProgressDialogExample {
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* buttonProcess = new wxButton(panel, wxID_ANY, "Process", {10, 10});
-    wxButton* buttonIndeterminateProcess = new wxButton(panel, wxID_ANY, "Indeterminate Process", {100, 10});
+    wxButton* buttonProcess = new wxButton {panel, wxID_ANY, "Process", {10, 10}};
+    wxButton* buttonIndeterminateProcess = new wxButton {panel, wxID_ANY, "Indeterminate Process", {100, 10}};
   };
 
   class Application : public wxApp {
