41540e5bfd1812a4e0d105e67912e6e9641f8ab1
Gammasoft <gammasoft71@gmail.com>
2024-02-02 17:59:16 +0100

    Review code

--- wxCore/Dialogs/SaveFileDialog/SaveFileDialog.cpp
+++ wxCore/Dialogs/SaveFileDialog/SaveFileDialog.cpp
@@ -1,22 +1,26 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
 #include <wx/filedlg.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace SaveFileDialogExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "SaveFileDialog example") {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxFileDialog saveFileDialog(this, wxEmptyString, wxEmptyString, "MyFile.txt", "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFD_SAVE);
+        auto saveFileDialog = wxFileDialog {this, wxEmptyString, wxEmptyString, "MyFile.txt", "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFD_SAVE};
         if (saveFileDialog.ShowModal() == wxID_OK) {
-          label->SetLabelText(wxString::Format("File = %s",  saveFileDialog.GetPath()));
+          label->SetLabelText(wxString::Format("File = %s", saveFileDialog.GetPath()));
         }
       });
     }
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Save...", {10, 10});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "", {10, 40});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Save...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "", {10, 40}};
   };
 
   class Application : public wxApp {
