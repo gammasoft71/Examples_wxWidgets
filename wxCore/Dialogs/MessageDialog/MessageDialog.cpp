53997d46ef63f9f67bcd32ac621841062414a338
Gammasoft <gammasoft71@gmail.com>
2024-02-02 12:14:18 +0100

    Review code

--- wxCore/Dialogs/MessageDialog/MessageDialog.cpp
+++ wxCore/Dialogs/MessageDialog/MessageDialog.cpp
@@ -1,4 +1,9 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/msgdlg.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace MessageDialogExample {
   class Frame : public wxFrame {
@@ -6,15 +11,15 @@ namespace MessageDialogExample {
     Frame() : wxFrame(nullptr, wxID_ANY, "MessageDialog example") {
       buttonShowMessage->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         labelDialogResult->SetLabelText("Result = ");
-        wxMessageDialog messageDialog(this, "Hello, World!", "Message", wxOK|wxCANCEL);
+        auto messageDialog = wxMessageDialog {this, "Hello, World!", "Message", wxOK|wxCANCEL};
         labelDialogResult->SetLabelText(wxString::Format("Result = %s", messageDialog.ShowModal() == wxID_OK ? "wxOK" : "wxCANCEL"));
       });
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* buttonShowMessage = new wxButton(panel, wxID_ANY, "Message...", {10, 10});
-    wxStaticText* labelDialogResult = new wxStaticText(panel, wxID_ANY, "Result = ", {10, 45});
+    wxButton* buttonShowMessage = new wxButton {panel, wxID_ANY, "Message...", {10, 10}};
+    wxStaticText* labelDialogResult = new wxStaticText {panel, wxID_ANY, "Result = ", {10, 45}};
   };
 
   class Application : public wxApp {
