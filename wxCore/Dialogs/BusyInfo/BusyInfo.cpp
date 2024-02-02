896ff39c3f421028dada5d2a39f2b6d6df7e16a8
Gammasoft <gammasoft71@gmail.com>
2024-02-02 10:00:03 +0100

    Review code

--- wxCore/Dialogs/BusyInfo/BusyInfo.cpp
+++ wxCore/Dialogs/BusyInfo/BusyInfo.cpp
@@ -1,24 +1,27 @@
 #include <thread>
-#include <wx/wx.h>
+#include <wx/app.h>
 #include <wx/artprov.h>
 #include <wx/busyinfo.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 
 namespace BusyInfoExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "BusyInfo example") {
       button->Bind(wxEVT_BUTTON, [this](wxCommandEvent& event) {
-        wxBusyInfo busiInfo(wxBusyInfoFlags().Title("Application busy").Label("Please wait while do something...").Icon(wxArtProvider::GetIcon(wxART_WARNING)).Background({255, 0, 0}).Foreground({255, 255, 255}).Transparency(192));
+        auto busiInfo = wxBusyInfo {wxBusyInfoFlags {}.Title("Application busy").Label("Please wait while do something...").Icon(wxArtProvider::GetIcon(wxART_WARNING)).Background({255, 0, 0}).Foreground({255, 255, 255}).Transparency(192)};
         for (auto count = 0; count < 500; ++count) {
-          std::this_thread::sleep_for(std::chrono::milliseconds(10));
+          std::this_thread::sleep_for(std::chrono::milliseconds {10});
           wxYield();
         }
       });
     }
     
   private:
-    wxPanel* panel = new wxPanel(this);
-    wxButton* button = new wxButton(panel, wxID_ANY, "Do something...", {10, 10});
+    wxPanel* panel = new wxPanel {this};
+    wxButton* button = new wxButton {panel, wxID_ANY, "Do something...", {10, 10}};
   };
 
   class Application : public wxApp {
