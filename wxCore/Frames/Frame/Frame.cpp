7d7c6032a5f378a67b4484bbdf527512c5f3075c
Gammasoft <gammasoft71@gmail.com>
2024-02-02 19:40:47 +0100

    Review code

--- wxCore/Frames/Frame/Frame.cpp
+++ wxCore/Frames/Frame/Frame.cpp
@@ -1,20 +1,16 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/msgdlg.h>
+#include <wx/panel.h>
 
 namespace FrameExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "Window example", wxDefaultPosition, {640, 480}) {
-      buttonClose->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        Close();
-      });
-
-      buttonExit->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxTheApp->Exit();
-      });
-      
-      buttonExitMainLoop->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxTheApp->ExitMainLoop();
-      });
+      buttonClose->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {Close();});
+      buttonExit->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {wxTheApp->Exit();});
+      buttonExitMainLoop->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {wxTheApp->ExitMainLoop();});
 
       Bind(wxEVT_CLOSE_WINDOW, [&](wxCloseEvent& event) {
         auto closed = wxMessageBox("Are you sure you want exit?", "Close Window", wxICON_QUESTION|wxYES_NO) == wxYES;
@@ -25,9 +21,9 @@ namespace FrameExample {
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* buttonClose = new wxButton(panel, wxID_ANY, "Close", {10, 10});
-    wxButton* buttonExit = new wxButton(panel, wxID_ANY, "Exit", {100, 10});
-    wxButton* buttonExitMainLoop = new wxButton(panel, wxID_ANY, "ExitMainLoop", {190, 10});
+    wxButton* buttonClose = new wxButton {panel, wxID_ANY, "Close", {10, 10}};
+    wxButton* buttonExit = new wxButton {panel, wxID_ANY, "Exit", {100, 10}};
+    wxButton* buttonExitMainLoop = new wxButton {panel, wxID_ANY, "ExitMainLoop", {190, 10}};
   };
 
   class Application : public wxApp {
