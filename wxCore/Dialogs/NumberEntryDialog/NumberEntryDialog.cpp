1817956cf7d77f7e08fc4f870bbbd26d09ef475b
Gammasoft <gammasoft71@gmail.com>
2024-02-02 12:29:06 +0100

    Review code

--- wxCore/Dialogs/NumberEntryDialog/NumberEntryDialog.cpp
+++ wxCore/Dialogs/NumberEntryDialog/NumberEntryDialog.cpp
@@ -1,6 +1,9 @@
-#include <vector>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
 #include <wx/numdlg.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace NumberEntryDialogExample {
 class Frame : public wxFrame {
@@ -8,7 +11,7 @@ public:
   Frame() : wxFrame(nullptr, wxID_ANY, "NumberEntryDialog example") {
     button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
       label->SetLabel("Number = ");
-      wxNumberEntryDialog dialog(this, "Message text", "Prompt text", "Caption text", 50, 10, 100);
+      auto dialog = wxNumberEntryDialog {this, "Message text", "Prompt text", "Caption text", 50, 10, 100};
       if (dialog.ShowModal() == wxID_OK)
         label->SetLabel(wxString::Format("Number = %ld", dialog.GetValue()));
     });
@@ -16,8 +19,8 @@ public:
   
 private:
   wxPanel* panel = new wxPanel {this};
-  wxButton* button = new wxButton(panel, wxID_ANY, "Number...", {10, 10});
-  wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Number = ", {10, 50});
+  wxButton* button = new wxButton {panel, wxID_ANY, "Number...", {10, 10}};
+  wxStaticText* label = new wxStaticText {panel, wxID_ANY, "Number = ", {10, 50}};
 };
 
 class Application : public wxApp {
