37f7a69d6e04f688a9a164047aaad39b1ee10f74
Gammasoft <gammasoft71@gmail.com>
2024-02-02 11:37:31 +0100

    Review code

--- wxCore/Dialogs/FontDialog/FontDialog.cpp
+++ wxCore/Dialogs/FontDialog/FontDialog.cpp
@@ -1,5 +1,9 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
 #include <wx/fontdlg.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace FontDialogExample {
   class Frame : public wxFrame {
@@ -7,10 +11,10 @@ namespace FontDialogExample {
     Frame() : wxFrame(nullptr, wxID_ANY, "Font example") {
       SetClientSize(400, 400);
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxFontData fontData;
+        auto fontData = wxFontData {};
         fontData.SetInitialFont(label->GetFont());
         fontData.SetColour(label->GetForegroundColour());
-        wxFontDialog fontDialog(this, fontData);
+        auto fontDialog = wxFontDialog {this, fontData};
         if (fontDialog.ShowModal() == wxID_OK) {
           label->SetFont(fontDialog.GetFontData().GetChosenFont());
           label->SetForegroundColour(fontDialog.GetFontData().GetColour());
@@ -20,8 +24,8 @@ namespace FontDialogExample {
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Font...", {10, 10});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY,
+    wxButton* button = new wxButton {panel, wxID_ANY, "Font...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY,
       "The quick brown fox jumps over the lazy dog.\n"
       "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.\n"
       "0123456789+-*/%~^&|=<>≤≥±÷≠{{[()]}},;:.?¿!¡\n"
@@ -30,7 +34,7 @@ namespace FontDialogExample {
       "\u0410\u0411\u0412\u0413\u0414\u0415\u0416\u0417\u0418\u0419\u041a\u041b\u041c\u041d\u041e\u041f\n"
       "\u4ea0\u4ea1\u4ea2\u4ea3\u4ea4\u4ea5\u4ea6\u4ea7\u4ea8\u4ea9\u4eaa\u4eab\u4eac\u4ead\u4eae\u4eaf\n"
       "\u4eb0\u4eb1\u4eb2\u4eb3\u4eb4\u4eb5\u4eb6\u4eb7\u4eb8\u4eb9\u4eba\u4ebb\u4ebc\u4ebd\u4ebe\u4ebf\n"
-      "\U0001F428", {10, 50});
+      "\U0001F428", {10, 50}};
   };
 
   class Application : public wxApp {
