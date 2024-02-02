ec39c0f66c440e9951f31433945075c7a095a559
Gammasoft <gammasoft71@gmail.com>
2024-02-02 10:05:52 +0100

    Review code

--- wxCore/Dialogs/ColourDialog/ColourDialog.cpp
+++ wxCore/Dialogs/ColourDialog/ColourDialog.cpp
@@ -9,24 +9,24 @@ namespace ColourDialogExample {
   public:
     Frame() : wxFrame(nullptr, wxID_ANY, "ColourDialog example") {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-        wxColourData colourData;
+        auto colourData = wxColourData {};
         colourData.SetColour(GetBackgroundColour());
-        for (int index = 0; index < static_cast<int>(customColours.size()); index++)
+        for (auto index = 0; index < static_cast<int>(customColours.size()); index++)
           colourData.SetCustomColour(index, customColours[index]);
-        wxColourDialog colourDialog(this, &colourData);
+        auto colourDialog = wxColourDialog {this, &colourData};
         if (colourDialog.ShowModal() == wxID_OK) {
           SetBackgroundColour(colourDialog.GetColourData().GetColour());
           Refresh();
         }
-        for (int index = 0; index < static_cast<int>(customColours.size()); index++)
+        for (auto index = 0; index < static_cast<int>(customColours.size()); index++)
           customColours[index], colourData.GetCustomColour(index);
       });
     }
 
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Color...", {10, 10});
-    vector<wxColour> customColours {wxColour(0xFF, 0x00, 0x00), wxColour(0x00, 0xFF, 0x00), wxColour(0x00, 0x00, 0xFF), wxColour(0xFF, 0xFF, 0x00), wxColour(0xF0, 0xF0, 0xF0), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF), wxColour(0xFF, 0xFF, 0xFF)};
+    wxButton* button = new wxButton {panel, wxID_ANY, "Color...", {10, 10}};
+    vector<wxColour> customColours {{0xFF, 0x00, 0x00}, {0x00, 0xFF, 0x00}, {0x00, 0x00, 0xFF}, {0xFF, 0xFF, 0x00}, {0xF0, 0xF0, 0xF0}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}};
   };
 
   class Application : public wxApp {
