9c51aeb2ccd4254cddc1c0243ab1325ef207baa0
Gammasoft <gammasoft71@gmail.com>
2024-02-03 20:47:33 +0100

    Review code

--- wxCore/Controls/FileCtrl/FileCtrl.cpp
+++ wxCore/Controls/FileCtrl/FileCtrl.cpp
@@ -9,7 +9,7 @@
 #include <wx/filectrl.h>
 class FileCtrl : public wxFileCtrl {
 public:
-  FileCtrl(wxWindow*parent, wxWindowID id, const wxString& defaultDirectory = wxEmptyString, const wxString& defaultFilename = wxEmptyString, const wxString& wildCard = wxFileSelectorDefaultWildcardStr, long style = wxFC_DEFAULT_STYLE, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, const wxString& name = wxFileCtrlNameStr) : wxFileCtrl(parent, id, defaultDirectory, defaultFilename, wildCard, style, pos, size, name) {
+  FileCtrl(wxWindow*parent, wxWindowID id, const wxString& defaultDirectory = wxEmptyString, const wxString& defaultFilename = wxEmptyString, const wxString& wildCard = wxFileSelectorDefaultWildcardStr, long style = wxFC_DEFAULT_STYLE, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, const wxString& name = wxFileCtrlNameStr) : wxFileCtrl {parent, id, defaultDirectory, defaultFilename, wildCard, style, pos, size, name} {
     // Workaround : with wxWidgets version <= 3.2.0 show hidden is true by default on Gtk
     ShowHidden(false);
   }
@@ -21,10 +21,10 @@ public:
 #include <wx/checkbox.h>
 class FileCtrl : public wxFileCtrl {
 public:
-  FileCtrl(wxWindow*parent, wxWindowID id, const wxString& defaultDirectory = wxEmptyString, const wxString& defaultFilename = wxEmptyString, const wxString& wildCard = wxFileSelectorDefaultWildcardStr, long style = wxFC_DEFAULT_STYLE, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, const wxString& name = wxFileCtrlNameStr) : wxFileCtrl(parent, id, defaultDirectory, defaultFilename, wildCard, style, pos, size, name) {
+  FileCtrl(wxWindow*parent, wxWindowID id, const wxString& defaultDirectory = wxEmptyString, const wxString& defaultFilename = wxEmptyString, const wxString& wildCard = wxFileSelectorDefaultWildcardStr, long style = wxFC_DEFAULT_STYLE, const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, const wxString& name = wxFileCtrlNameStr) : wxFileCtrl {parent, id, defaultDirectory, defaultFilename, wildCard, style, pos, size, name} {
     // Workaround : with wxWidgets version <= 3.2.0 folder, executable and link are set with specific color (respectively Blue, red and middle gray) on macOS (not standard)
 #if __APPLE__
-    for (int i = 0; i < GetFileList()->GetItemCount(); i++)
+    for (auto i = 0; i < GetFileList()->GetItemCount(); i++)
       GetFileList()->SetItemTextColour(i, GetForegroundColour());
     GetFileList()->Bind(wxEVT_LIST_INSERT_ITEM, [&](wxListEvent& event) {
       GetFileList()->SetItemTextColour(event.GetIndex(), GetForegroundColour());
@@ -42,7 +42,7 @@ public:
 namespace FileCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "FileCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "FileCtrl example"} {
       SetClientSize({800, 450});
       fileCtrl1->SetPosition({10, 10});
       fileCtrl1->SetSize({780, 390});
@@ -53,8 +53,8 @@ namespace FileCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    FileCtrl* fileCtrl1 = new FileCtrl(panel, wxID_ANY, wxStandardPaths::Get().GetDocumentsDir(), wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFC_DEFAULT_STYLE);
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "", {10, 410});
+    FileCtrl* fileCtrl1 = new FileCtrl {panel, wxID_ANY, wxStandardPaths::Get().GetDocumentsDir(), wxEmptyString, "Text Files (*.txt)|*.txt|All Files (*.*)|*.*", wxFC_DEFAULT_STYLE};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, "", {10, 410}};
   };
 
   class Application : public wxApp {
