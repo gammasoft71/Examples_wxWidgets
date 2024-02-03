113cfca436528a2259e577548185fe2a9dc59f50
Gammasoft <gammasoft71@gmail.com>
2024-02-03 21:09:55 +0100

    Review code

--- wxCore/Controls/HyperlinkCtrl/HyperlinkCtrl.cpp
+++ wxCore/Controls/HyperlinkCtrl/HyperlinkCtrl.cpp
@@ -1,5 +1,5 @@
-#include <wx/debug.h>
 #include <wx/app.h>
+#include <wx/debug.h>
 #include <wx/frame.h>
 #include <wx/hyperlink.h>
 #include <wx/panel.h>
@@ -9,7 +9,7 @@
 namespace HyperlinkCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "HyperlinkCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "HyperlinkCtrl example"} {
       panel->SetSizer(boxSizerLinks);
       boxSizerLinks->Add(hyperlinkCtrl1, 0, wxSTRETCH_NOT, 0);
       boxSizerLinks->Add(staticText1, 0, wxSTRETCH_NOT, 0);
@@ -19,11 +19,11 @@ namespace HyperlinkCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxBoxSizer* boxSizerLinks = new wxBoxSizer(wxHORIZONTAL);
-    wxHyperlinkCtrl* hyperlinkCtrl1 = new wxHyperlinkCtrl(panel, wxID_ANY, "Gammasoft", "https://gammasoft71.wixsite.com/gammasoft");
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, " presents ");
-    wxHyperlinkCtrl* hyperlinkCtrl2 = new wxHyperlinkCtrl(panel, wxID_ANY, "wxWidgets", "https://gammasoft71.wixsite.com/gammasoft/wxwidgets");
-    wxStaticText* staticText2 = new wxStaticText(panel, wxID_ANY, " examples");
+    wxBoxSizer* boxSizerLinks = new wxBoxSizer {wxHORIZONTAL};
+    wxHyperlinkCtrl* hyperlinkCtrl1 = new wxHyperlinkCtrl {panel, wxID_ANY, "Gammasoft", "https://gammasoft71.wixsite.com/gammasoft"};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, " presents "};
+    wxHyperlinkCtrl* hyperlinkCtrl2 = new wxHyperlinkCtrl {panel, wxID_ANY, "wxWidgets", "https://gammasoft71.wixsite.com/gammasoft/wxwidgets"};
+    wxStaticText* staticText2 = new wxStaticText {panel, wxID_ANY, " examples"};
   };
 
   class Application : public wxApp {
