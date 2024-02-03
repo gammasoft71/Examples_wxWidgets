3e9e12bc5686f6857dd55ef4ecba1a1f42c2a5ae
Gammasoft <gammasoft71@gmail.com>
2024-02-03 10:29:16 +0100

    Review code

--- wxCore/Containers/Panel/Panel.cpp
+++ wxCore/Containers/Panel/Panel.cpp
@@ -1,4 +1,6 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 
 namespace PanelExample {
   class Frame : public wxFrame {
@@ -9,8 +11,8 @@ namespace PanelExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxPanel* panel1 = new wxPanel(panel, wxID_ANY, {10, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_SIMPLE);
-    wxPanel* panel2 = new wxPanel(panel, wxID_ANY, {325, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_THEME);
+    wxPanel* panel1 = new wxPanel {panel, wxID_ANY, {10, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_SIMPLE};
+    wxPanel* panel2 = new wxPanel {panel, wxID_ANY, {325, 10}, {305, 460}, wxTAB_TRAVERSAL|wxBORDER_THEME};
   };
 
   class Application : public wxApp {
