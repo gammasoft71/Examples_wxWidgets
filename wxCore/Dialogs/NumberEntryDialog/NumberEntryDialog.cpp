32e4b92a8385e5e90f4ea799a8a62ebc367f3699
Gammasoft <gammasoft71@gmail.com>
2024-02-03 20:20:36 +0100

    Review code

--- wxCore/Dialogs/NumberEntryDialog/NumberEntryDialog.cpp
+++ wxCore/Dialogs/NumberEntryDialog/NumberEntryDialog.cpp
@@ -6,26 +6,26 @@
 #include <wx/stattext.h>
 
 namespace NumberEntryDialogExample {
-class Frame : public wxFrame {
-public:
-  Frame() : wxFrame {nullptr, wxID_ANY, "NumberEntryDialog example"} {
-    button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
-      label->SetLabel("Number = ");
-      auto dialog = wxNumberEntryDialog {this, "Message text", "Prompt text", "Caption text", 50, 10, 100};
-      if (dialog.ShowModal() == wxID_OK)
-        label->SetLabel(wxString::Format("Number = %ld", dialog.GetValue()));
-    });
-  }
-  
-private:
-  wxPanel* panel = new wxPanel {this};
-  wxButton* button = new wxButton {panel, wxID_ANY, "Number...", {10, 10}};
-  wxStaticText* label = new wxStaticText {panel, wxID_ANY, "Number = ", {10, 50}};
-};
+  class Frame : public wxFrame {
+  public:
+    Frame() : wxFrame {nullptr, wxID_ANY, "NumberEntryDialog example"} {
+      button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
+        label->SetLabel("Number = ");
+        auto dialog = wxNumberEntryDialog {this, "Message text", "Prompt text", "Caption text", 50, 10, 100};
+        if (dialog.ShowModal() == wxID_OK)
+          label->SetLabel(wxString::Format("Number = %ld", dialog.GetValue()));
+      });
+    }
+    
+  private:
+    wxPanel* panel = new wxPanel {this};
+    wxButton* button = new wxButton {panel, wxID_ANY, "Number...", {10, 10}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "Number = ", {10, 50}};
+  };
 
-class Application : public wxApp {
-  bool OnInit() override {return (new Frame)->Show();}
-};
+  class Application : public wxApp {
+    bool OnInit() override {return (new Frame)->Show();}
+  };
 }
 
 wxIMPLEMENT_APP(NumberEntryDialogExample::Application);
