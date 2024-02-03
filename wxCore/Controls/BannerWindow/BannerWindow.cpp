d216db7f132127424478496f0f37f05de94faf71
Gammasoft <gammasoft71@gmail.com>
2024-02-03 18:35:53 +0100

    Review code

--- wxCore/Controls/BannerWindow/BannerWindow.cpp
+++ wxCore/Controls/BannerWindow/BannerWindow.cpp
@@ -1,19 +1,19 @@
 #include <wx/app.h>
+#include <wx/bannerwindow.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
-#include <wx/bannerwindow.h>
 
 namespace BannerWindowExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "BannerWindow example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "BannerWindow example"} {
       banner->SetText("Banner caption", "Banner message text");
       banner->SetForegroundColour({0, 0, 0});
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxBannerWindow* banner = new wxBannerWindow(panel, wxID_ANY, wxTOP, wxDefaultPosition, {800, 50}, wxALIGN_CENTRE_HORIZONTAL);
+    wxBannerWindow* banner = new wxBannerWindow {panel, wxID_ANY, wxTOP, wxDefaultPosition, {800, 50}, wxALIGN_CENTRE_HORIZONTAL};
   };
 
   class Application : public wxApp {
