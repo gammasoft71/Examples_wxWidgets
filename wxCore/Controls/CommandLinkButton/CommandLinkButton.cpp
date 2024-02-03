dff366f2e35e3d489496eccd22336679f2e79f3d
Gammasoft <gammasoft71@gmail.com>
2024-02-03 20:33:10 +0100

    Review code

--- wxCore/Controls/CommandLinkButton/CommandLinkButton.cpp
+++ wxCore/Controls/CommandLinkButton/CommandLinkButton.cpp
@@ -7,7 +7,7 @@
 namespace CommandLinkButtonExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "CommandLinkButton example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "CommandLinkButton example"} {
       commandLinkButton1->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         staticText1->SetLabel(wxString::Format("commandLinkButton1 clicked %d times", ++commandLinkButton1Clicked));
       });
@@ -15,8 +15,8 @@ namespace CommandLinkButtonExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxCommandLinkButton* commandLinkButton1 = new wxCommandLinkButton(panel, wxID_ANY, "Command link", "width supplementary text", {30, 30}, {240, 60});
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "commandLinkButton1 clicked 0 times", {30, 100}, {200, 50});
+    wxCommandLinkButton* commandLinkButton1 = new wxCommandLinkButton {panel, wxID_ANY, "Command link", "width supplementary text", {30, 30}, {240, 60}};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, "commandLinkButton1 clicked 0 times", {30, 100}, {200, 50}};
     int commandLinkButton1Clicked = 0;
   };
 
