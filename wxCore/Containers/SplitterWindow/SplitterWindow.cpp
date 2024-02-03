b685a731a43a3d779e9a5a72f506b02e26403c67
Gammasoft <gammasoft71@gmail.com>
2024-02-03 12:55:46 +0100

    Review code

--- wxCore/Containers/SplitterWindow/SplitterWindow.cpp
+++ wxCore/Containers/SplitterWindow/SplitterWindow.cpp
@@ -1,10 +1,12 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/splitter.h>
 
 namespace SplitterWindowExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "SplitterWindow example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "SplitterWindow example"} {
       SetClientSize(300, 300);
       splitterWindowLeft->SetSplitMode(wxSPLIT_VERTICAL);
       splitterWindowLeft->SplitVertically(leftPanel1, leftPanel2, 70);
@@ -22,15 +24,15 @@ namespace SplitterWindowExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxSplitterWindow* splitterWindowLeft = new wxSplitterWindow(panel, wxID_ANY, {0, 0}, {300, 300}, wxSP_NOBORDER);
-    wxPanel* leftPanel1 = new wxPanel(splitterWindowLeft);
-    wxPanel* leftPanel2 = new wxPanel(splitterWindowLeft);
-    wxSplitterWindow* splitterWindowRight = new wxSplitterWindow(leftPanel2, wxID_ANY, {0, 0}, {230, 300}, wxSP_NOBORDER);
-    wxPanel* rightPanel1 = new wxPanel(splitterWindowRight);
-    wxPanel* rightPanel2 = new wxPanel(splitterWindowRight);
-    wxSplitterWindow* splitterWindowCenter = new wxSplitterWindow(rightPanel1, wxID_ANY, {0, 0}, {160, 300}, wxSP_NOBORDER);
-    wxPanel* centerPanel1 = new wxPanel(splitterWindowCenter);
-    wxPanel* centerPanel2 = new wxPanel(splitterWindowCenter);
+    wxSplitterWindow* splitterWindowLeft = new wxSplitterWindow {panel, wxID_ANY, {0, 0}, {300, 300}, wxSP_NOBORDER};
+    wxPanel* leftPanel1 = new wxPanel {splitterWindowLeft};
+    wxPanel* leftPanel2 = new wxPanel {splitterWindowLeft};
+    wxSplitterWindow* splitterWindowRight = new wxSplitterWindow {leftPanel2, wxID_ANY, {0, 0}, {230, 300}, wxSP_NOBORDER};
+    wxPanel* rightPanel1 = new wxPanel {splitterWindowRight};
+    wxPanel* rightPanel2 = new wxPanel {splitterWindowRight};
+    wxSplitterWindow* splitterWindowCenter = new wxSplitterWindow {rightPanel1, wxID_ANY, {0, 0}, {160, 300}, wxSP_NOBORDER};
+    wxPanel* centerPanel1 = new wxPanel {splitterWindowCenter};
+    wxPanel* centerPanel2 = new wxPanel {splitterWindowCenter};
   };
 
   class Application : public wxApp {
