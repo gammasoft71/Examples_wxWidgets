f8f82df03d43daf67e973a45ad6f11c76063ff19
Gammasoft <gammasoft71@gmail.com>
2024-02-03 12:57:24 +0100

    Review code

--- wxCore/Containers/StaticBox/StaticBox.cpp
+++ wxCore/Containers/StaticBox/StaticBox.cpp
@@ -1,16 +1,19 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/statbox.h>
 
 namespace StaticBoxExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "StaticBox example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "StaticBox example"} {
       SetClientSize(640, 480);
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxStaticBox* staticBox1 = new wxStaticBox(panel, wxID_ANY, "GroupBox 1", {10, 10}, {305, 460});
-    wxStaticBox* staticBox2 = new wxStaticBox(panel, wxID_ANY, wxEmptyString, {325, 10}, {305, 460});
+    wxStaticBox* staticBox1 = new wxStaticBox {panel, wxID_ANY, "GroupBox 1", {10, 10}, {305, 460}};
+    wxStaticBox* staticBox2 = new wxStaticBox {panel, wxID_ANY, wxEmptyString, {325, 10}, {305, 460}};
   };
 
   class Application : public wxApp {
