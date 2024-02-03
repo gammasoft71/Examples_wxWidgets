5f779117caae58d1f40bdad3c7998ac2d7381542
Gammasoft <gammasoft71@gmail.com>
2024-02-03 18:31:58 +0100

    Review code

--- wxCore/Controls/ActivityIndicator/ActivityIndicator.cpp
+++ wxCore/Controls/ActivityIndicator/ActivityIndicator.cpp
@@ -1,18 +1,22 @@
+#include <wx/activityindicator.h>
 #include <wx/app.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
-#include <wx/activityindicator.h>
 
 namespace ActivityIndicatorExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "ActivityIndicator example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "ActivityIndicator example"} {
       activityIndicator->Start();
+      panel->Bind(wxEVT_LEFT_DOWN, [&](wxMouseEvent& event) {
+        if (activityIndicator->IsRunning()) activityIndicator->Stop();
+        else activityIndicator->Start();
+      });
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxActivityIndicator* activityIndicator = new wxActivityIndicator(panel, wxID_ANY, {10, 10});
+    wxActivityIndicator* activityIndicator = new wxActivityIndicator {panel, wxID_ANY, {10, 10}};
   };
 
   class Application : public wxApp {
