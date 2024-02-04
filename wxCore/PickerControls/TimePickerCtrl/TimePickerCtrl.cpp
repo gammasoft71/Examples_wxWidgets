320cf46eea472fd2e4477bdc08c3d5dd7f54d09c
Gammasoft <gammasoft71@gmail.com>
2024-02-04 20:08:35 +0100

    Review code

--- wxCore/PickerControls/TimePickerCtrl/TimePickerCtrl.cpp
+++ wxCore/PickerControls/TimePickerCtrl/TimePickerCtrl.cpp
@@ -1,11 +1,14 @@
-#include <wx/wx.h>
-#include <wx/timectrl.h>
+#include <wx/app.h>
 #include <wx/dateevt.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
+#include <wx/timectrl.h>
 
 namespace TimePickerCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "TimePickerCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "TimePickerCtrl example"} {
       timePicker1->SetTime(12, 34, 56);
       timePicker1->Bind(wxEVT_TIME_CHANGED, [&](wxDateEvent& event) {
         staticText1->SetLabelText(timePicker1->GetValue().FormatTime());
@@ -15,8 +18,8 @@ namespace TimePickerCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxTimePickerCtrl* timePicker1 = new wxTimePickerCtrl(panel, wxID_ANY, wxDefaultDateTime, {30, 30});
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {30, 70});
+    wxTimePickerCtrl* timePicker1 = new wxTimePickerCtrl {panel, wxID_ANY, wxDefaultDateTime, {30, 30}};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, wxEmptyString, {30, 70}};
   };
 
   class Application : public wxApp {
