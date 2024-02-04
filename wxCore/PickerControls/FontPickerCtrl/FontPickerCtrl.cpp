cdeff9b561f326f2224d051aa0932630ff556958
Gammasoft <gammasoft71@gmail.com>
2024-02-04 20:06:36 +0100

    Review code

--- wxCore/PickerControls/FontPickerCtrl/FontPickerCtrl.cpp
+++ wxCore/PickerControls/FontPickerCtrl/FontPickerCtrl.cpp
@@ -1,10 +1,13 @@
-#include <wx/wx.h>
+#include <wx/app.h>
 #include <wx/fontpicker.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace FontPickerCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "FontPickerCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "FontPickerCtrl example"} {
       SetClientSize(400, 400);
       picker->SetSelectedFont(label->GetFont());
       picker->SetSelectedColour(label->GetForegroundColour());
@@ -16,7 +19,7 @@ namespace FontPickerCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY,
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY,
       "The quick brown fox jumps over the lazy dog.\n"
       "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.\n"
       "0123456789+-*/%~^&|=<>≤≥±÷≠{{[()]}},;:.?¿!¡\n"
@@ -25,8 +28,8 @@ namespace FontPickerCtrlExample {
       "\u0410\u0411\u0412\u0413\u0414\u0415\u0416\u0417\u0418\u0419\u041a\u041b\u041c\u041d\u041e\u041f\n"
       "\u4ea0\u4ea1\u4ea2\u4ea3\u4ea4\u4ea5\u4ea6\u4ea7\u4ea8\u4ea9\u4eaa\u4eab\u4eac\u4ead\u4eae\u4eaf\n"
       "\u4eb0\u4eb1\u4eb2\u4eb3\u4eb4\u4eb5\u4eb6\u4eb7\u4eb8\u4eb9\u4eba\u4ebb\u4ebc\u4ebd\u4ebe\u4ebf\n"
-      "\U0001F428", wxPoint(10, 50));
-    wxFontPickerCtrl* picker = new wxFontPickerCtrl(panel, wxID_ANY, {}, {10, 10}, wxDefaultSize, wxFNTP_FONTDESC_AS_LABEL);
+      "\U0001F428", {10, 50}};
+    wxFontPickerCtrl* picker = new wxFontPickerCtrl {panel, wxID_ANY, {}, {10, 10}, wxDefaultSize, wxFNTP_FONTDESC_AS_LABEL};
   };
 
   class Application : public wxApp {
