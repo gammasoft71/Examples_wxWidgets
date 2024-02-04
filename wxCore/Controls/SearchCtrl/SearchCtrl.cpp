ff10807351f93b23f4d63ea439f7e28acc84db4a
Gammasoft <gammasoft71@gmail.com>
2024-02-04 11:10:14 +0100

    Review code

--- wxCore/Controls/SearchCtrl/SearchCtrl.cpp
+++ wxCore/Controls/SearchCtrl/SearchCtrl.cpp
@@ -1,5 +1,3 @@
-#include <map>
-#include <string>
 #include <wx/app.h>
 #include <wx/frame.h>
 #include <wx/menu.h>
@@ -7,23 +5,18 @@
 #include <wx/panel.h>
 #include <wx/srchctrl.h>
 #include <wx/stattext.h>
+#include <map>
 
 namespace SearchCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "SearchCtrl example") {
-      
+    Frame() : wxFrame {nullptr, wxID_ANY, "SearchCtrl example"} {
       menuSearch->AppendRadioItem(ID_TEXT, "Text");
       menuSearch->AppendRadioItem(ID_REFERENCES, "References");
       menuSearch->AppendRadioItem(ID_DEFINITIONS, "Definitions");
       menuSearch->AppendRadioItem(ID_REGULAR_EXPRESSION, "Regular expression");
 
-      wxArrayString fruits;
-      fruits.Add("Apple");
-      fruits.Add("Banana");
-      fruits.Add("Pear");
-      fruits.Add("Raspberry");
-      searchCtrl1->AutoComplete(fruits);
+      searchCtrl1->AutoComplete(wxArrayString {"Apple", "Banana", "Pear", "Raspberry"});
       searchCtrl1->ShowCancelButton(true);
       searchCtrl1->SetMenu(menuSearch);
       searchCtrl1->Bind(wxEVT_SEARCH, [&](wxCommandEvent& event) {
@@ -31,16 +24,16 @@ namespace SearchCtrlExample {
       });
       
       searchCtrl1->GetMenu()->Bind(wxEVT_MENU, [&](wxCommandEvent& event) {
-        std::map<int, std::string> modes = {{ID_TEXT, "Text"}, {ID_REFERENCES, "References"}, {ID_DEFINITIONS, "Definitions"}, {ID_REGULAR_EXPRESSION, "Regular expression"}};
+        static auto modes = std::map<int, wxString> {{ID_TEXT, "Text"}, {ID_REFERENCES, "References"}, {ID_DEFINITIONS, "Definitions"}, {ID_REGULAR_EXPRESSION, "Regular expression"}};
         staticText1->SetLabel(wxString::Format("Mode : %s", modes[event.GetId()]));
       });
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxSearchCtrl* searchCtrl1 = new wxSearchCtrl(panel, wxID_ANY, wxEmptyString, {10, 10}, {280, wxDefaultCoord});
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, "Mode : Text", {10, 50}, {280, 20});
-    wxStaticText* staticText2 = new wxStaticText(panel, wxID_ANY, "Search for : ", {10, 80}, {280, 50});
+    wxSearchCtrl* searchCtrl1 = new wxSearchCtrl {panel, wxID_ANY, wxEmptyString, {10, 10}, {280, wxDefaultCoord}};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, "Mode : Text", {10, 50}, {280, 20}};
+    wxStaticText* staticText2 = new wxStaticText {panel, wxID_ANY, "Search for : ", {10, 80}, {280, 50}};
     wxMenu* menuSearch = new wxMenu();
     
     inline static const int ID_TEXT = 1;
