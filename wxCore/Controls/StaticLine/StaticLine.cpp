2e3d6df9a230473de266aa4a58b4be443c2d0d63
Gammasoft <gammasoft71@gmail.com>
2024-02-04 11:22:20 +0100

    Review code

--- wxCore/Controls/StaticLine/StaticLine.cpp
+++ wxCore/Controls/StaticLine/StaticLine.cpp
@@ -6,16 +6,16 @@
 namespace StaticLineExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "StaticLine example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "StaticLine example"} {
       SetClientSize({300, 300});
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxStaticLine* staticLine1 = new wxStaticLine(panel, wxID_ANY, {20, 15}, {255, wxDefaultCoord});
-    wxStaticLine* staticLine2 = new wxStaticLine(panel, wxID_ANY, {285, 20}, {wxDefaultCoord, 255}, wxLI_VERTICAL);
-    wxStaticLine* staticLine3 = new wxStaticLine(panel, wxID_ANY, {20, 285}, {255, wxDefaultCoord});
-    wxStaticLine* staticLine4 = new wxStaticLine(panel, wxID_ANY, {15, 20}, {wxDefaultCoord, 255}, wxLI_VERTICAL);
+    wxStaticLine* staticLine1 = new wxStaticLine {panel, wxID_ANY, {20, 15}, {255, wxDefaultCoord}};
+    wxStaticLine* staticLine2 = new wxStaticLine {panel, wxID_ANY, {285, 20}, {wxDefaultCoord, 255}, wxLI_VERTICAL};
+    wxStaticLine* staticLine3 = new wxStaticLine {panel, wxID_ANY, {20, 285}, {255, wxDefaultCoord}};
+    wxStaticLine* staticLine4 = new wxStaticLine {panel, wxID_ANY, {15, 20}, {wxDefaultCoord, 255}, wxLI_VERTICAL};
   };
 
   class Application : public wxApp {
