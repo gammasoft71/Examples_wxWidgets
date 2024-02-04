c46b0f0633f037059802485ec6cc78732d5285c9
Gammasoft <gammasoft71@gmail.com>
2024-02-04 11:14:33 +0100

    Review code

--- wxCore/Controls/SpinCtrl/SpinCtrl.cpp
+++ wxCore/Controls/SpinCtrl/SpinCtrl.cpp
@@ -1,14 +1,13 @@
 #include <wx/app.h>
-#include <wx/spinctrl.h>
 #include <wx/frame.h>
-#include <wx/msgout.h>
 #include <wx/panel.h>
+#include <wx/spinctrl.h>
 #include <wx/stattext.h>
 
 namespace SpinCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "SpinCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "SpinCtrl example"} {
       spinCtrl1->SetValue(50);
       spinCtrl1->Bind(wxEVT_SPINCTRL, [&](wxCommandEvent& event) {
         text->SetLabel(wxString::Format("value = %d", spinCtrl1->GetValue()));
@@ -17,8 +16,8 @@ namespace SpinCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxSpinCtrl* spinCtrl1 = new wxSpinCtrl(panel, 100, wxEmptyString, {80, 50});
-    wxStaticText* text = new wxStaticText(panel, wxID_ANY, "value = 50", {80, 100});
+    wxSpinCtrl* spinCtrl1 = new wxSpinCtrl {panel, 100, wxEmptyString, {80, 50}};
+    wxStaticText* text = new wxStaticText {panel, wxID_ANY, "value = 50", {80, 100}};
   };
 
   class Application : public wxApp {
