04b4b6f4168fcfde97d4f1ca1d01d7ccc22a3c53
Gammasoft <gammasoft71@gmail.com>
2024-02-04 11:13:44 +0100

    Review code

--- wxCore/Controls/SpinButton/SpinButton.cpp
+++ wxCore/Controls/SpinButton/SpinButton.cpp
@@ -1,14 +1,13 @@
 #include <wx/app.h>
-#include <wx/spinbutt.h>
 #include <wx/frame.h>
-#include <wx/msgout.h>
 #include <wx/panel.h>
+#include <wx/spinbutt.h>
 #include <wx/stattext.h>
 
 namespace SpinButtonExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "SpinButton example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "SpinButton example"} {
       spinButton1->SetRange(10, 20);
       spinButton1->SetValue(15);
       spinButton1->Bind(wxEVT_SPIN, [&](wxCommandEvent& event) {
@@ -18,8 +17,8 @@ namespace SpinButtonExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxSpinButton* spinButton1 = new wxSpinButton(panel, wxID_ANY, {10, 10}, wxDefaultSize, wxSP_VERTICAL|wxSP_WRAP);
-    wxStaticText* text = new wxStaticText(panel, wxID_ANY, "value = 15", {10, 50});
+    wxSpinButton* spinButton1 = new wxSpinButton {panel, wxID_ANY, {10, 10}, wxDefaultSize, wxSP_VERTICAL|wxSP_WRAP};
+    wxStaticText* text = new wxStaticText {panel, wxID_ANY, "value = 15", {10, 50}};
   };
 
   class Application : public wxApp {
