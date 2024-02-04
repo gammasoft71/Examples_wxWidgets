049ee9228bf6f2b82f0a4ead096e98ff4309872f
Gammasoft <gammasoft71@gmail.com>
2024-02-04 19:43:56 +0100

    Review code

--- wxCore/Controls/TreeCtrl/TreeCtrl.cpp
+++ wxCore/Controls/TreeCtrl/TreeCtrl.cpp
@@ -1,10 +1,12 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/treectrl.h>
 
 namespace TreeCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "TreeCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "TreeCtrl example"} {
       auto rootId = treeCtrl->AddRoot("Root");
       auto firstId = treeCtrl->AppendItem(rootId, "First");
       treeCtrl->AppendItem(firstId, "Second");
@@ -19,7 +21,7 @@ namespace TreeCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxTreeCtrl* treeCtrl = new wxTreeCtrl(panel, wxID_ANY, {10, 10}, {150, 200});
+    wxTreeCtrl* treeCtrl = new wxTreeCtrl {panel, wxID_ANY, {10, 10}, {150, 200}};
   };
 
   class Application : public wxApp {
