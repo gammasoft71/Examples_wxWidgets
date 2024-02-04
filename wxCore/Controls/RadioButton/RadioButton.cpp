0071aaeeb83f5f94bee1a45fe390947963e52a0f
Gammasoft <gammasoft71@gmail.com>
2024-02-04 11:03:02 +0100

    REview code

--- wxCore/Controls/RadioButton/RadioButton.cpp
+++ wxCore/Controls/RadioButton/RadioButton.cpp
@@ -1,14 +1,14 @@
 #include <wx/app.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
-#include <wx/sizer.h>
 #include <wx/radiobut.h>
+#include <wx/sizer.h>
 #include <wx/stattext.h>
 
 namespace RadioButtonExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "RadioButton example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "RadioButton example"} {
       radioButton1->SetValue(true);
       Bind(wxEVT_RADIOBUTTON, [&](wxCommandEvent& event) {
         staticText1->SetLabel(wxString::Format("radioButton3 state = %s", radioButton3->GetValue() ? "true" : "false"));
@@ -19,10 +19,10 @@ namespace RadioButtonExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxRadioButton* radioButton1 = new wxRadioButton(panel, wxID_ANY, "radioButton 1", {30, 30});
-    wxRadioButton* radioButton2 = new wxRadioButton(panel, wxID_ANY, "radioButton 2", {30, 60});
-    wxRadioButton* radioButton3 = new wxRadioButton(panel, wxID_ANY, "radioButton 3", {30, 90});
-    wxStaticText* staticText1 = new wxStaticText(panel, wxID_ANY, wxEmptyString, {30, 150});
+    wxRadioButton* radioButton1 = new wxRadioButton {panel, wxID_ANY, "radioButton 1", {30, 30}};
+    wxRadioButton* radioButton2 = new wxRadioButton {panel, wxID_ANY, "radioButton 2", {30, 60}};
+    wxRadioButton* radioButton3 = new wxRadioButton {panel, wxID_ANY, "radioButton 3", {30, 90}};
+    wxStaticText* staticText1 = new wxStaticText {panel, wxID_ANY, wxEmptyString, {30, 150}};
   };
 
   class Application : public wxApp {
