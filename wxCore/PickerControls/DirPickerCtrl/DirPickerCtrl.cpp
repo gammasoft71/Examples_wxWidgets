830b73867212904a90303ada2634fc2bf69dfa95
Gammasoft <gammasoft71@gmail.com>
2024-02-04 19:55:25 +0100

    Review code

--- wxCore/PickerControls/DirPickerCtrl/DirPickerCtrl.cpp
+++ wxCore/PickerControls/DirPickerCtrl/DirPickerCtrl.cpp
@@ -1,10 +1,13 @@
-#include <wx/wx.h>
+#include <wx/app.h>
 #include <wx/filepicker.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
 
 namespace DirPickerCtrlExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "DirPickerCtrl example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "DirPickerCtrl example"} {
       picker->Bind(wxEVT_DIRPICKER_CHANGED, [&](wxFileDirPickerEvent& event) {
         label->SetLabel(wxString::Format("Path = %s", event.GetPath()));
       });
@@ -12,8 +15,8 @@ namespace DirPickerCtrlExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "Path = ", wxPoint(10, 50));
-    wxDirPickerCtrl* picker = new wxDirPickerCtrl(panel, wxID_ANY, wxEmptyString, wxEmptyString, {10, 10}, wxDefaultSize, wxDIRP_DEFAULT_STYLE|wxDIRP_SMALL);
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "Path = ", wxPoint(10, 50)};
+    wxDirPickerCtrl* picker = new wxDirPickerCtrl {panel, wxID_ANY, wxEmptyString, wxEmptyString, {10, 10}, wxDefaultSize, wxDIRP_DEFAULT_STYLE|wxDIRP_SMALL};
   };
 
   class Application : public wxApp {
