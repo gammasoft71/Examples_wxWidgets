9026ad0369717b3095c00fc3d5f1cdb5619ec73e
Gammasoft <gammasoft71@gmail.com>
2024-02-05 09:28:40 +0100

    Review code

--- wxCore/Components/RichToolTip/RichToolTip.cpp
+++ wxCore/Components/RichToolTip/RichToolTip.cpp
@@ -1,32 +1,38 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/checkbox.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/richtooltip.h>
+#include <wx/stattext.h>
 
 namespace RichTooolTipExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "TooTip example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "TooTip example"} {
       button->Bind(wxEVT_ENTER_WINDOW, [&](wxMouseEvent& event) {
-        wxRichToolTip toolTipButton("ToolTip title", "button toolTip...");
-        toolTipButton.ShowFor(button);
+        auto toolTip = wxRichToolTip {"ToolTip title", "button toolTip..."};
+        toolTip.ShowFor(button);
       });
       
       check->Bind(wxEVT_ENTER_WINDOW, [&](wxMouseEvent& event) {
-        wxRichToolTip toolTipCheck("ToolTip title", "check toolTip..." );
-        toolTipCheck.ShowFor(check);
+        auto toolTip = wxRichToolTip {"ToolTip title", "check toolTip..." };
+        toolTip.SetIcon();
+        toolTip.ShowFor(check);
       });
       
       label->Bind(wxEVT_ENTER_WINDOW, [&](wxMouseEvent& event) {
-        wxRichToolTip toolTipLabel("ToolTip title", "label toolTip...");
-        toolTipLabel.SetIcon(wxICON_WARNING);
-        toolTipLabel.ShowFor(label);
+        auto toolTip = wxRichToolTip {"ToolTip title", "label toolTip..."};
+        toolTip.SetIcon(wxICON_WARNING);
+        toolTip.ShowFor(label);
       });
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "button", {10, 10});
-    wxCheckBox* check = new wxCheckBox(panel, wxID_ANY, "check", {10, 40});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "label", {10, 70});
+    wxButton* button = new wxButton {panel, wxID_ANY, "button", {10, 10}};
+    wxCheckBox* check = new wxCheckBox {panel, wxID_ANY, "check", {10, 40}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "label", {10, 70}};
   };
 
   class Application : public wxApp {
