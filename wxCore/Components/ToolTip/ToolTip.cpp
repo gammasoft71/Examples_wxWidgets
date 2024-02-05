14ea9ce6f8b5eaf5a2a9efd646e69c25299a26e8
Gammasoft <gammasoft71@gmail.com>
2024-02-05 14:47:26 +0100

    Review code

--- wxCore/Components/ToolTip/ToolTip.cpp
+++ wxCore/Components/ToolTip/ToolTip.cpp
@@ -1,11 +1,15 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/checkbox.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/richtooltip.h>
+#include <wx/stattext.h>
 
 namespace ToolTipExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "TooTip example") {
-
+    Frame() : wxFrame {nullptr, wxID_ANY, "TooTip example"} {
       button->SetToolTip("button toolTip...");
       check->SetToolTip("check toolTip...");
       label->SetToolTip("label toolTip...");
@@ -13,9 +17,9 @@ namespace ToolTipExample {
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "button", {10, 10});
-    wxCheckBox* check = new wxCheckBox(panel, wxID_ANY, "check", {10, 40});
-    wxStaticText* label = new wxStaticText(panel, wxID_ANY, "label", {10, 70});
+    wxButton* button = new wxButton {panel, wxID_ANY, "button", {10, 10}};
+    wxCheckBox* check = new wxCheckBox {panel, wxID_ANY, "check", {10, 40}};
+    wxStaticText* label = new wxStaticText {panel, wxID_ANY, "label", {10, 70}};
   };
 
   class Application : public wxApp {
