20b89eda20440e469e04ece1e26de9d8b7dfe6e3
Gammasoft <gammasoft71@gmail.com>
2024-02-05 14:47:31 +0100

    Review code

--- wxCore/Printing/PrintAbortDialog/PrintAbortDialog.cpp
+++ wxCore/Printing/PrintAbortDialog/PrintAbortDialog.cpp
@@ -1,27 +1,29 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
 #include <wx/print.h>
-#include <wx/richtext/richtextprint.h>
 
 namespace PrintAbortDialogExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "PrintAbortDialog example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "PrintAbortDialog example"} {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         dialog->Show();
         wxYield();
         for (auto step = 0; step <= 10; step++) {
           dialog->SetProgress(step, 10, 1, 1);
-          wxYield();
           wxMilliSleep(200);
+          wxYield();
         }
         dialog->Hide();
       });
     }
 
   private:
-    wxPrintAbortDialog* dialog = new wxPrintAbortDialog(this, "My document");
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Print abort...", {10, 10});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Print abort...", {10, 10}};
+    wxPrintAbortDialog* dialog = new wxPrintAbortDialog {this, "My document"};
   };
 
   class Application : public wxApp {
