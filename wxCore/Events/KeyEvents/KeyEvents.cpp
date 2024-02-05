e86e0d9d5a5f1ad2ffeb4125ace147c3b722cf5e
Gammasoft <gammasoft71@gmail.com>
2024-02-05 23:04:18 +0100

    Review code

--- wxCore/Events/KeyEvents/KeyEvents.cpp
+++ wxCore/Events/KeyEvents/KeyEvents.cpp
@@ -1,22 +1,23 @@
 #undef NDEBUG
-#include <wx/wx.h>
+#include <wx/app.h>
 #include <wx/display.h>
+#include <wx/frame.h>
 #include <wx/log.h>
 
 namespace KeyEventsExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "KeyEvents") {
-      panel->Bind(wxEVT_KEY_DOWN, [&](wxKeyEvent& event) {
+    Frame() : wxFrame {nullptr, wxID_ANY, "KeyEvents"} {
+      control->Bind(wxEVT_KEY_DOWN, [&](wxKeyEvent& event) {
         wxLogDebug(wxString::Format("KeyDown={KeyCode=0x%04X, Modifiers=[%s]}", event.GetKeyCode(), ModiiersToString(event.GetModifiers())));
         event.Skip();
       });
       
-      panel->Bind(wxEVT_CHAR, [&](wxKeyEvent& event) {
+      control->Bind(wxEVT_CHAR, [&](wxKeyEvent& event) {
         wxLogDebug(wxString::Format("Char={UnicodeKey=%s}", event.GetUnicodeKey()== WXK_NONE ? "[None]" : wxString::Format("'%c'", event.GetUnicodeKey())));
       });
       
-      panel->Bind(wxEVT_KEY_UP, [&](wxKeyEvent& event) {
+      control->Bind(wxEVT_KEY_UP, [&](wxKeyEvent& event) {
         wxLogDebug(wxString::Format("KeyUp={KeyCode=0x%04X, Modifiers=[%s]}", event.GetKeyCode(), ModiiersToString(event.GetModifiers())));
         if (event.GetModifiers() == WXK_NONE) wxLogDebug("");
       });
@@ -25,23 +26,25 @@ namespace KeyEventsExample {
   private:
     static wxString ModiiersToString(int modifiers) {
       if (!modifiers) return "None";
-      wxString result;
+      auto result = wxString {};
       if ((modifiers & wxMOD_SHIFT) == wxMOD_SHIFT) result += "Shift, ";
       if ((modifiers & wxMOD_RAW_CONTROL) == wxMOD_RAW_CONTROL) result += "Control, ";
       if ((modifiers & wxMOD_ALT) == wxMOD_ALT) result += "Alt, ";
+#if defined(__APPLE__)
       if ((modifiers & wxMOD_CONTROL) == wxMOD_CONTROL) result += "Command, ";
+#endif
       if ((modifiers & wxMOD_META) == wxMOD_META) result += "Meta, ";
       if (result.size() > 1) result.resize(result.size() - 2);
       return result;
     }
     
-    wxControl* panel = new wxControl(this, wxID_ANY);
+    wxControl* control = new wxControl {this, wxID_ANY};
   };
 
   class Application : public wxApp {
     bool OnInit() override {
-      wxLogWindow* logWindow = new wxLogWindow(nullptr, "Debug");
-      logWindow->GetFrame()->SetSize(wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetLeft(), wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetTop() + wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetHeight() - wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetHeight() / 4, wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetWidth(), wxDisplay(wxDisplay::GetFromWindow(logWindow->GetFrame())).GetClientArea().GetHeight() / 4);
+      auto logWindow = new wxLogWindow {nullptr, "Debug"};
+      logWindow->GetFrame()->SetSize(wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetLeft(), wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetTop() + wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetHeight() - wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetHeight() / 4, wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetWidth(), wxDisplay {static_cast<unsigned>(wxDisplay::GetFromWindow(logWindow->GetFrame()))}.GetClientArea().GetHeight() / 4);
       return (new Frame)->Show();
     }
   };
