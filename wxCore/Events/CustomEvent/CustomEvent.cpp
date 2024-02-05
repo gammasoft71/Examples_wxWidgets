c6c754934094dbc87980c86958914e8f37cc22ff
Gammasoft <gammasoft71@gmail.com>
2024-02-05 17:31:54 +0100

    Review code

--- wxCore/Events/CustomEvent/CustomEvent.cpp
+++ wxCore/Events/CustomEvent/CustomEvent.cpp
@@ -1,44 +1,44 @@
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/frame.h>
+#include <wx/msgdlg.h>
+#include <wx/panel.h>
 #include <any>
-#include <wx/wx.h>
 
 using namespace std;
 
 namespace CustomEventExample {
   class CustomEvent;
   wxDECLARE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
-
+  
   class CustomEvent : public wxCommandEvent {
   public:
-    CustomEvent(wxEventType commandType = WX_EVT_CUSTOM_EVENT, int id = 0) : wxCommandEvent(commandType, id) { }
-    CustomEvent(const CustomEvent& event) : wxCommandEvent(event), tag_(event.tag_) {}
-    
-    any tag() const {return tag_;}
-    void tag(const any& value) {tag_ = value;}
+    explicit CustomEvent(const std::any& tag, wxEventType commandType = WX_EVT_CUSTOM_EVENT, int id = 0) : wxCommandEvent {commandType, id}, tag {tag} {}
+
+    std::any Tag() const noexcept {return tag;}
     
   private:
-    any tag_;
+    std::any tag;
   };
 
   wxDEFINE_EVENT(WX_EVT_CUSTOM_EVENT, CustomEvent);
 
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "Custom event example") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "Custom event example"} {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         static auto counter = 0;
-        auto customEvent = new CustomEvent();
-        customEvent->tag(++counter);
-        wxQueueEvent(this, customEvent);
+        wxQueueEvent(this, new CustomEvent {++counter});
       });
       
       Bind(WX_EVT_CUSTOM_EVENT, [&](CustomEvent& event) {
-        wxMessageBox(wxString::Format("Receive CustomEvent event (%d)", any_cast<int>(event.tag())), "CustomEVent", wxOK);
+        wxMessageBox(wxString::Format("Receive CustomEvent event (%d)", any_cast<int>(event.Tag())), "CustomEVent", wxOK, this);
       });
     }
     
   private:
     wxPanel* panel = new wxPanel {this};
-    wxButton* button = new wxButton(panel, wxID_ANY, "Send Event", {10, 10});
+    wxButton* button = new wxButton {panel, wxID_ANY, "Send Event", {10, 10}};
   };
 
   class Application : public wxApp {
