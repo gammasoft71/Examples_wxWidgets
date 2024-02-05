3d2a9adca54e560a965be0e149c6203a9c85e66a
Gammasoft <gammasoft71@gmail.com>
2024-02-05 22:51:55 +0100

    Review code

--- wxCore/Events/FrameClick/FrameClick.cpp
+++ wxCore/Events/FrameClick/FrameClick.cpp
@@ -1,9 +1,12 @@
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/frame.h>
+#include <wx/msgdlg.h>
+#include <wx/panel.h>
 
 namespace FrameClickExample {
   class Frame : public wxFrame {
   public:
-    Frame() : wxFrame(nullptr, wxID_ANY, "Click anywhere on the frame") {
+    Frame() : wxFrame {nullptr, wxID_ANY, "Click anywhere on the frame"} {
       panel->Bind(wxEVT_LEFT_UP, [&](wxMouseEvent& event) {
         wxMessageBox(wxString::Format("The frame is clicked at {x=%d, y=%d}", event.GetPosition().x, event.GetPosition().y), "FrameClick", wxOK) ;
       });
