2cb4a430dc6aa688fa907c207346986311dae9fb
Gammasoft <gammasoft71@gmail.com>
2024-02-06 13:29:32 +0100

    Review code

--- wxCore/Others/UserDialogShowWindowModal/UserDialogShowWindowModal.cpp
+++ wxCore/Others/UserDialogShowWindowModal/UserDialogShowWindowModal.cpp
@@ -1,10 +1,15 @@
-#include <string>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
 
 namespace UserDialogShowWindowModalExample {
   class UserInputDialog : public wxDialog {
   public:
-    UserInputDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, "Input Dialog") {
+    UserInputDialog(wxWindow* parent) : wxDialog {parent, wxID_ANY, "Input Dialog"} {
       SetClientSize(200, 80);
       CenterOnScreen();
       buttonOk->SetDefault();
@@ -14,15 +19,15 @@ namespace UserDialogShowWindowModalExample {
     virtual void SetInputText(const wxString& inputString) {text->SetValue(inputString);}
 
   private:
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, {10, 10}, {180, 25});
-    wxButton* buttonCancel = new wxButton(panel, wxID_CANCEL, "Cancel", {10, 50}, {85, -1});
-    wxButton* buttonOk = new wxButton(panel, wxID_OK, "OK", {105, 50}, {85, -1});
+    wxPanel* panel = new wxPanel {this};
+    wxTextCtrl* text = new wxTextCtrl {panel, wxID_ANY, wxEmptyString, {10, 10}, {180, 25}};
+    wxButton* buttonCancel = new wxButton {panel, wxID_CANCEL, "Cancel", {10, 50}, {85, -1}};
+    wxButton* buttonOk = new wxButton {panel, wxID_OK, "OK", {105, 50}, {85, -1}};
   };
 
-  class Form1 : public wxFrame {
+  class Frame : public wxFrame {
   public:
-    Form1() : wxFrame(nullptr, wxID_ANY, "User dialog example", wxDefaultPosition, {800, 450}) {
+    Frame() : wxFrame(nullptr, wxID_ANY, "User dialog example", wxDefaultPosition, {800, 450}) {
       Center();
       
       buttonDialog->Bind(wxEVT_BUTTON, [&](wxCommandEvent& e) {
@@ -35,14 +40,14 @@ namespace UserDialogShowWindowModalExample {
       });
     }
   private:
-    UserInputDialog* dialog = new UserInputDialog(this);
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxButton* buttonDialog = new wxButton(panel, wxID_ANY, "Dialog", {10, 10});
-    wxStaticText* inputText = new wxStaticText(panel, wxID_ANY, "User input text", {10, 50});
+    UserInputDialog* dialog = new UserInputDialog {this};
+    wxPanel* panel = new wxPanel {this};
+    wxButton* buttonDialog = new wxButton {panel, wxID_ANY, "Dialog", {10, 10}};
+    wxStaticText* inputText = new wxStaticText {panel, wxID_ANY, "User input text", {10, 50}};
   };
 
   class Application : public wxApp {
-    bool OnInit() override {return (new Form1())->Show();}
+    bool OnInit() override {return (new Frame())->Show();}
   };
 }
 
--- wxCore/UserDialogs/UserDialog/UserDialog.cpp
+++ wxCore/UserDialogs/UserDialog/UserDialog.cpp
@@ -1,10 +1,15 @@
-#include <string>
-#include <wx/wx.h>
+#include <wx/app.h>
+#include <wx/button.h>
+#include <wx/dialog.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/stattext.h>
+#include <wx/textctrl.h>
 
 namespace UserDialogExample {
   class UserInputDialog : public wxDialog {
   public:
-    UserInputDialog(wxWindow* parent) : wxDialog(parent, wxID_ANY, "Input Dialog") {
+    UserInputDialog(wxWindow* parent) : wxDialog {parent, wxID_ANY, "Input Dialog"} {
       SetClientSize(200, 80);
       CenterOnParent();
       buttonOk->SetDefault();
@@ -14,30 +19,30 @@ namespace UserDialogExample {
     virtual void SetInputText(const wxString& inputString) {text->SetValue(inputString);}
 
   private:
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxTextCtrl* text = new wxTextCtrl(panel, wxID_ANY, wxEmptyString, {10, 10}, {180, 25});
-    wxButton* buttonCancel = new wxButton(panel, wxID_CANCEL, "Cancel", {10, 50}, {85, -1});
-    wxButton* buttonOk = new wxButton(panel, wxID_OK, "OK", {105, 50}, {85, -1});
+    wxPanel* panel = new wxPanel {this, wxID_ANY};
+    wxTextCtrl* text = new wxTextCtrl {panel, wxID_ANY, wxEmptyString, {10, 10}, {180, 25}};
+    wxButton* buttonCancel = new wxButton {panel, wxID_CANCEL, "Cancel", {10, 50}, {85, -1}};
+    wxButton* buttonOk = new wxButton {panel, wxID_OK, "OK", {105, 50}, {85, -1}};
   };
 
-  class Form1 : public wxFrame {
+  class Frame : public wxFrame {
   public:
-    Form1() : wxFrame(nullptr, wxID_ANY, "User dialog example", wxDefaultPosition, {800, 450}, wxDEFAULT_FRAME_STYLE) {
+    Frame() : wxFrame {nullptr, wxID_ANY, "User dialog example", wxDefaultPosition, {800, 450}, wxDEFAULT_FRAME_STYLE} {
       
       buttonDialog->Bind(wxEVT_BUTTON, [&](wxCommandEvent& e) {
-        UserInputDialog dialog(this);
+        auto dialog = UserInputDialog {this};
         dialog.SetInputText(inputText->GetLabel());
         if (dialog.ShowModal() == wxID_OK) inputText->SetLabel(dialog.GetInputText());
       });
     }
   private:
-    wxPanel* panel = new wxPanel(this, wxID_ANY);
-    wxButton* buttonDialog = new wxButton(panel, wxID_ANY, "Dialog", {10, 10});
-    wxStaticText* inputText = new wxStaticText(panel, wxID_ANY, "User input text", {10, 50});
+    wxPanel* panel = new wxPanel {this};
+    wxButton* buttonDialog = new wxButton {panel, wxID_ANY, "Dialog", {10, 10}};
+    wxStaticText* inputText = new wxStaticText {panel, wxID_ANY, "User input text", {10, 50}};
   };
 
   class Application : public wxApp {
-    bool OnInit() override {return (new Form1())->Show();}
+    bool OnInit() override {return (new Frame)->Show();}
   };
 }
 
