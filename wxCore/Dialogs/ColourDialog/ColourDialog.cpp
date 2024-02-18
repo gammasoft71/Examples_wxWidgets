86794f82946752708b4316e2978f04ad8bb05730
Gammasoft <gammasoft71@gmail.com>
2024-02-18 07:12:22 +0100

    Review example

--- wxCore/Dialogs/ColourDialog/ColourDialog.cpp
+++ wxCore/Dialogs/ColourDialog/ColourDialog.cpp
@@ -3,6 +3,7 @@
 #include <wx/colordlg.h>
 #include <wx/frame.h>
 #include <wx/panel.h>
+#include <wx/stattext.h>
 #include <vector>
 
 using namespace std;
@@ -13,22 +14,30 @@ namespace ColourDialogExample {
     Frame() : wxFrame {nullptr, wxID_ANY, "ColourDialog example"} {
       button->Bind(wxEVT_BUTTON, [&](wxCommandEvent& event) {
         auto colourData = wxColourData {};
-        colourData.SetColour(GetBackgroundColour());
+        colourData.SetColour(selectedColor);
         for (auto index = 0; index < static_cast<int>(customColours.size()); index++)
           colourData.SetCustomColour(index, customColours[index]);
         auto colourDialog = wxColourDialog {this, &colourData};
         if (colourDialog.ShowModal() == wxID_OK) {
-          SetBackgroundColour(colourDialog.GetColourData().GetColour());
-          Refresh();
+          selectedColor = colourDialog.GetColourData().GetColour();
+          testZone->SetBackgroundColour(selectedColor);
+          testZone->Refresh();
+          colorText->SetLabel(selectedColor.GetAsString(wxC2S_HTML_SYNTAX));
         }
         for (auto index = 0; index < static_cast<int>(customColours.size()); index++)
           customColours[index], colourData.GetCustomColour(index);
       });
+
+      testZone->SetBackgroundColour(selectedColor);
+      colorText->SetLabel(selectedColor.GetAsString(wxC2S_HTML_SYNTAX));
     }
 
   private:
     wxPanel* panel = new wxPanel {this};
     wxButton* button = new wxButton {panel, wxID_ANY, "Color...", {10, 10}};
+    wxPanel* testZone = new wxPanel {panel, wxID_ANY, {10, 50}, {200, 100}, wxBORDER_SUNKEN | wxALIGN_CENTER | wxALIGN_CENTER_VERTICAL};
+    wxStaticText* colorText = new wxStaticText {panel, wxID_ANY, wxEmptyString, {10, 160}};
+    wxColour selectedColor = wxTheColourDatabase->Find("red");
     vector<wxColour> customColours {{0xFF, 0x00, 0x00}, {0x00, 0xFF, 0x00}, {0x00, 0x00, 0xFF}, {0xFF, 0xFF, 0x00}, {0xF0, 0xF0, 0xF0}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}, {0xFF, 0xFF, 0xFF}};
   };
 
