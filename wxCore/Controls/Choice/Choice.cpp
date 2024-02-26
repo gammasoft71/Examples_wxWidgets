3a37933af3385df68d792d515decaddc291d39ae
Gammasoft <gammasoft71@gmail.com>
2024-02-26 14:37:49 +0100

    Update Choice.cpp

--- wxCore/Controls/Choice/Choice.cpp
+++ wxCore/Controls/Choice/Choice.cpp
@@ -9,12 +9,10 @@ namespace ChoiceExample {
   class Frame : public wxFrame {
   public:
     Frame() : wxFrame {nullptr, wxID_ANY, "Choice example"} {
-      auto items = vector<wxString> {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"};
-      choice1->Append(static_cast<int>(items.size()), items.data());
+      choice1->Append(10, vector<wxString> {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"}.data());
       choice1->SetSelection(0);
       choice1->Bind(wxEVT_CHOICE, &Frame::OnChoicClick, this);
 
-      choice2->Append(static_cast<int>(items.size()), items.data());
       choice2->SetSelection(0);
       choice2->Bind(wxEVT_CHOICE, &Frame::OnChoicClick, this);
     }
@@ -25,8 +23,8 @@ namespace ChoiceExample {
       choice2->SetSelection(static_cast<wxChoice*>(e.GetEventObject())->GetSelection());
     }
     wxPanel* panel = new wxPanel {this};
-    wxChoice* choice1 = new wxChoice {panel, wxID_ANY, {10, 10}};
-    wxChoice* choice2 = new wxChoice {panel, wxID_ANY, {10, 50}};
+    wxChoice* choice1 = new wxChoice {panel, wxID_ANY, {10, 10}, {112, wxDefaultCoord}};
+    wxChoice* choice2 = new wxChoice {panel, wxID_ANY, {10, 50}, {112, wxDefaultCoord}, 10, vector<wxString> {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"}.data()};
   };
 
   class Application : public wxApp {
